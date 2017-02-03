c553edaae7074401f4e66077f1c5724cef363eab
Anwesh43 <anweshthecool0@gmail.com>
2017-02-04 02:19:13 +0530

    fourbuttons impl

--- app/src/main/java/com/anwesome/ui/customviewaday/MainActivity.java
+++ app/src/main/java/com/anwesome/ui/customviewaday/MainActivity.java
@@ -1,14 +1,17 @@
 package com.anwesome.ui.customviewaday;
 
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
+import com.anwesome.ui.fourbutton.FourButtons;
 import com.anwesome.ui.polygonaltraverseview.PolygonalTraverseView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private String actions[] = {"action1","action2","action3","action4"};
+    private int images[] = {R.drawable.delivered,R.drawable.restart,R.drawable.onway,R.drawable.order};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,8 +21,21 @@ public class MainActivity extends AppCompatActivity {
         ppView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this,"Hello", Toast.LENGTH_SHORT).show();
+                showFourButtons();
             }
         });
+    }
+    public void showFourButtons() {
+        FourButtons fourButtons = new FourButtons(this);
+        for(int i=0;i<actions.length;i++) {
+            final int curIndex = i;
+            fourButtons.addButton(actions[i], BitmapFactory.decodeResource(getResources(), images[i]), new FourButtons.FourButtonListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(MainActivity.this,actions[curIndex],Toast.LENGTH_SHORT).show();
+                }
+            });
+            fourButtons.show();
+        }
     }
 }
