c4ce887e32679578c155c43f97b793f8a9633ecb
Anwesh43 <anweshthecool0@gmail.com>
2017-02-06 00:27:26 +0530

    Adding base BullettedList logic

--- bulletedlist/src/main/java/com/anwesome/ui/bulletedlist/BulletedList.java
+++ bulletedlist/src/main/java/com/anwesome/ui/bulletedlist/BulletedList.java
@@ -1,7 +1,35 @@
 package com.anwesome.ui.bulletedlist;
 
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.View;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by anweshmishra on 06/02/17.
  */
 public class BulletedList {
+    private Activity activity;
+    private List<String> items = new ArrayList<>();
+    public BulletedList(Activity activity) {
+        this.activity = activity;
+    }
+    public void show() {
+
+    }
+    public void addItem(String item) {
+        this.items.add(item);
+    }
+    public void setItems(String items[]) {
+        for(String item:items) {
+            this.items.add(item);
+        }
+    }
+    public void setItems(List<String> items) {
+        this.items = items;
+    }
 }
