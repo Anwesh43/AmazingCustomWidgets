8f7bc005006e1f728a93eb4c63daae69d68ea703
Anwesh43 <anweshthecool0@gmail.com>
2017-02-06 00:53:33 +0530

    Adding Bulleted Objects

--- bulletedlist/src/main/java/com/anwesome/ui/bulletedlist/BulletedList.java
+++ bulletedlist/src/main/java/com/anwesome/ui/bulletedlist/BulletedList.java
@@ -2,12 +2,10 @@ package com.anwesome.ui.bulletedlist;
 
 import android.app.Activity;
 import android.content.Context;
-import android.graphics.Canvas;
+import android.graphics.*;
+import android.view.MotionEvent;
 import android.view.View;
-import android.widget.ArrayAdapter;
-
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by anweshmishra on 06/02/17.
@@ -31,5 +29,72 @@ public class BulletedList {
     }
     public void setItems(List<String> items) {
         this.items = items;
+    }
+    public class BulletedListView extends View {
+        private int time = 0;
+        private boolean isAnimated = false;
+        private List<BulletedElement> elements = new ArrayList<>();
+        private Triangle triangle;
+        public BulletedListView(Context context) {
+            super(context);
+        }
+
+        public void onDraw(Canvas canvas) {
+            int w = canvas.getWidth(),h = canvas.getHeight();
+            if(time == 0) {
+                triangle = new Triangle();
+            }
+            time++;
+            if(isAnimated) {
+                try {
+                    Thread.sleep(50);
+                    invalidate();
+                }
+                catch(Exception ex) {
+
+                }
+            }
+        }
+        public boolean onTouchEvent(MotionEvent event) {
+            if(event.getAction() == MotionEvent.ACTION_DOWN && !isAnimated) {
+
+            }
+            return true;
+        }
+    }
+    private class Triangle  {
+        private float deg = 180,x,y,rot=0;
+        public Triangle(float x,float y) {
+            this.x = x;
+            this.y = y;
+        }
+        public void render(Canvas canvas,Paint paint) {
+            canvas.save();
+            canvas.translate(x,y);
+            canvas.rotate(deg);
+            canvas.restore();
+            deg+=rot;
+            rot = deg%180 == 0?0:rot;
+        }
+        public void startAnimating() {
+            if(rot == 0 && deg%180 == 0) {
+                rot = deg == 180?-30:30;
+            }
+        }
+    }
+    private class BulletedElement {
+        private String item;
+        private float y,h;
+        public BulletedElement(String item,float y,float h) {
+            this.item = item;
+            this.y = y;
+            this.h = h;
+        }
+        public boolean containsTap(float y) {
+            return y>this.y && y<this.y+this.h;
+        }
+        public int hashCode() {
+            return item.hashCode()+(int)y+(int)h;
+        }
     }
 }
