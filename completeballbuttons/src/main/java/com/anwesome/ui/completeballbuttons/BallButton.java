0f7e804004fde0c0402dca68b60e5d0ea4781fe3
Anwesh43 <anweshthecool0@gmail.com>
2017-02-07 12:44:37 +0530

    Adding BallButton

---
+++ completeballbuttons/src/main/java/com/anwesome/ui/completeballbuttons/BallButton.java
@@ -0,0 +1,53 @@
+package com.anwesome.ui.completeballbuttons;
+
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.view.View;
+
+/**
+ * Created by anweshmishra on 07/02/17.
+ */
+public class BallButton {
+    private char label;
+    private int deg;
+    private float x,y,r;
+    private View.OnClickListener onClickListener;
+    private float scale = 0,scaleSpeed = 0.0f,degLabel = 0,degSpeed = 0;
+    public void setR(float r) {
+        this.r = r;
+    }
+    public BallButton(char label) {
+        this.label = label;
+    }
+    public void setSpeed(float dir) {
+        this.scaleSpeed = 0.1f*dir;
+        this.degSpeed = 36*dir;
+    }
+    public void setDimensions(int deg,float pivotx,float pivoty,float r1,float r) {
+        this.deg = deg;
+        this.x = pivotx+(float)(r1*Math.cos(deg*Math.PI/180));
+        this.y = pivoty+(float)(r1*Math.sin(deg*Math.PI/180));
+        this.r = r;
+    }
+    public void draw(Canvas canvas, Paint paint,int color) {
+        canvas.save();
+        canvas.translate(r,r);
+        canvas.rotate(degLabel);
+        canvas.scale(scale,scale);
+        canvas.restore();
+        scale+=scaleSpeed;
+        degLabel+=degSpeed;
+        if(degLabel>=360 && scale>=1.0f) {
+            scale = 1.0f;
+            degLabel = 360;
+            degSpeed = 0;
+            scaleSpeed = 0;
+        }
+    }
+    public int hashCode() {
+        return (int)x+(int)y+deg+label;
+    }
+    public void setOnClickListener(View.OnClickListener onClickListener) {
+        this.onClickListener = onClickListener;
+    }
+}
