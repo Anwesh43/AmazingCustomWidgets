e2a60654903986bb838d7221323a4e7b721644c9
Anwesh43 <anweshthecool0@gmail.com>
2017-02-09 01:53:42 +0530

    Adding CompassButton class

---
+++ compassbutton/src/main/java/com/anwesome/ui/compassbutton/CompassButton.java
@@ -0,0 +1,16 @@
+package com.anwesome.ui.compassbutton;
+
+import android.app.Activity;
+
+/**
+ * Created by anweshmishra on 09/02/17.
+ */
+public class CompassButton {
+    private Activity activity;
+    public CompassButton(Activity activity) {
+        this.activity = activity;
+    }
+    public void show(int x,int y) {
+
+    }
+}
