754b5588a316f81c3c5c0b57115fb53daa5a59f0
Anwesh43 <anweshthecool0@gmail.com>
2017-02-11 03:26:08 +0530

    Adding circular window view

---
+++ circularwindow/src/main/java/com/anwesome/app/circularwindow/CircularWindow.java
@@ -0,0 +1,16 @@
+package com.anwesome.app.circularwindow;
+
+import android.app.Activity;
+
+/**
+ * Created by anweshmishra on 11/02/17.
+ */
+public class CircularWindow {
+    private Activity activity;
+    public CircularWindow(Activity activity) {
+        this.activity = activity;
+    }
+    public void show() {
+
+    }
+}
