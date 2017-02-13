a33953456ff2b5f1777492374cea64c66e81c57b
Anwesh43 <anweshthecool0@gmail.com>
2017-02-13 11:08:44 +0530

    Adding basic framework for menuexpander

---
+++ menuexpander/src/main/java/com/anwesome/app/menuexpander/MenuExpander.java
@@ -0,0 +1,16 @@
+package com.anwesome.app.menuexpander;
+
+import android.app.Activity;
+
+/**
+ * Created by anweshmishra on 13/02/17.
+ */
+public class MenuExpander {
+    private Activity activity;
+    public MenuExpander(Activity activity) {
+        this.activity = activity;
+    }
+    public void show() {
+        
+    }
+}
