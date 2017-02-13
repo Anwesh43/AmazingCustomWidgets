a7d529941142725e7cbe122e99002838ddb35aba
Anwesh43 <anweshthecool0@gmail.com>
2017-02-13 13:25:56 +0530

    Adding OnClickListener

--- menuexpander/src/main/java/com/anwesome/app/menuexpander/Menu.java
+++ menuexpander/src/main/java/com/anwesome/app/menuexpander/Menu.java
@@ -1,16 +1,22 @@
 package com.anwesome.app.menuexpander;
 
+import android.app.Activity;
 import android.graphics.*;
+
 
 /**
  * Created by anweshmishra on 13/02/17.
  */
 public class Menu {
     private Bitmap bitmap;
+    private MenuClickListener onClickListener;
     private boolean clicked = false;
     private float x,y,r,scale = 0.7f,dir =0;
     public Menu(Bitmap bitmap) {
         this.bitmap = bitmap;
+    }
+    public void setOnClickListener(MenuClickListener onClickListener) {
+        this.onClickListener = onClickListener;
     }
     public void setDimensions(float x,float y,float r) {
         bitmap = Bitmap.createScaledBitmap(bitmap,(int)r,(int)r,true);
@@ -38,10 +44,14 @@ public class Menu {
             dir = 0;
             scale = 0.7f;
             clicked = true;
+            if(onClickListener!=null) {
+                onClickListener.onClick();
+            }
+
         }
     }
     public boolean handleTap(float x,float y) {
-        boolean condition = x>=this.x-r && x<=this.x+r && y>=this.y-r && y<=this.y+r;
+        boolean condition = x>=this.x && x<=this.x+2*r && y>=this.y && y<=this.y+2*r;
         if(condition) {
             dir = 0.1f;
         }
@@ -55,5 +65,8 @@ public class Menu {
     }
     public void resetClick() {
         this.clicked = false;
+    }
+    public interface MenuClickListener {
+        void onClick();
     }
 }
