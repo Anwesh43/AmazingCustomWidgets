d407a8d46b9f8e780e47de82d59257481b8d1fee
Anwesh43 <anweshthecool0@gmail.com>
2017-02-14 02:30:34 +0530

    Adding ButtonsTriangle clas

---
+++ buttonintriangle/src/main/java/com/anwesome/app/buttonintriangle/ButtonsTriangle.java
@@ -0,0 +1,16 @@
+package com.anwesome.app.buttonintriangle;
+
+import android.app.Activity;
+
+/**
+ * Created by anweshmishra on 14/02/17.
+ */
+public class ButtonsTriangle {
+    private Activity activity;
+    public ButtonsTriangle(Activity activity) {
+        this.activity = activity;
+    }
+    public void show() {
+
+    }
+}
