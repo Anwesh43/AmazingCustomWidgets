ae20d7304281cdad1eda670871a860cf92a40ef8
Anwesh43 <anweshthecool0@gmail.com>
2017-02-13 11:29:53 +0530

    Adding MenuExpanderView to draw the menuexpanderview

---
+++ menuexpander/src/main/java/com/anwesome/app/menuexpander/Menu.java
@@ -0,0 +1,7 @@
+package com.anwesome.app.menuexpander;
+
+/**
+ * Created by anweshmishra on 13/02/17.
+ */
+public class Menu {
+}
--- menuexpander/src/main/java/com/anwesome/app/menuexpander/MenuExpander.java
+++ menuexpander/src/main/java/com/anwesome/app/menuexpander/MenuExpander.java
@@ -1,6 +1,13 @@
 package com.anwesome.app.menuexpander;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.view.MotionEvent;
+import android.view.View;
 
 /**
  * Created by anweshmishra on 13/02/17.
@@ -11,6 +18,51 @@ public class MenuExpander {
         this.activity = activity;
     }
     public void show() {
-        
+
+    }
+    private class MenuExpanderView extends View {
+        private float scale = 0.2f,deg = 0;
+        private boolean expanded = false,isAnimated = false;
+        private float boundW,boundH,x,y;
+        private int time = 0;
+        private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        public MenuExpanderView(Context context) {
+            super(context);
+        }
+        public void onDraw(Canvas canvas) {
+            int w = canvas.getWidth(),h = canvas.getHeight();
+            if(time == 0) {
+                boundW = 0.2f* w;
+                boundH = 0.2f*h;
+                x = w/2 -boundW;
+                y = h/2-boundH;
+            }
+            paint.setStyle(Paint.Style.FILL);
+            paint.setColor(Color.parseColor("#BB000000"));
+
+            int r = Math.max(w,h)/6;
+            canvas.save();
+            canvas.translate(w/2,h/2);
+            canvas.rotate(deg);
+            canvas.scale(scale,scale);
+            canvas.drawRoundRect(new RectF(-w/2,-h/2,w/2,h/2),r,r,paint);
+            canvas.restore();
+            time++;
+            if(isAnimated) {
+                try {
+                    Thread.sleep(50);
+                }catch (Exception ex) {
+
+                }
+            }
+
+        }
+        public boolean onTouchEvent(MotionEvent event) {
+            float x = event.getX(),y = event.getY();
+            if(event.getAction() == MotionEvent.ACTION_DOWN && !isAnimated) {
+
+            }
+            return true;
+        }
     }
 }
