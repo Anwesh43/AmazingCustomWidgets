5f45ec4d0334835700acabc1e04c775c8e48a153
Anwesh43 <anweshthecool0@gmail.com>
2017-02-16 00:47:55 +0530

    moving logic for group image button

--- groupimagebuttons/src/main/java/com/anwesome/app/groupimagebuttons/GroupImageButton.java
+++ groupimagebuttons/src/main/java/com/anwesome/app/groupimagebuttons/GroupImageButton.java
@@ -7,9 +7,20 @@ import android.graphics.*;
  */
 public class GroupImageButton {
     private Bitmap bitmap;
-    public float x,y,w,h,scale = 0.2f,deg = 0,xdir = 0,ydir = 0,finalX,finalY;
+    private int mode = 0;
+    public float x,y,w,h,scale = 0.2f,deg = 0,xdir = 0,ydir = 0,finalX,finalY,scaleSpeed=0,degSpeed=0;
+    private boolean moving = false;
     public GroupImageButton(Bitmap bitmap) {
         this.bitmap = bitmap;
+    }
+    public void startAnimating(int mode,float finalX,float finalY,float scaleSpeed,float degSpeed) {
+        this.mode = mode;
+        this.finalX = finalX;
+        this.finalY = finalY;
+        this.scaleSpeed = scaleSpeed;
+        this.degSpeed = degSpeed;
+        this.xdir = (finalX-x)/5;
+        this.ydir = (finalY-y)/5;
     }
     public void setDimensions(float x,float y,float w,float h) {
         this.x = x;
@@ -18,7 +29,10 @@ public class GroupImageButton {
         this.h = h;
         this.finalX = x;
         this.finalY = y;
-
+        this.moving = true;
+    }
+    public boolean isMoving() {
+        return moving;
     }
     public void draw(Canvas canvas, Paint paint) {
         canvas.save();
@@ -32,6 +46,27 @@ public class GroupImageButton {
         canvas.restore();
     }
     public void update() {
-        
+        x+=xdir;
+        y+=ydir;
+        if(mode == 0) {
+            if((ydir<0 && y<finalY) || (ydir>0 && y>finalY)) {
+                ydir = 0;
+                xdir = 0;
+                x = finalX;
+                y = finalY;
+                degSpeed = 0;
+                scaleSpeed = 0;
+            }
+        }
+        else if(mode == 1) {
+            if((ydir<0 && y<finalY) || (ydir>0 && y>finalY)) {
+                ydir = 0;
+                y = finalY;
+            }
+            if((xdir<0 && x<finalX) || (xdir>0 && x>finalX)) {
+                xdir = 0;
+                x = finalX;
+            }
+        }
     }
 }
