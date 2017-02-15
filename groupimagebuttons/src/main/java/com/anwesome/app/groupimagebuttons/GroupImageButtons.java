2d267cdfc638c24ecdd68bb8c7b3df9882051b3d
Anwesh43 <anweshthecool0@gmail.com>
2017-02-16 00:12:52 +0530

    Adding class GroupImageButtons

---
+++ groupimagebuttons/src/main/java/com/anwesome/app/groupimagebuttons/GroupImageButtons.java
@@ -0,0 +1,15 @@
+package com.anwesome.app.groupimagebuttons;
+
+import android.app.Activity;
+
+/**
+ * Created by anweshmishra on 16/02/17.
+ */
+public class GroupImageButtons {
+    public GroupImageButtons(Activity activity) {
+
+    }
+    public void show(){
+
+    }
+}
