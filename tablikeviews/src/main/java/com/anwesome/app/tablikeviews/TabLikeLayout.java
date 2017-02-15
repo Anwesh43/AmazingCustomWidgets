575913b372716bc6e6bc6a5eabc68af06e76607a
Anwesh43 <anweshthecool0@gmail.com>
2017-02-15 11:59:34 +0530

    Positioning the tabs in tabview

--- tablikeviews/src/main/java/com/anwesome/app/tablikeviews/TabLikeLayout.java
+++ tablikeviews/src/main/java/com/anwesome/app/tablikeviews/TabLikeLayout.java
@@ -3,10 +3,12 @@ package com.anwesome.app.tablikeviews;
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.graphics.Point;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 /**
  * Created by anweshmishra on 15/02/17.
@@ -14,7 +16,10 @@ import android.view.ViewGroup;
 public class TabLikeLayout {
     private Activity activity;
     private TabLikeView tabLikeView;
+    private ConcurrentLinkedQueue<TabElement> tabs = new ConcurrentLinkedQueue<>();
     private int w,h,tabW,tabH;
+    private float x = 0;
+    private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
     public TabLikeLayout(Activity activity) {
         this.activity = activity;
         initWH();
@@ -23,12 +28,25 @@ public class TabLikeLayout {
         Point size = new Point();
         w = size.x;
         h = size.y;
+        paint.setTextSize(h/60);
+        tabW = w;
+        tabH = h/40;
     }
-
+    public void addTab(TabElement tabElement) {
+        float text_W =  paint.measureText(tabElement.getTitle());
+        float currTabW = 2*text_W,currTabh = 2*paint.getTextSize();
+        if(x+currTabW>tabW) {
+            tabH+=currTabh;
+            x = 0;
+        }
+        tabElement.setDimensions(x,tabH,currTabW,currTabh);
+        x+=currTabW;
+        tabs.add(tabElement);
+    }
     public void show() {
         if(tabLikeView == null) {
             tabLikeView = new TabLikeView(activity);
-            activity.addContentView(tabLikeView,new ViewGroup.LayoutParams(tabW,tabH));
+            activity.addContentView(tabLikeView,new ViewGroup.LayoutParams(tabW,tabH+3*(int)paint.getTextSize()));
         }
     }
     private class TabLikeView extends View {
