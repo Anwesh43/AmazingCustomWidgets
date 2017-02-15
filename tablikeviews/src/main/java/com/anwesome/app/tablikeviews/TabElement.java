da6983f6a2771a48de59e0b2194ba0bf52c6b689
Anwesh43 <anweshthecool0@gmail.com>
2017-02-15 11:59:10 +0530

    Positioning tabs in the tabview

--- tablikeviews/src/main/java/com/anwesome/app/tablikeviews/TabElement.java
+++ tablikeviews/src/main/java/com/anwesome/app/tablikeviews/TabElement.java
@@ -13,9 +13,38 @@ public class TabElement {
     private Fragment fragment;
     private String title="";
     private float x=0,y=0,w=0,h=0,scale = 0f,deg=0,dir = 0;
+    private  boolean animStopped = false;
     public TabElement(Fragment fragment,String title) {
         this.fragment = fragment;
         this.title = title;
+    }
+
+    public Fragment getFragment() {
+        return fragment;
+    }
+
+    public float getX() {
+
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public float getW() {
+        return w;
+    }
+
+    public float getH() {
+        return h;
+    }
+
+    public void setDimensions(float x, float y, float w, float h) {
+        this.x = x;
+        this.w = w;
+        this.y = y;
+        this.h = h;
     }
     public void setDefault() {
         scale = 1;
@@ -37,6 +66,12 @@ public class TabElement {
         canvas.drawRect(new RectF(-w/2,-h/2,w/2,h/2),paint);
         canvas.restore();
     }
+    public boolean isAnimStopped() {
+        return animStopped;
+    }
+    public void startAnimation() {
+        this.animStopped = false;
+    }
     public void update() {
         this.scale+=0.2f*dir;
         this.deg+=72*dir;
@@ -44,11 +79,13 @@ public class TabElement {
             this.scale = 1;
             this.deg = 360;
             this.dir = 0;
+            animStopped = true;
         }
         else if(this.dir == -1 && this.scale <0) {
             this.dir = 0;
             this.scale = 0;
             this.deg = 0;
+            animStopped = true;
         }
     }
 
