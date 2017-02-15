0701300d4fc78ed61f02dd1e81600cd02d1a274c
Anwesh43 <anweshthecool0@gmail.com>
2017-02-16 00:09:28 +0530

    Removing unnecessary import

--- tablikeviews/src/main/java/com/anwesome/app/tablikeviews/TabLikeLayout.java
+++ tablikeviews/src/main/java/com/anwesome/app/tablikeviews/TabLikeLayout.java
@@ -1,7 +1,6 @@
 package com.anwesome.app.tablikeviews;
 
 import android.content.Context;
-import android.content.pm.ProviderInfo;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
