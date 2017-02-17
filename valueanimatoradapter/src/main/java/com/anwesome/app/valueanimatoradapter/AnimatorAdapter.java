d3478ea13ba92a366e6cbef2f091e45f8769c73d
Anwesh43 <anweshthecool0@gmail.com>
2017-02-17 12:02:58 +0530

    Adding Animator Adapter

--- valueanimatoradapter/src/main/java/com/anwesome/app/valueanimatoradapter/AnimatorAdapter.java
+++ valueanimatoradapter/src/main/java/com/anwesome/app/valueanimatoradapter/AnimatorAdapter.java
@@ -1,7 +1,25 @@
 package com.anwesome.app.valueanimatoradapter;
 
+import android.animation.Animator;
+import android.animation.ValueAnimator;
+
 /**
  * Created by anweshmishra on 17/02/17.
  */
-public class AnimatorAdapter {
+public class AnimatorAdapter implements ValueAnimator.AnimatorUpdateListener,Animator.AnimatorListener {
+    public void onAnimationUpdate(ValueAnimator animator) {
+
+    }
+    public void onAnimationEnd(Animator animator) {
+
+    }
+    public void onAnimationRepeat(Animator animator) {
+
+    }
+    public void onAnimationCancel(Animator animator) {
+
+    }
+    public void onAnimationStart(Animator animator) {
+
+    }
 }
