80dc1a33082abee6fd8e4c184c2ab4b56e48de57
Anwesh43 <anweshthecool0@gmail.com>
2017-02-17 11:41:23 +0530

    Adding constants

---
+++ customactionsheet/src/main/java/com/anwesome/app/customactionsheet/ActionSheet.java
@@ -0,0 +1,7 @@
+package com.anwesome.app.customactionsheet;
+
+/**
+ * Created by anweshmishra on 17/02/17.
+ */
+public class ActionSheet {
+}
---
+++ customactionsheet/src/main/java/com/anwesome/app/customactionsheet/ActionSheetConstants.java
@@ -0,0 +1,9 @@
+package com.anwesome.app.customactionsheet;
+
+/**
+ * Created by anweshmishra on 17/02/17.
+ */
+public class ActionSheetConstants {
+    public final static int TITLE_FONT = 25;
+    public final static int TEXT_FONT = 40;
+}
