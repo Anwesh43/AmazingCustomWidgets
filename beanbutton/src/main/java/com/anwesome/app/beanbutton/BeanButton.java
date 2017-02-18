6cf480497cb17e576883153c94eadb5724a46481
Anwesh43 <anweshthecool0@gmail.com>
2017-02-18 08:21:55 +0530

    Adding bean button module

---
+++ beanbutton/src/main/java/com/anwesome/app/beanbutton/BeanButton.java
@@ -0,0 +1,16 @@
+package com.anwesome.app.beanbutton;
+
+import android.app.Activity;
+
+/**
+ * Created by anweshmishra on 18/02/17.
+ */
+public class BeanButton {
+    private Activity activity;
+    public BeanButton(Activity activity) {
+        this.activity = activity;
+    }
+    public void show() {
+        
+    }
+}
