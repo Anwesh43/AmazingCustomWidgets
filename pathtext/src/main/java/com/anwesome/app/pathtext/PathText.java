093fcbf3f86e9990a169ce9852ee062d39a526e7
Anwesh43 <anweshthecool0@gmail.com>
2017-02-20 01:17:13 +0530

    Adding PathTextView

--- pathtext/src/main/java/com/anwesome/app/pathtext/PathText.java
+++ pathtext/src/main/java/com/anwesome/app/pathtext/PathText.java
@@ -1,6 +1,10 @@
 package com.anwesome.app.pathtext;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.MotionEvent;
+import android.view.View;
 
 /**
  * Created by anweshmishra on 20/02/17.
@@ -11,6 +15,17 @@ public class PathText {
         this.activity = activity;
     }
     public void show() {
-        
+
+    }
+    private class PathTextView extends View {
+        public PathTextView(Context context) {
+            super(context);
+        }
+        public void onDraw(Canvas canvas) {
+            
+        }
+        public boolean onTouchEvent(MotionEvent event) {
+            return true;
+        }
     }
 }
