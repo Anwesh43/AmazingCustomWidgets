7f4b0c48082167c407065e3223c79941acc7b5a2
Anwesh43 <anweshthecool0@gmail.com>
2017-02-19 09:45:38 +0530

    Adding ActionIcon

--- customfloatingactionbutton/src/main/java/com/anwesome/app/customfloatingactionbutton/ActionIcon.java
+++ customfloatingactionbutton/src/main/java/com/anwesome/app/customfloatingactionbutton/ActionIcon.java
@@ -8,24 +8,42 @@ import android.graphics.*;
  */
 public class ActionIcon {
     private Bitmap bitmap;
-    private float x,y,w;
+    private float x,y,w,cy,intialY,dir = 0;
     public ActionIcon(Bitmap bitmap) {
         this.bitmap = bitmap;
     }
-    public void setDimensions(float x,float y,float w) {
+    public void setDir(float dir) {
+        this.dir = dir;
+    }
+    public void setDimensions(float x,float y,float w,float cy) {
         this.x = x;
         this.y = y;
         this.w = w;
+        this.cy = cy;
+        this.intialY = cy;
         bitmap = Bitmap.createScaledBitmap(bitmap,(int)w/2,(int)w/2,true);
     }
     public void draw(Canvas canvas, Paint paint) {
         float r = w/2;
         Path path = new Path();
-        path.addCircle(x,y,r, Path.Direction.CCW);
+        path.addCircle(x,cy,r, Path.Direction.CCW);
         paint.setColor(Color.GRAY);
-        canvas.drawCircle(x,y,r,paint);
+        canvas.drawCircle(x,cy,r,paint);
         canvas.clipPath(path);
-        canvas.drawBitmap(bitmap,x-r/2,y-r/2,paint);
+        canvas.drawBitmap(bitmap,x-r/2,cy-r/2,paint);
+    }
+    public void update() {
+        float speed = Math.abs(y-intialY)/5;
+        y+=speed*dir;
+        if((dir == 1 && cy>=y) || (dir == -1 && cy<=intialY)) {
+            if(dir == 1) {
+                cy = y;
+            }
+            else {
+                cy = intialY;
+            }
+            dir = 0;
+        }
     }
     public int hashCode() {
         return bitmap.hashCode()+(int)y;
