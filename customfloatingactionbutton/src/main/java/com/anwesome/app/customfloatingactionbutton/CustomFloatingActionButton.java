799d97b2f0592276c48e0b2e2688612ddca02d72
Anwesh43 <anweshthecool0@gmail.com>
2017-02-19 09:46:20 +0530

    Adding MainActionButton,setting dimensions of actionicons,drawing actionicons

--- customfloatingactionbutton/src/main/java/com/anwesome/app/customfloatingactionbutton/CustomFloatingActionButton.java
+++ customfloatingactionbutton/src/main/java/com/anwesome/app/customfloatingactionbutton/CustomFloatingActionButton.java
@@ -1,7 +1,9 @@
 package com.anwesome.app.customfloatingactionbutton;
 
 import android.app.Activity;
-import android.graphics.Point;
+import android.content.Context;
+import android.graphics.*;
+import android.view.View;
 
 import com.anwesome.ui.dimensionsutil.DimensionsUtil;
 
@@ -12,8 +14,13 @@ import java.util.*;
  */
 public class CustomFloatingActionButton {
     private Activity activity;
-    private float y = 0;
+    private float y = 0,x=0,r=10;
+    private int color = Color.parseColor("#303F9F");
     private List<ActionIcon> actionIcons = new ArrayList<>();
+    private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    public void setColor(int color) {
+        this.color = color;
+    }
     public CustomFloatingActionButton(Activity activity) {
         this.activity = activity;
     }
@@ -22,12 +29,48 @@ public class CustomFloatingActionButton {
     }
     public void show() {
         Point size = DimensionsUtil.getDeviceDimension(activity);
-        float w = size.x,h = size.y,y = h/20,y1 = h/10+h/40+h/30;
+        float w = size.x,h = size.y,y1 = h/10+h/40+h/30;
+        x = w/2;
+        y = h/20;
+        r = h/20;
         for(ActionIcon actionIcon:actionIcons) {
-            actionIcon.setDimensions(w/2,y1,h/15);
+            actionIcon.setDimensions(w/2,y1,h/15,y);
             y1+=h/40+h/30;
         }
 
+    }
+    private class MainActionButton {
+        private boolean expanded = false;
+        private float deg = 0;
+        public void draw(Canvas canvas,Paint paint) {
+            for(ActionIcon actionIcon:actionIcons) {
+                actionIcon.draw(canvas,paint);
+            }
+            paint.setColor(color);
+            canvas.save();
+            canvas.translate(x,y);
+            canvas.rotate(deg);
+            canvas.drawCircle(0,0,r,paint);
+            for(int i=0;i<2;i++) {
+                canvas.save();
+                canvas.rotate(90*i);
+                paint.setColor(Color.WHITE);
+                canvas.drawLine(-(r*3)/4,0,(r*3)/4,0,paint);
+                canvas.restore();
+            }
+            canvas.restore();
+        }
+        public void update() {
+
+        }
+    }
+    private class FloatingActionButtonView extends View {
+        public FloatingActionButtonView(Context context) {
+            super(context);
+        }
+        public void onDraw(Canvas canvas) {
+
+        }
     }
 
 }
