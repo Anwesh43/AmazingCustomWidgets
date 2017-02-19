d829161d0842570252ded14b8917025e4540972b
Anwesh43 <anweshthecool0@gmail.com>
2017-02-20 01:14:46 +0530

    Adding PathText class

---
+++ pathtext/src/main/java/com/anwesome/app/pathtext/PathText.java
@@ -0,0 +1,16 @@
+package com.anwesome.app.pathtext;
+
+import android.app.Activity;
+
+/**
+ * Created by anweshmishra on 20/02/17.
+ */
+public class PathText {
+    private Activity activity;
+    public PathText(Activity activity) {
+        this.activity = activity;
+    }
+    public void show() {
+        
+    }
+}
