7ebbff4682e6cb5ab1609e1136d5f60300919fb9
Anwesh43 <anweshthecool0@gmail.com>
2017-02-21 01:05:31 +0530

    Adding MotionStore to store mode

--- modakbutton/src/main/java/com/anwesome/app/modakbutton/MotionStore.java
+++ modakbutton/src/main/java/com/anwesome/app/modakbutton/MotionStore.java
@@ -4,4 +4,11 @@ package com.anwesome.app.modakbutton;
  * Created by anweshmishra on 21/02/17.
  */
 public class MotionStore {
+    private int mode = 0;
+    public int getMode() {
+        return mode;
+    }
+    public void setMode(int mode) {
+        this.mode = mode;
+    }
 }
