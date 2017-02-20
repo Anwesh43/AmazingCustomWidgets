b0f7cc3ad7c45c8ad04061fb08e01f96c4008017
Anwesh43 <anweshthecool0@gmail.com>
2017-02-21 00:55:30 +0530

    Adding ModakButton class

---
+++ modakbutton/src/main/java/com/anwesome/app/modakbutton/ModakButton.java
@@ -0,0 +1,16 @@
+package com.anwesome.app.modakbutton;
+
+import android.app.Activity;
+
+/**
+ * Created by anweshmishra on 21/02/17.
+ */
+public class ModakButton {
+    private Activity activity;
+    public ModakButton(Activity activity) {
+        this.activity = activity;
+    }
+    public void show() {
+
+    }
+}
