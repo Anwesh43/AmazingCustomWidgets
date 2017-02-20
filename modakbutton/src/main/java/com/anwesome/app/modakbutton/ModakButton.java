a3748000439babef07a360e6d551ab4c5d67565f
Anwesh43 <anweshthecool0@gmail.com>
2017-02-21 02:31:10 +0530

    Drawing logic for ModakButton

--- modakbutton/src/main/java/com/anwesome/app/modakbutton/ModakButton.java
+++ modakbutton/src/main/java/com/anwesome/app/modakbutton/ModakButton.java
@@ -3,6 +3,7 @@ package com.anwesome.app.modakbutton;
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.view.View;
 
@@ -11,6 +12,7 @@ import android.view.View;
  */
 public class ModakButton {
     private Activity activity;
+    private LineMover lineMover;
     public ModakButton(Activity activity) {
         this.activity = activity;
     }
@@ -18,12 +20,22 @@ public class ModakButton {
 
     }
     private class ModakButtonView extends View {
+        private int time = 0;
         private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
         public ModakButtonView(Context context) {
             super(context);
         }
         public void onDraw(Canvas canvas) {
-
+            int w = canvas.getWidth(),h = canvas.getHeight();
+            if(time == 0) {
+                lineMover = new LineMover(w/2,h/2,w/3);
+            }
+            paint.setColor(Color.GRAY);
+            paint.setStyle(Paint.Style.STROKE);
+            paint.setStrokeWidth(canvas.getWidth()/40);
+            canvas.drawCircle(w/2,h/2,w/3,paint);
+            lineMover.draw(canvas,paint);
+            time++;
             try {
                 Thread.sleep(50);
                 invalidate();
