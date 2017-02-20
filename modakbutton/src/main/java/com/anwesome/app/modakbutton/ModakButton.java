5fcc923b7b6726278157829de393d041ab5a342c
Anwesh43 <anweshthecool0@gmail.com>
2017-02-21 00:58:16 +0530

    Adding the view class

--- modakbutton/src/main/java/com/anwesome/app/modakbutton/ModakButton.java
+++ modakbutton/src/main/java/com/anwesome/app/modakbutton/ModakButton.java
@@ -1,6 +1,9 @@
 package com.anwesome.app.modakbutton;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.View;
 
 /**
  * Created by anweshmishra on 21/02/17.
@@ -12,5 +15,13 @@ public class ModakButton {
     }
     public void show() {
 
+    }
+    private class ModakButtonView extends View {
+        public ModakButtonView(Context context) {
+            super(context);
+        }
+        public void onDraw(Canvas canvas) {
+
+        }
     }
 }
