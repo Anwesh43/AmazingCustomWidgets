606597e9349519984454a891decc9457a239c219
Anwesh43 <anweshthecool0@gmail.com>
2017-02-22 09:52:34 +0530

    Adding ImageClipper

---
+++ imageclippe/src/main/java/com/anwesome/app/imageclippe/ImageClipper.java
@@ -0,0 +1,15 @@
+package com.anwesome.app.imageclippe;
+
+import android.app.Activity;
+
+/**
+ * Created by anweshmishra on 22/02/17.
+ */
+public class ImageClipper {
+    public ImageClipper(Activity activity) {
+
+    }
+    public void show() {
+
+    }
+}
