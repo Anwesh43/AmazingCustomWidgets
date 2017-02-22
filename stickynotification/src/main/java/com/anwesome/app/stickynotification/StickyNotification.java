94e25dd361a2ba48de99729b8c606a144a969de4
Anwesh43 <anweshthecool0@gmail.com>
2017-02-23 04:23:54 +0530

    Initing Dimensions and adding dimensions util

--- stickynotification/src/main/java/com/anwesome/app/stickynotification/StickyNotification.java
+++ stickynotification/src/main/java/com/anwesome/app/stickynotification/StickyNotification.java
@@ -2,10 +2,14 @@ package com.anwesome.app.stickynotification;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Point;
 import android.view.MotionEvent;
 import android.view.View;
+
+import com.anwesome.ui.dimensionsutil.DimensionsUtil;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,12 +26,48 @@ public class StickyNotification {
     private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
     private StickyElementTextContainer stickyElementTextContainer;
     private List<StickyNotificationTextElement> textElements = new ArrayList<>();
-    public StickyNotification(Activity activity,String text) {
+    private Bitmap bitmap;
+    public StickyNotification(Activity activity,String text,Bitmap bitmap) {
         mActivity = activity;
+        this.bitmap = bitmap;
         initNotification(text);
     }
     private void initNotification(String text) {
-
+        Point size = DimensionsUtil.getDeviceDimension(mActivity);
+        int w = size.x,h = size.y;
+        float textSize = h/30,notifW= 9*w/10,textW = w/20,notifH = h/60,notifX = textW;
+        String tokens[] = text.split(" ");
+        String msg = "";
+        for(String token:tokens) {
+            float tw = paint.measureText(msg);
+            if(textW+tw>notifW) {
+                textW = w/10;
+                notifH+=textSize;
+                msg = token;
+            }
+            else {
+                msg = msg+" "+token;
+            }
+        }
+        msg = "";
+        notifH+=textSize*1.5f;
+        float totalH = notifH*2,notifY = totalH/2+textSize/2;
+        for(String token:tokens) {
+            float tw = paint.measureText(msg);
+            if(textW+tw>notifW) {
+                textElements.add(new StickyNotificationTextElement(msg,notifX,notifY));
+                notifX = w/20;
+                notifY+=textSize;
+                msg = token;
+            }
+            else {
+                msg = msg+" "+token;
+            }
+            stickyElementTextContainer = new StickyElementTextContainer(w/20,totalH/2+textSize/2,textElements,9*w/10,(int)notifH);
+            closeButton = new CloseButton(w-w/20,notifH/2-w/20,w/20);
+            
+        }
+        paint.setTextSize(textSize);
     }
     public void show() {
 
