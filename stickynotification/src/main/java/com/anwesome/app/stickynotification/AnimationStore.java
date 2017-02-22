201192224a32a6eba0a1380f37801def6ee54b21
Anwesh43 <anweshthecool0@gmail.com>
2017-02-23 03:49:26 +0530

    Adding animaiton store to store state of animation

--- stickynotification/src/main/java/com/anwesome/app/stickynotification/AnimationStore.java
+++ stickynotification/src/main/java/com/anwesome/app/stickynotification/AnimationStore.java
@@ -4,4 +4,11 @@ package com.anwesome.app.stickynotification;
  * Created by anweshmishra on 23/02/17.
  */
 public class AnimationStore {
+    private int mode = 0;
+    public void setMode(int mode) {
+        this.mode = mode;
+    }
+    public int getMode() {
+        return mode;
+    }
 }
