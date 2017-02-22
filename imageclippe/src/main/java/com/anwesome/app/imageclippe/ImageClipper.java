c4a9b9f23c1f1d376683c1f3155d58e7a5475c0f
Anwesh43 <anweshthecool0@gmail.com>
2017-02-22 09:54:58 +0530

    Adding ImageClipperView

--- imageclippe/src/main/java/com/anwesome/app/imageclippe/ImageClipper.java
+++ imageclippe/src/main/java/com/anwesome/app/imageclippe/ImageClipper.java
@@ -1,6 +1,10 @@
 package com.anwesome.app.imageclippe;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.MotionEvent;
+import android.view.View;
 
 /**
  * Created by anweshmishra on 22/02/17.
@@ -11,5 +15,16 @@ public class ImageClipper {
     }
     public void show() {
 
+    }
+    private class ImageClipperView  extends View {
+        public ImageClipperView(Context context) {
+            super(context);
+        }
+        public void onDraw(Canvas canvas) {
+            
+        }
+        public boolean onTouchEvent(MotionEvent event) {
+            return true;
+        }
     }
 }
