e8dbb2fb77a763f1d3eb30e3c69f560da6292153
Anwesh43 <anweshthecool0@gmail.com>
2017-02-23 02:37:20 +0530

    TextElement for each line of text

--- stickynotification/src/main/java/com/anwesome/app/stickynotification/StickyNotificationTextElement.java
+++ stickynotification/src/main/java/com/anwesome/app/stickynotification/StickyNotificationTextElement.java
@@ -1,7 +1,26 @@
 package com.anwesome.app.stickynotification;
 
+import android.graphics.*;
 /**
  * Created by anweshmishra on 23/02/17.
  */
 public class StickyNotificationTextElement {
+    private float x,y,pivotX = 0,pivotY;
+    private String text;
+    public StickyNotificationTextElement(String text,float x,float y) {
+        this.text = text;
+        this.x = x;
+        this.y = y;
+    }
+    public void setPivots(float x,float y) {
+        this.pivotX = x;
+        this.pivotY = y;
+    }
+    public void draw(Canvas canvas, Paint paint) {
+        paint.setColor(Color.WHITE);
+        canvas.drawText(text,x-pivotX,y-pivotY,paint);
+    }
+    public int hashCode() {
+        return (int)y+text.hashCode();
+    }
 }
