8b8f4e9b3fcb56c00dd4e4099cf5d01406f7d1d4
Anwesh43 <anweshthecool0@gmail.com>
2017-02-23 07:02:48 +0530

    Adding extra mode -1

--- stickynotification/src/main/java/com/anwesome/app/stickynotification/AnimationStore.java
+++ stickynotification/src/main/java/com/anwesome/app/stickynotification/AnimationStore.java
@@ -4,7 +4,7 @@ package com.anwesome.app.stickynotification;
  * Created by anweshmishra on 23/02/17.
  */
 public class AnimationStore {
-    private int mode = 0;
+    private int mode = -1;
     public void setMode(int mode) {
         this.mode = mode;
     }
