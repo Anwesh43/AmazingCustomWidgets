a9f0c12df7cbd088e83d80717d463d2910af86e7
Anwesh43 <anweshthecool0@gmail.com>
2017-02-23 07:03:43 +0530

    Rotate animation of close button

--- stickynotification/src/main/java/com/anwesome/app/stickynotification/CloseButton.java
+++ stickynotification/src/main/java/com/anwesome/app/stickynotification/CloseButton.java
@@ -8,40 +8,42 @@ import android.graphics.Paint;
  * Created by anweshmishra on 23/02/17.
  */
 public class CloseButton {
-    private float x,y,r,deg = 0,scale = 1,scaleDir = 0,rotDir = 0;
+    private float x,y,r,deg = 0,dir = 1,rotDir = 0;
     public CloseButton(float x,float y,float r) {
         this.x = x;
         this.y = y;
         this.r = r;
     }
     public void draw(Canvas canvas, Paint paint) {
+        paint.setStrokeWidth(canvas.getWidth()/80);
         paint.setColor(Color.parseColor("#4FC3F7"));
         canvas.save();
         canvas.translate(x,y);
         canvas.rotate(deg);
-        canvas.scale(scale,scale);
         canvas.drawCircle(0,0,r,paint);
         paint.setColor(Color.WHITE);
-        canvas.drawLine(-r/2,0,0,r/2,paint);
+        canvas.drawLine(-r/2,0,r/2,0,paint);
         canvas.drawLine(0,-r/2,0,r/2,paint);
         canvas.restore();
     }
     public void update() {
-        scale+=scaleDir;
-        deg+=rotDir;
-        if(scale<=0) {
-            scaleDir = 0;
+        deg+=rotDir*dir;
+        if(deg>=45) {
             rotDir = 0;
+            dir = -1;
+        }
+        if(deg<=0) {
+            rotDir = 0;
+            dir = 1;
         }
     }
     public void startMoving() {
-        scaleDir = -0.2f;
-        rotDir = -36;
+        rotDir = 9;
     }
     public boolean handleTap(float x,float y) {
         return (x>=this.x-r && x<=this.x+r && y>=this.y-r && y<=this.y+r);
     }
     public boolean isStop() {
-        return (scaleDir == 0 && rotDir == 0);
+        return (rotDir == 0);
     }
 }
