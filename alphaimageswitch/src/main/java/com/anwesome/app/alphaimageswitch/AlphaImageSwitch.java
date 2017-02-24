39f71a86d8bfa10141054afbb8df836055b5bcad
Anwesh43 <anweshthecool0@gmail.com>
2017-02-24 09:11:44 +0530

    Drawing imageswitchbuttons

--- alphaimageswitch/src/main/java/com/anwesome/app/alphaimageswitch/AlphaImageSwitch.java
+++ alphaimageswitch/src/main/java/com/anwesome/app/alphaimageswitch/AlphaImageSwitch.java
@@ -3,6 +3,7 @@ package com.anwesome.app.alphaimageswitch;
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.*;
+import android.view.MotionEvent;
 import android.view.View;
 
 import java.util.*;
@@ -20,6 +21,21 @@ public class AlphaImageSwitch {
         this.buttons.add(imageSwitchButton);
     }
     public void show() {
+
+    }
+    private class AlphaImageSwitchView extends View {
+        private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        public AlphaImageSwitchView(Context context) {
+            super(context);
+        }
+        public void onDraw(Canvas canvas) {
+            for(AlphaImageSwitchButton imageSwitchButton:buttons) {
+                imageSwitchButton.draw(canvas,paint);
+            }
+        }
+        public boolean onTouchEvent(MotionEvent event) {
+            return true;
+        }
 
     }
 }
