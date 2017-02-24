147f2d012fc54eb8759af9d64c936e06dd559f65
Anwesh43 <anweshthecool0@gmail.com>
2017-02-24 09:01:44 +0530

    Adding alphaimageswitch to hold all buttons

--- alphaimageswitch/src/main/java/com/anwesome/app/alphaimageswitch/AlphaImageSwitch.java
+++ alphaimageswitch/src/main/java/com/anwesome/app/alphaimageswitch/AlphaImageSwitch.java
@@ -1,7 +1,25 @@
 package com.anwesome.app.alphaimageswitch;
 
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.*;
+import android.view.View;
+
+import java.util.*;
+
 /**
  * Created by anweshmishra on 24/02/17.
  */
 public class AlphaImageSwitch {
+    private Activity activity;
+    private List<AlphaImageSwitchButton> buttons = new ArrayList<>();
+    public AlphaImageSwitch(Activity activity) {
+        this.activity = activity;
+    }
+    public void addImageSwitchButton(AlphaImageSwitchButton imageSwitchButton) {
+        this.buttons.add(imageSwitchButton);
+    }
+    public void show() {
+
+    }
 }
