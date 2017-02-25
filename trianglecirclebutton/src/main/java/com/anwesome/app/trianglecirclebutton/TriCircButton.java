94a213883153a4f9fdf33302e1e26555a23539e8
Anwesh43 <anweshthecool0@gmail.com>
2017-02-26 05:28:27 +0530

    Positioning button and rotating to 360 degree

--- trianglecirclebutton/src/main/java/com/anwesome/app/trianglecirclebutton/TriCircButton.java
+++ trianglecirclebutton/src/main/java/com/anwesome/app/trianglecirclebutton/TriCircButton.java
@@ -20,38 +20,42 @@ public class TriCircButton {
     public static TriCircButton newInstance(){
         return new TriCircButton();
     }
+    public void unselect() {
+        selected = false;
+    }
     public void draw(Canvas canvas,Paint paint) {
-        paint.setColor(Color.parseColor("#E0E0E0"));
+        paint.setColor(Color.parseColor("#BDBDBD"));
         float x[] = {-size/2,size/2,0},y[] = {(float)Math.sqrt(3)*size/4,(float)Math.sqrt(3)*size/4,-(float)Math.sqrt(3)*size/4};
         canvas.save();
         canvas.translate(this.x,this.y);
         canvas.rotate(deg);
         for(int i=0;i<3;i++) {
-            canvas.drawCircle(x[i],y[i],size/8,paint);
+            canvas.drawCircle(x[i],y[i],size/2,paint);
         }
         canvas.restore();
         if(selected) {
-            float lineY = y[0]+this.y+size/7;
-            canvas.drawLine(0,lineY,size,lineY,paint);
+            paint.setStrokeWidth(size/15);
+            float lineY = y[0]+this.y+size/2;
+            canvas.drawLine(this.x-size/2,lineY,this.x+size/2,lineY,paint);
         }
     }
     public boolean stopped() {
-        boolean condition = deg%120 == 0;
+        boolean condition = deg%360 == 0;
         if(condition) {
-            deg -= deg%120;
+            deg -= deg%360;
         }
         return condition;
     }
     public boolean handleTap(float x,float y) {
-        float h = (float)Math.sqrt(3)*size/2;
-        boolean condition = x>=this.x-size/2 && x<=this.x+size/2 && y>=this.y-h/2 && y<=this.y+size/2;
+        float h = (float)(Math.sqrt(3)*size/2);
+        boolean condition = x>=this.x-size/2 && x<=this.x+size/2 && y>=this.y-h/2 && y<=this.y+h/2;
         if(condition) {
             selected = true;
         }
         return condition;
     }
     public void update() {
-        deg+=24;
+        deg+=72;
     }
     public int hashCode() {
         return (int)x+(int)deg;
