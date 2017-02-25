4b825b5270bd8743722e3703b3d93984864c3e28
Anwesh43 <anweshthecool0@gmail.com>
2017-02-26 03:12:39 +0530

    Removing TricCircButton

--- trianglecirclebutton/src/main/java/com/anwesome/app/trianglecirclebutton/TriCircButton.java
+++ trianglecirclebutton/src/main/java/com/anwesome/app/trianglecirclebutton/TriCircButton.java
@@ -1,15 +1,11 @@
 package com.anwesome.app.trianglecirclebutton;
 
-import android.content.Context;
+
+import android.graphics.*;
 
 /**
  * Created by anweshmishra on 26/02/17.
  */
 public class TriCircButton {
-    public TriCircButton(Context context) {
-        
-    }
-    public void show() {
-        
-    }
+    
 }
