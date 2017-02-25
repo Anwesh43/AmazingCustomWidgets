eb69524c3e9102209f169b40cd592db642a50064
Anwesh43 <anweshthecool0@gmail.com>
2017-02-26 03:37:57 +0530

    Handling tap event for each button and drawing each button

--- trianglecirclebutton/src/main/java/com/anwesome/app/trianglecirclebutton/TricSwitch.java
+++ trianglecirclebutton/src/main/java/com/anwesome/app/trianglecirclebutton/TricSwitch.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.view.MotionEvent;
 import android.view.View;
 
 import java.util.ArrayList;
@@ -22,17 +23,49 @@ public class TricSwitch {
         buttons.add(button);
     }
     public void show() {
-
     }
     private class TricSwitchView extends View {
         private boolean isAnimated = false;
-        private TriCircButton prevButton=null,currButton = null;
+        private TriCircButton currButton = null;
         private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
         public TricSwitchView(Context context) {
             super(context);
         }
         public void onDraw(Canvas canvas) {
+            for(TriCircButton triCircButton:buttons) {
+                triCircButton.draw(canvas,paint);
+            }
+            if(isAnimated) {
+                if(currButton!=null) {
+                    currButton.update();
+                    if (currButton.stopped()) {
+                        isAnimated = false;
+                        currButton = null;
+                    }
+                }
+                try {
+                    Thread.sleep(50);
+                    invalidate();
+                }
+                catch (Exception ex) {
 
+                }
+            }
+        }
+        public boolean onTouchEvent(MotionEvent event) {
+            if(event.getAction() == MotionEvent.ACTION_DOWN && !isAnimated && currButton == null) {
+                for(TriCircButton triCircButton:buttons) {
+                    if(triCircButton.handleTap(event.getX(),event.getY())) {
+                        currButton = triCircButton;
+                        break;
+                    }
+                    if(currButton!=null) {
+                        isAnimated = true;
+                        postInvalidate();
+                    }
+                }
+            }
+            return true;
         }
     }
 }
