e4965032d10022089005e1fdedb4350acbda0906
Anwesh43 <anweshthecool0@gmail.com>
2017-02-25 09:20:17 +0530

    Adding Animation Store

---
+++ lockbuttons/src/main/java/com/anwesome/app/lockbuttons/AnimationStore.java
@@ -0,0 +1,14 @@
+package com.anwesome.app.lockbuttons;
+
+/**
+ * Created by anweshmishra on 25/02/17.
+ */
+public class AnimationStore {
+    private int mode = 0;
+    public void setMode(int mode) {
+        this.mode = mode;
+    }
+    public int getMode() {
+        return mode;
+    }
+}
