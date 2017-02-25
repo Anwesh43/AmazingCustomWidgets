684acb1739341caae0afdaea0951b5fdc18c0d75
Anwesh43 <anweshthecool0@gmail.com>
2017-02-25 08:20:43 +0530

    Basic Framework for lock button

--- lockbuttons/src/main/java/com/anwesome/app/lockbuttons/LockButton.java
+++ lockbuttons/src/main/java/com/anwesome/app/lockbuttons/LockButton.java
@@ -1,8 +1,17 @@
 package com.anwesome.app.lockbuttons;
 
+import android.app.Activity;
+import android.content.Context;
+
 /**
  * Created by anweshmishra on 25/02/17.
  */
 public class LockButton {
-    
+    private Activity activity;
+    public LockButton(Activity activity){
+        this.activity = activity;
+    }
+    public void show() {
+        
+    }
 }
