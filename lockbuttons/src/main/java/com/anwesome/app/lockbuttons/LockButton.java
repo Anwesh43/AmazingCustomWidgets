e3329b440d93130c8dd1d34b3b11b55b0de397f4
Anwesh43 <anweshthecool0@gmail.com>
2017-02-25 09:22:10 +0530

    Using Utils and controller to do the drawing and animation part of the lock and handling tap event

--- lockbuttons/src/main/java/com/anwesome/app/lockbuttons/LockButton.java
+++ lockbuttons/src/main/java/com/anwesome/app/lockbuttons/LockButton.java
@@ -3,6 +3,7 @@ package com.anwesome.app.lockbuttons;
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.*;
+import android.view.MotionEvent;
 import android.view.View;
 
 /**
@@ -25,42 +26,21 @@ public class LockButton {
         private Lock lock = null;
         private LockKey lockKey = null;
         private int w,h;
+        private AnimationController animationController;
         public LockButtonView(Context context) {
             super(context);
         }
         public void onDraw(Canvas canvas) {
             if(time == 0) {
                 initLockAndKey(canvas.getWidth(),canvas.getHeight());
+                animationController = new AnimationController(lock,lockKey,this);
             }
+            drawObjects(canvas,paint);
             time++;
-            if(isAnimated) {
-                try {
-                    Thread.sleep(50);
-                    invalidate();
-                }
-                catch(Exception ex) {
-
-                }
-            }
+            animationController.animate();
         }
         private void drawObjects(Canvas canvas,Paint paint) {
-            lock.draw(canvas,paint);
-            paint.setStyle(Paint.Style.FILL);
-            paint.setColor(Color.parseColor("#64B5F6"));
-            switch(lockButtonType) {
-                case RECTANGLE:
-                    canvas.drawRect(new RectF(w/2,h/2-w/2,w,h/2+w/2),paint);
-                    break;
-                case ROUNDRECTANGLE:
-                    canvas.drawRoundRect(new RectF(w/2,h/2-w/2,w,h/2+w/2),w/16,w/16,paint);
-                    break;
-                case CIRCLE:
-                    canvas.drawCircle(3*w/4,h/2,w/4,paint);
-                    break;
-                default:
-                    break;
-            }
-            lockKey.draw(canvas,paint);
+            LockDrawUtil.drawObjects(canvas,paint,lock,lockKey,lockButtonType,w,h);
         }
         private void initLockAndKey(int w,int h) {
             this.w = w;
@@ -68,6 +48,12 @@ public class LockButton {
             lockKey = new LockKey(3*w/4,h/2);
             float x = 3*w/4+(w/4)*(float)Math.cos(240*Math.PI/180),y = h/2+(w/4)*(float)Math.sin(240*Math.PI/180),lockR = Math.abs(x-3*w/4);
             lock = new Lock(x,y,lockR);
+        }
+        public boolean onTouchEvent(MotionEvent event) {
+            if(event.getAction() == MotionEvent.ACTION_DOWN &&  (animationController!=null && !animationController.isAnimated())) {
+                animationController.startAnimating();
+            }
+            return true;
         }
     }
 }
