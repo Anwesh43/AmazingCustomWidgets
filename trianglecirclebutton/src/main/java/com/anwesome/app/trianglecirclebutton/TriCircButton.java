e104f600066c09bd2e1e7cddd52e52ee41c25729
Anwesh43 <anweshthecool0@gmail.com>
2017-02-26 03:09:37 +0530

    Adding tricircbutton and settings

---
+++ trianglecirclebutton/src/main/java/com/anwesome/app/trianglecirclebutton/TriCircButton.java
@@ -0,0 +1,15 @@
+package com.anwesome.app.trianglecirclebutton;
+
+import android.content.Context;
+
+/**
+ * Created by anweshmishra on 26/02/17.
+ */
+public class TriCircButton {
+    public TriCircButton(Context context) {
+        
+    }
+    public void show() {
+        
+    }
+}
