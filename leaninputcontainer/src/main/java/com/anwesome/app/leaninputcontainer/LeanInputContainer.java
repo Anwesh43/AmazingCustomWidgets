d99d3201012bf8cfa9bc0e0605cdd61da1f87860
Anwesh43 <anweshthecool0@gmail.com>
2017-02-27 00:18:52 +0530

    Adding LeanInputContainer class

---
+++ leaninputcontainer/src/main/java/com/anwesome/app/leaninputcontainer/LeanInputContainer.java
@@ -0,0 +1,15 @@
+package com.anwesome.app.leaninputcontainer;
+
+import android.app.Activity;
+
+/**
+ * Created by anweshmishra on 27/02/17.
+ */
+public class LeanInputContainer {
+    public LeanInputContainer(Activity activity) {
+
+    }
+    public void show() {
+        
+    }
+}
