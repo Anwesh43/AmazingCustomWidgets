f208cb8d25c952917c04e5085c258d9083d0d6a2
Anwesh43 <anweshthecool0@gmail.com>
2017-02-27 01:00:23 +0530

    Showing LeanInputContainerView

--- leaninputcontainer/src/main/java/com/anwesome/app/leaninputcontainer/LeanInputContainer.java
+++ leaninputcontainer/src/main/java/com/anwesome/app/leaninputcontainer/LeanInputContainer.java
@@ -5,17 +5,22 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by anweshmishra on 27/02/17.
  */
 public class LeanInputContainer {
     private Activity activity;
+    private LeanInputContainerView leanInputContainerView;
     public LeanInputContainer(Activity activity) {
         this.activity = activity;
     }
     public void show() {
-
+        if(leanInputContainerView == null) {
+            leanInputContainerView = new LeanInputContainerView(activity);
+            this.activity.addContentView(leanInputContainerView,new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        }
     }
     private class LeanInputContainerView extends View {
         private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
