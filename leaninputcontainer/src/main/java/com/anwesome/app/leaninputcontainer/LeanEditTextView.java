dff6b823b03c2408cc9bbd7ba809493b83b3594b
Anwesh43 <anweshthecool0@gmail.com>
2017-02-27 01:23:53 +0530

    Adding LeanEditTextView to show the text input

---
+++ leaninputcontainer/src/main/java/com/anwesome/app/leaninputcontainer/LeanEditTextView.java
@@ -0,0 +1,31 @@
+package com.anwesome.app.leaninputcontainer;
+
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+
+/**
+ * Created by anweshmishra on 27/02/17.
+ */
+public class LeanEditTextView {
+    private float x,y,size;
+    private String text = "";
+    public LeanEditTextView(float x,float y,float size) {
+        this.x = x;
+        this.y = y;
+        this.size = size;
+    }
+    public void draw(Canvas canvas, Paint paint) {
+        paint.setColor(Color.BLACK);
+        paint.setTextSize(size/2);
+        canvas.drawText(text,x-paint.measureText(text)/2,y,paint);
+        paint.setColor(Color.parseColor("#FF6F00"));
+        canvas.drawLine(x-size/2,y+size/2,x+size/2,y+size/2,paint);
+    }
+    public void addText(char letter) {
+        text+=letter;
+    }
+    public String getText() {
+        return text;
+    }
+}
