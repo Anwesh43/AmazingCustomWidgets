bc5bdd2e89adcb9677281d3af992eef181c5f6e0
Anwesh43 <anweshthecool0@gmail.com>
2017-02-28 01:36:27 +0530

    Adding mirrorimagebutton in activity

--- mirrorimagebutton/src/main/java/com/anwesome/app/mirrorimagebutton/MirrorImageButton.java
+++ mirrorimagebutton/src/main/java/com/anwesome/app/mirrorimagebutton/MirrorImageButton.java
@@ -4,23 +4,31 @@ import android.app.Activity;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by anweshmishra on 28/02/17.
  */
 public class MirrorImageButton {
     private Activity activity;
+    private MirrorImageButtonView mirrorImageButtonView;
     public MirrorImageButton(Activity activity) {
         this.activity = activity;
     }
-    public void show() {
-
+    public void show(int x,int y) {
+        if(mirrorImageButtonView == null) {
+            mirrorImageButtonView = new MirrorImageButtonView(activity);
+            activity.addContentView(mirrorImageButtonView,new ViewGroup.LayoutParams(400,400));
+        }
+        mirrorImageButtonView.setX(x);
+        mirrorImageButtonView.setY(y);
     }
     private class MirrorImageButtonView extends View {
         public MirrorImageButtonView(Context context) {
             super(context);
         }
         public void onDraw(Canvas canvas) {
+
         }
     }
 }
