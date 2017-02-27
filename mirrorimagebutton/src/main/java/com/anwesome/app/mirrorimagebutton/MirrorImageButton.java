8676d3944c08f6a6455d270be5af8d95f43a5010
Anwesh43 <anweshthecool0@gmail.com>
2017-02-28 02:08:59 +0530

    Drawing MirrorImageButtonView and handling Tap event

--- mirrorimagebutton/src/main/java/com/anwesome/app/mirrorimagebutton/MirrorImageButton.java
+++ mirrorimagebutton/src/main/java/com/anwesome/app/mirrorimagebutton/MirrorImageButton.java
@@ -2,7 +2,10 @@ package com.anwesome.app.mirrorimagebutton;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -12,8 +15,10 @@ import android.view.ViewGroup;
 public class MirrorImageButton {
     private Activity activity;
     private MirrorImageButtonView mirrorImageButtonView;
-    public MirrorImageButton(Activity activity) {
+    private Bitmap bitmap;
+    public MirrorImageButton(Activity activity,Bitmap bitmap) {
         this.activity = activity;
+        this.bitmap = bitmap;
     }
     public void show(int x,int y) {
         if(mirrorImageButtonView == null) {
@@ -24,11 +29,35 @@ public class MirrorImageButton {
         mirrorImageButtonView.setY(y);
     }
     private class MirrorImageButtonView extends View {
+        private boolean isAnimated = false;
+        private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        private MirroImageRenderer mirroImageRenderer;
         public MirrorImageButtonView(Context context) {
             super(context);
+            mirroImageRenderer = new MirroImageRenderer(bitmap);
         }
         public void onDraw(Canvas canvas) {
+            mirroImageRenderer.render(canvas,paint);
+            if(isAnimated) {
+                mirroImageRenderer.update();
+                if(mirroImageRenderer.isStop()) {
+                    isAnimated = false;
+                }
+                try {
+                    Thread.sleep(50);
+                    invalidate();
+                }
+                catch (Exception ex) {
 
+                }
+            }
+        }
+        public boolean onTouchEvent(MotionEvent event) {
+            if(event.getAction() == MotionEvent.ACTION_DOWN && !isAnimated) {
+                isAnimated = true;
+                postInvalidate();
+            }
+            return true;
         }
     }
 }
