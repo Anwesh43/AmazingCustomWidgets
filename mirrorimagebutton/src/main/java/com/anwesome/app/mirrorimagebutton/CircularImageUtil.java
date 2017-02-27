4b6a8326dbb30952d71e4fc8f8421336cbf50d36
Anwesh43 <anweshthecool0@gmail.com>
2017-02-28 02:07:57 +0530

    Util to draw circular image

---
+++ mirrorimagebutton/src/main/java/com/anwesome/app/mirrorimagebutton/CircularImageUtil.java
@@ -0,0 +1,20 @@
+package com.anwesome.app.mirrorimagebutton;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
+
+/**
+ * Created by anweshmishra on 28/02/17.
+ */
+public class CircularImageUtil {
+    public static void drawImage(Canvas canvas, Paint paint,Bitmap bitmap, float x, float y, float r) {
+        canvas.save();
+        Path path = new Path();
+        path.addCircle(x,y,r, Path.Direction.CCW);
+        canvas.clipPath(path);
+        canvas.drawBitmap(bitmap,x-r/2,y-r/2,paint);
+        canvas.restore();
+    }
+}
---
+++ mirrorimagebutton/src/main/java/com/anwesome/app/mirrorimagebutton/MirroImageRenderer.java
@@ -0,0 +1,7 @@
+package com.anwesome.app.mirrorimagebutton;
+
+/**
+ * Created by anweshmishra on 28/02/17.
+ */
+public class MirroImageRenderer {
+}
