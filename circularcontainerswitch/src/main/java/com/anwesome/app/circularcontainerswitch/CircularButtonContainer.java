12dfc91727c0f22aac276707b1ca6811dae4c54a
Anwesh43 <anweshthecool0@gmail.com>
2017-03-01 07:03:58 +0530

    Show method implementation and adding container view

--- circularcontainerswitch/src/main/java/com/anwesome/app/circularcontainerswitch/CircularButtonContainer.java
+++ circularcontainerswitch/src/main/java/com/anwesome/app/circularcontainerswitch/CircularButtonContainer.java
@@ -11,7 +11,9 @@ import java.util.*;
  */
 public class CircularButtonContainer {
     private Activity activity;
+    private float gap;
     private List<CircularButton> circularButtons = new ArrayList<>();
+    private CircularButtonContainerView circularButtonContainerView = null;
     public CircularButtonContainer(Activity activity) {
         this.activity = activity;
     }
@@ -20,7 +22,19 @@ public class CircularButtonContainer {
         circularButtons.add(circularButton);
     }
     public void show() {
-
+        if(circularButtonContainerView == null) {
+            if(circularButtons.size() > 0) {
+                gap = 360/(circularButtons.size());
+                float deg = 0;
+                for(CircularButton circularButton:circularButtons) {
+                    circularButton.setDeg(deg);
+                    deg+=gap;
+                }
+                circularButtons.get(0).setSelected(true);
+            }
+            circularButtonContainerView = new CircularButtonContainerView(activity);
+            activity.addContentView(circularButtonContainerView,new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        }
     }
     private class CircularButtonContainerView extends View {
         public CircularButtonContainerView(Context context) {
