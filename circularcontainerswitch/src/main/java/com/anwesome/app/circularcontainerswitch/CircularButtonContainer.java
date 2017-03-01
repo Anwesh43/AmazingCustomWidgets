9e4abdad1abb609925a25dfc51794cceae2320e9
Anwesh43 <anweshthecool0@gmail.com>
2017-03-01 06:41:52 +0530

    Adding circularbuttoncontainer to hold circular buttons

---
+++ circularcontainerswitch/src/main/java/com/anwesome/app/circularcontainerswitch/CircularButtonContainer.java
@@ -0,0 +1,16 @@
+package com.anwesome.app.circularcontainerswitch;
+
+import android.app.Activity;
+
+/**
+ * Created by anweshmishra on 01/03/17.
+ */
+public class CircularButtonContainer {
+    private Activity activity;
+    public CircularButtonContainer(Activity activity) {
+        this.activity = activity;
+    }
+    public void show() {
+
+    }
+}
