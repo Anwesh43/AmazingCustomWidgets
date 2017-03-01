37315b05788a4e3dabae9c8e08fde3267f076ce7
Anwesh43 <anweshthecool0@gmail.com>
2017-03-01 07:04:51 +0530

    showing red color on selected

--- circularcontainerswitch/src/main/java/com/anwesome/app/circularcontainerswitch/CircularButton.java
+++ circularcontainerswitch/src/main/java/com/anwesome/app/circularcontainerswitch/CircularButton.java
@@ -2,7 +2,9 @@ package com.anwesome.app.circularcontainerswitch;
 
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
 
 /**
  * Created by anweshmishra on 01/03/17.
@@ -10,11 +12,18 @@ import android.graphics.Paint;
 public class CircularButton {
     private Bitmap bitmap;
     private char tag;
+    private boolean selected = false;
     private int render = 0;
     private float deg = 0;
     private CircularButton(Bitmap bitmap,char tag) {
         this.bitmap = bitmap;
         this.tag = tag;
+    }
+    public void setSelected(boolean selected) {
+        this.selected = selected;
+    }
+    public boolean isSelected() {
+        return selected;
     }
     public void setDeg(float deg) {
         this.deg = deg;
@@ -23,11 +32,14 @@ public class CircularButton {
         if(render == 0) {
             bitmap = Bitmap.createScaledBitmap(bitmap,canvas.getWidth()/9,canvas.getWidth()/9,true);
         }
-        canvas.save();
-        canvas.translate(canvas.getWidth()/3,0);
-        canvas.rotate(deg);
-        canvas.drawBitmap(bitmap,0,0,paint);
-        canvas.restore();
+        Path path = new Path();
+        path.addCircle(canvas.getWidth()/3,0,canvas.getWidth()/18,Path.Direction.CCW);
+        canvas.clipPath(path);
+        canvas.drawBitmap(bitmap,5*canvas.getWidth()/18,-canvas.getWidth()/18,paint);
+        if(selected) {
+            paint.setColor(Color.parseColor("#AAFF0000"));
+            canvas.drawPath(path, paint);
+        }
         render++;
     }
     public float getDeg() {
