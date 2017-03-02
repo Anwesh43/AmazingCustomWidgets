3ebc762a6e4e78dc5cf69c949e40a434291ad248
Anwesh43 <anweshthecool0@gmail.com>
2017-03-02 09:45:12 +0530

    Adding individual bargraphs

---
+++ bargraphbutton/src/main/java/com/anwesome/app/bargraphbutton/BarGraph.java
@@ -0,0 +1,53 @@
+package com.anwesome.app.bargraphbutton;
+
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+
+/**
+ * Created by anweshmishra on 02/03/17.
+ */
+public class BarGraph {
+    private float x,y,w,h,scale = 0.2f,dir = 0;
+    private int color = Color.parseColor("#009688");
+    private BarGraph(float x,float y,float w,float h) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+    }
+    public static BarGraph newInstance(float x,float y,float w,float h) {
+        return new BarGraph(x,y,w,h);
+    }
+    public void draw(Canvas canvas, Paint paint) {
+        paint.setColor(color);
+        canvas.save();
+        canvas.translate(x+w/2,y);
+        canvas.scale(scale,scale);
+        canvas.drawRect(new RectF(-w/2,-h,w/2,0),paint);
+        canvas.restore();
+    }
+    public void update() {
+        scale+=0.1f*dir;
+        if(scale>=1 || scale<=0.2f) {
+            dir = 0;
+            if(scale>=1) {
+                scale = 1;
+            }
+            if(scale<=0.2f) {
+                scale = 0.2f;
+            }
+        }
+    }
+    public int hashCode() {
+        return (int)x+(int)y;
+    }
+    public boolean handleTap(float x,float y) {
+        boolean condition = false;
+        if(!((scale >= 1)|| dir != 0)) {
+            condition = x>=this.x && x<=this.x+w && y>=this.y-h*0.2f && y<=this.y;
+        }
+        return condition;
+    }
+}
