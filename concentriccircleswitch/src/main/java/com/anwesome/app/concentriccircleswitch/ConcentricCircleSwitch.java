7b354054d482553b0c8e382f07c84f78dd1feb7f
Anwesh43 <anweshthecool0@gmail.com>
2017-03-03 00:50:36 +0530

    Adding View

--- concentriccircleswitch/src/main/java/com/anwesome/app/concentriccircleswitch/ConcentricCircleSwitch.java
+++ concentriccircleswitch/src/main/java/com/anwesome/app/concentriccircleswitch/ConcentricCircleSwitch.java
@@ -1,6 +1,10 @@
 package com.anwesome.app.concentriccircleswitch;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.MotionEvent;
+import android.view.View;
 
 /**
  * Created by anweshmishra on 03/03/17.
@@ -11,6 +15,17 @@ public class ConcentricCircleSwitch {
         this.activity = activity;
     }
     public void show() {
-        
+
+    }
+    private class ConcentricCircleView extends View {
+        public ConcentricCircleView(Context context) {
+            super(context);
+        }
+        public void onDraw(Canvas canvas) {
+
+        }
+        public boolean onTouchEvent(MotionEvent event) {
+            return true;
+        }
     }
 }
