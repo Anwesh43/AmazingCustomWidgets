0ba431a0d1b4b2167a00b54f1986042b1e40ad48
Anwesh43 <anweshthecool0@gmail.com>
2017-03-03 00:48:35 +0530

    ConcentricCircleSwitch class

---
+++ concentriccircleswitch/src/main/java/com/anwesome/app/concentriccircleswitch/ConcentricCircleSwitch.java
@@ -0,0 +1,16 @@
+package com.anwesome.app.concentriccircleswitch;
+
+import android.app.Activity;
+
+/**
+ * Created by anweshmishra on 03/03/17.
+ */
+public class ConcentricCircleSwitch {
+    private Activity activity;
+    public ConcentricCircleSwitch(Activity activity) {
+        this.activity = activity;
+    }
+    public void show() {
+        
+    }
+}
