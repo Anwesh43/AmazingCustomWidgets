006afaf0f5712a14bfd988a718d4490da2c4781f
Anwesh43 <anweshthecool0@gmail.com>
2017-03-04 03:02:17 +0530

    Adding WidHolderView

---
+++ widholder/src/main/java/com/anwesome/games/widholder/WidHolder.java
@@ -0,0 +1,34 @@
+package com.anwesome.games.widholder;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.*;
+import android.view.*;
+/**
+ * Created by anweshmishra on 04/03/17.
+ */
+public class WidHolder {
+    private Activity activity;
+    private WidHolderView view;
+    public WidHolder(Activity activity) {
+        this.activity = activity;
+    }
+    public void show() {
+        if(view == null) {
+            view = new WidHolderView(activity);
+            activity.addContentView(view,new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        }
+    }
+    private class WidHolderView extends View {
+        public WidHolderView(Context context) {
+            super(context);
+        }
+        public void onDraw(Canvas canvas) {
+            
+        }
+        public boolean onTouchEvent(MotionEvent event) {
+
+            return true;
+        }
+    }
+}
