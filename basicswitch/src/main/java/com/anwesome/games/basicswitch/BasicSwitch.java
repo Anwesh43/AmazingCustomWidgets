a72a015fe849b74f88e86de91084ead33d737db0
Anwesh43 <anweshthecool0@gmail.com>
2017-03-06 12:03:50 +0530

    Drawing switchbuttons,animating it and handling tap

--- basicswitch/src/main/java/com/anwesome/games/basicswitch/BasicSwitch.java
+++ basicswitch/src/main/java/com/anwesome/games/basicswitch/BasicSwitch.java
@@ -3,6 +3,7 @@ package com.anwesome.games.basicswitch;
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
@@ -32,13 +33,60 @@ public class BasicSwitch {
         }
     }
     private class BasicSwitchView extends View{
+        private boolean isAnimated = false;
+        private SwitchObject currObject=null,prevObject=null;
+        private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
         public BasicSwitchView(Context context) {
             super(context);
         }
+        private void stopAnimating() {
+            prevObject = currObject;
+            currObject = null;
+            isAnimated = false;
+        }
         public void onDraw(Canvas canvas) {
+            for(SwitchObject switchObject:switchObjects) {
+                switchObject.draw(canvas,paint);
+            }
+            if(isAnimated) {
+                if(currObject!=null) {
+                    currObject.update();
+                    if(prevObject!=null) {
+                        prevObject.update();
+                        if(prevObject.stoppedAnimating() && currObject.stoppedAnimating()) {
+                            stopAnimating();
+                        }
+                    }
+                    if(currObject.stoppedAnimating()) {
+                        stopAnimating();
+                    }
+                }
+                try {
+                    Thread.sleep(50);
+                    invalidate();
+                }
+                catch (Exception ex) {
 
+                }
+            }
         }
         public boolean onTouchEvent(MotionEvent event) {
+            if(event.getAction() == MotionEvent.ACTION_DOWN && !isAnimated && currObject==null) {
+                for(SwitchObject switchObject:switchObjects) {
+                    if(switchObject.handleTap(event.getX(),event.getY())) {
+                        currObject = switchObject;
+                        break;
+                    }
+                }
+                if(currObject!=null) {
+                    currObject.select();
+                    if(prevObject!=null) {
+                        prevObject.unselect();
+                    }
+                    isAnimated = true;
+                    postInvalidate();
+                }
+            }
             return true;
         }
 
