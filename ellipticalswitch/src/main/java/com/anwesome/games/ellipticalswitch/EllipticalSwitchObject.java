495afcd9c0ed08e9e3867cc90648b32fe0f178ed
Anwesh43 <anweshthecool0@gmail.com>
2017-03-06 12:16:05 +0530

    Adding EllipticalSwitchObject for drawing buttons of elliptical size

--- ellipticalswitch/src/main/java/com/anwesome/games/ellipticalswitch/EllipticalSwitchObject.java
+++ ellipticalswitch/src/main/java/com/anwesome/games/ellipticalswitch/EllipticalSwitchObject.java
@@ -1,7 +1,37 @@
 package com.anwesome.games.ellipticalswitch;
 
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.RectF;
+
+import com.anwesome.games.basicswitch.OnSelectedListener;
+import com.anwesome.games.basicswitch.SwitchObject;
+
 /**
  * Created by anweshmishra on 06/03/17.
  */
-public class EllipticalSwitchObject {
+public class EllipticalSwitchObject extends SwitchObject {
+    private float scale=1,rot = 0;
+    public EllipticalSwitchObject(OnSelectedListener onSelectedListener) {
+        super(onSelectedListener);
+    }
+    public void drawObject(Canvas canvas, Paint paint) {
+        canvas.scale(scale,scale);
+        canvas.rotate(rot);
+        canvas.drawArc(new RectF(-size/2,-size/4,size/2,size/4),0,360,true,paint);
+    }
+    public void update() {
+        scale+=0.1f*dir;
+        rot+=18*dir;
+        if(scale>=1.5f) {
+            dir = 0;
+            rot = 90;
+            scale = 1.5f;
+        }
+        if(scale<=1.0f) {
+            dir = 0;
+            rot = 0;
+            scale = 1.0f;
+        }
+    }
 }
