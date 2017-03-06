230f72b3135f76c08b6a4e3dbb337f943ad07db0
Anwesh43 <anweshthecool0@gmail.com>
2017-03-06 12:20:36 +0530

    Elliptical Switch

--- ellipticalswitch/src/main/java/com/anwesome/games/ellipticalswitch/EllipticalSwitch.java
+++ ellipticalswitch/src/main/java/com/anwesome/games/ellipticalswitch/EllipticalSwitch.java
@@ -1,7 +1,19 @@
 package com.anwesome.games.ellipticalswitch;
 
+import android.app.Activity;
+
+import com.anwesome.games.basicswitch.BasicSwitch;
+import com.anwesome.games.basicswitch.OnSelectedListener;
+import com.anwesome.games.basicswitch.SwitchObject;
+
 /**
  * Created by anweshmishra on 06/03/17.
  */
-public class EllipticalSwitch {
+public class EllipticalSwitch extends BasicSwitch {
+    public EllipticalSwitch(Activity activity) {
+        super(activity);
+    }
+    public SwitchObject getSwitchObject(OnSelectedListener onSelectedListener) {
+        return new EllipticalSwitchObject(onSelectedListener);
+    }
 }
