cad6a03a7a3a5644c3a50b43f9cdfb46555a4f81
Anwesh43 <anweshthecool0@gmail.com>
2017-03-07 01:23:37 +0530

    Adding MultipleCheckBox methods

--- multiplecheckbox/src/main/java/com/anwesome/games/multiplecheckbox/MultipleCheckBox.java
+++ multiplecheckbox/src/main/java/com/anwesome/games/multiplecheckbox/MultipleCheckBox.java
@@ -1,7 +1,19 @@
 package com.anwesome.games.multiplecheckbox;
 
+import android.app.Activity;
+
 /**
  * Created by anweshmishra on 07/03/17.
  */
 public class MultipleCheckBox {
+    private Activity activity;
+    public MultipleCheckBox(Activity activity) {
+        this.activity = activity;
+    }
+    public void show() {
+
+    }
+    public void addCheckBox(String text) {
+
+    }
 }
