6eedb9d19b269f9b6b3eecc6ba1223fa6b0406b5
Anwesh43 <anweshthecool0@gmail.com>
2017-03-08 00:41:59 +0530

    Adding show method for CircledArrowButton class

---
+++ circledarrowbutton/src/main/java/com/anwesome/games/circledarrowbutton/CircledArrowButton.java
@@ -0,0 +1,17 @@
+package com.anwesome.games.circledarrowbutton;
+
+import android.app.Activity;
+import android.content.Context;
+
+/**
+ * Created by anweshmishra on 08/03/17.
+ */
+public class CircledArrowButton {
+    private Activity activity;
+    public CircledArrowButton(Activity activity) {
+        this.activity = activity;
+    }
+    public void show() {
+
+    }
+}
