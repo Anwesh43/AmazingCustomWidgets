a8e02342c6a279d7874faefe8efff4608fc4a694
Anwesh43 <anweshthecool0@gmail.com>
2017-03-08 00:59:12 +0530

    Adding view

--- circledarrowbutton/src/main/java/com/anwesome/games/circledarrowbutton/CircledArrowButton.java
+++ circledarrowbutton/src/main/java/com/anwesome/games/circledarrowbutton/CircledArrowButton.java
@@ -2,6 +2,10 @@ package com.anwesome.games.circledarrowbutton;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.view.MotionEvent;
+import android.view.View;
 
 /**
  * Created by anweshmishra on 08/03/17.
@@ -13,5 +17,21 @@ public class CircledArrowButton {
     }
     public void show() {
 
+    }
+    private class CircledArrowView extends View {
+        private boolean isAnimated = false;
+        private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        public CircledArrowView(Context context) {
+            super(context);
+        }
+        public void onDraw(Canvas canvas) {
+
+        }
+        public boolean onTouchEvent(MotionEvent event) {
+            if(event.getAction() == MotionEvent.ACTION_DOWN && !isAnimated) {
+
+            }
+            return true;
+        }
     }
 }
