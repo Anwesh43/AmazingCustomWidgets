1fe9604e8713d0b6ac653ba054aba65f278aa321
Anwesh43 <anweshthecool0@gmail.com>
2017-03-08 04:21:31 +0530

    Adding ToggleSelectionListener

---
+++ circledarrowbutton/src/main/java/com/anwesome/games/circledarrowbutton/ToggleSelectionListener.java
@@ -0,0 +1,9 @@
+package com.anwesome.games.circledarrowbutton;
+
+/**
+ * Created by anweshmishra on 08/03/17.
+ */
+public interface ToggleSelectionListener {
+    void onSelected();
+    void onUnselected();
+}
