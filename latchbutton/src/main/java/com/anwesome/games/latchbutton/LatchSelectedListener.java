e26dd702991ed0fcc6134401c3fdb2842799e8f0
Anwesh43 <anweshthecool0@gmail.com>
2017-03-09 02:12:35 +0530

    Adding LatchSelectedListener for latchbutton

---
+++ latchbutton/src/main/java/com/anwesome/games/latchbutton/LatchSelectedListener.java
@@ -0,0 +1,9 @@
+package com.anwesome.games.latchbutton;
+
+/**
+ * Created by anweshmishra on 09/03/17.
+ */
+public interface LatchSelectedListener {
+    void onSelected();
+    void onUnSelected();
+}
