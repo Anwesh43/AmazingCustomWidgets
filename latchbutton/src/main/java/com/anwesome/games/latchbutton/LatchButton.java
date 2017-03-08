3e6139f264dac520e4384aa23902b2d3bccb338b
Anwesh43 <anweshthecool0@gmail.com>
2017-03-09 00:58:35 +0530

    Adding latchbutton view drawing method and handle tap

--- latchbutton/src/main/java/com/anwesome/games/latchbutton/LatchButton.java
+++ latchbutton/src/main/java/com/anwesome/games/latchbutton/LatchButton.java
@@ -1,6 +1,10 @@
 package com.anwesome.games.latchbutton;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.MotionEvent;
+import android.view.View;
 
 /**
  * Created by anweshmishra on 09/03/17.
@@ -15,5 +19,29 @@ public class LatchButton {
     }
     public void show() {
 
+    }
+    private class LatchButtonView extends View {
+        private boolean isAnimated = false;
+        public LatchButtonView(Context context) {
+            super(context);
+        }
+        public void onDraw(Canvas canvas) {
+            if(isAnimated) {
+                try {
+                    Thread.sleep(50);
+                    invalidate();
+                }
+                catch(Exception ex) {
+
+                }
+            }
+        }
+        public boolean onTouchEvent(MotionEvent event) {
+            if(event.getAction() == MotionEvent.ACTION_DOWN && !isAnimated) {
+                isAnimated = true;
+                postInvalidate();
+            }
+            return true;
+        }
     }
 }
