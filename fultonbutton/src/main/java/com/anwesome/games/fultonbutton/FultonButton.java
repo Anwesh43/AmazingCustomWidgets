d56827d05ed14fd2e68b23e6568dbd617ec3c40a
Anwesh43 <anweshthecool0@gmail.com>
2017-03-10 02:26:01 +0530

    implementing fultonbutton show method

--- fultonbutton/src/main/java/com/anwesome/games/fultonbutton/FultonButton.java
+++ fultonbutton/src/main/java/com/anwesome/games/fultonbutton/FultonButton.java
@@ -4,8 +4,12 @@ import android.app.Activity;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Point;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+
+import com.anwesome.ui.dimensionsutil.DimensionsUtil;
 
 /**
  * Created by anweshmishra on 10/03/17.
@@ -17,10 +21,16 @@ public class FultonButton {
     private FultonButton(Activity activity) {
         this.activity = activity;
     }
-    public void show() {
+    public void show(int x,int y) {
         if(fultonButtonView == null) {
-
+            Point size = DimensionsUtil.getDeviceDimension(activity);
+            int w = size.x/3;
+            fultonShape = FultonShape.getInstance(w,w);
+            fultonButtonView = new FultonButtonView(activity);
+            activity.addContentView(fultonButtonView,new ViewGroup.LayoutParams(w,w));
         }
+        fultonButtonView.setX(x);
+        fultonButtonView.setY(y);
     }
     private class FultonButtonView extends View {
         private boolean isAnimated = false;
