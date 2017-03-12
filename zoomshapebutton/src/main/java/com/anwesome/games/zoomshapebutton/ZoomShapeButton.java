cb1735bdc66029e25ea79da2b5a6c754308b7cb6
Anwesh43 <anweshthecool0@gmail.com>
2017-03-13 04:03:18 +0530

    Adding Zoomshapebutton show and constructor

--- zoomshapebutton/src/main/java/com/anwesome/games/zoomshapebutton/ZoomShapeButton.java
+++ zoomshapebutton/src/main/java/com/anwesome/games/zoomshapebutton/ZoomShapeButton.java
@@ -1,7 +1,16 @@
 package com.anwesome.games.zoomshapebutton;
 
+import android.app.Activity;
+
 /**
  * Created by anweshmishra on 13/03/17.
  */
 public class ZoomShapeButton {
+    private Activity activity;
+    public ZoomShapeButton(Activity activity) {
+       this.activity = activity;
+    }
+    public void show() {
+
+    }
 }
