b37b43ecd4dc38bece3407731cee181d4705ec73
Anwesh43 <anweshthecool0@gmail.com>
2017-03-15 07:48:56 +0530

    Adding PlayPause class

---
+++ playpause/src/main/java/com/anwesome/games/playpause/PlayPause.java
@@ -0,0 +1,16 @@
+package com.anwesome.games.playpause;
+
+import android.app.Activity;
+
+/**
+ * Created by anweshmishra on 15/03/17.
+ */
+public class PlayPause {
+    private Activity activity;
+    public PlayPause(Activity activity) {
+        this.activity = activity;
+    }
+    public void show() {
+
+    }
+}
