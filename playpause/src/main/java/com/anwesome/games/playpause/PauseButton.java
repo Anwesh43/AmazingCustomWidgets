dc1a5b52f32cb7cd47dec3e61e6822365c254309
Anwesh43 <anweshthecool0@gmail.com>
2017-03-15 08:04:40 +0530

    Addin pausebutton to draw pause ui

--- playpause/src/main/java/com/anwesome/games/playpause/PauseButton.java
+++ playpause/src/main/java/com/anwesome/games/playpause/PauseButton.java
@@ -2,14 +2,13 @@ package com.anwesome.games.playpause;
 
 import android.graphics.Canvas;
 import android.graphics.Paint;
-import android.graphics.Path;
 
 /**
  * Created by anweshmishra on 15/03/17.
  */
-public class PlayButton {
-    private float x,y,w,h,deg=0,scale=1f,dir = 0;
-    public PlayButton(float x,float y,float w,float h) {
+public class PauseButton {
+    private float x,y,w,h,scale=0,dir = 0,deg = 0;
+    public PauseButton(float x,float y,float w,float h) {
         this.x = x;
         this.y = y;
         this.w = w;
@@ -23,24 +22,18 @@ public class PlayButton {
         canvas.translate(x,y);
         canvas.rotate(deg);
         canvas.scale(scale,scale);
-        Path path = new Path();
-        path.lineTo(-w/2,h/2);
-        path.lineTo(w/2,h/2);
-        path.lineTo(0,-h/2);
-        canvas.drawPath(path,paint);
+        canvas.drawLine(-w/2,-h/2,-w/2,h/2,paint);
+        canvas.drawLine(w/2,-h/2,w/2,h/2,paint);
         canvas.restore();
     }
-    public boolean stopped() {
-        return dir == 0;
-    }
     public void update() {
-        deg+=36*dir;
-        scale+=0.1f*dir;
-        if((scale>=1 && deg>=360) || (scale <=0 || deg<=0)) {
+        deg+=dir*36;
+        scale+=dir*0.1f;
+        if((deg>=360 && scale>=1) || (deg<=0 && scale<=0)) {
             dir = 0;
         }
     }
-    public int hashCode() {
-        return (int)(deg+scale+x+y+w+h);
+    public int hashCode(){
+        return (int)(x+y+w+h+scale+deg);
     }
 }
