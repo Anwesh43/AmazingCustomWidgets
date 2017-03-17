d746c8994d0473e1d2d648ec4ace54d61b95ef5e
Anwesh43 <anweshthecool0@gmail.com>
2017-03-18 02:58:15 +0530

    HamburgerButtonView class

--- hamburgbutton/src/main/java/com/anwesome/games/hamburgbutton/HamburgerButton.java
+++ hamburgbutton/src/main/java/com/anwesome/games/hamburgbutton/HamburgerButton.java
@@ -1,6 +1,10 @@
 package com.anwesome.games.hamburgbutton;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.MotionEvent;
+import android.view.View;
 
 /**
  * Created by anweshmishra on 18/03/17.
@@ -13,5 +17,29 @@ public class HamburgerButton {
     public void show() {
 
     }
-    
+    private class HamburgerButtonView extends View {
+        private boolean isAnimated = false;
+        public HamburgerButtonView(Context context) {
+            super(context);
+        }
+        public void onDraw(Canvas canvas) {
+            if(isAnimated) {
+                try {
+                    Thread.sleep(50);
+                    invalidate();
+                }
+                catch (Exception ex) {
+
+                }
+            }
+        }
+        public boolean onTouchEvent(MotionEvent event) {
+            if(event.getAction() == MotionEvent.ACTION_DOWN && !isAnimated) {
+
+                isAnimated = true;
+                postInvalidate();
+            }
+            return true;
+        }
+    }
 }
