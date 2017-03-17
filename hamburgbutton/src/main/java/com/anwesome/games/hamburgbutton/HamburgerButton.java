ba847c070c68cf803e263d37113fb209b88a09d7
Anwesh43 <anweshthecool0@gmail.com>
2017-03-18 02:55:18 +0530

    Adding HamburgerButton class

---
+++ hamburgbutton/src/main/java/com/anwesome/games/hamburgbutton/HamburgerButton.java
@@ -0,0 +1,17 @@
+package com.anwesome.games.hamburgbutton;
+
+import android.app.Activity;
+
+/**
+ * Created by anweshmishra on 18/03/17.
+ */
+public class HamburgerButton {
+    private Activity activity;
+    public HamburgerButton(Activity activity) {
+        this.activity = activity;
+    }
+    public void show() {
+
+    }
+    
+}
