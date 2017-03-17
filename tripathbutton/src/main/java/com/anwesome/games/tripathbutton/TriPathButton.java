42e3781e748e9b1bd1fb882fedad8bd6cab5c1a2
Anwesh43 <anweshthecool0@gmail.com>
2017-03-17 13:43:19 +0530

    Adding TriPathButton class

---
+++ tripathbutton/src/main/java/com/anwesome/games/tripathbutton/TriPathButton.java
@@ -0,0 +1,16 @@
+package com.anwesome.games.tripathbutton;
+
+import android.app.Activity;
+
+/**
+ * Created by anweshmishra on 17/03/17.
+ */
+public class TriPathButton {
+    private Activity activity;
+    public TriPathButton(Activity activity) {
+        this.activity = activity;
+    }
+    public void show() {
+
+    }
+}
