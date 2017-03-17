a8c0eae928d841f4774bffa3b9c961cfd63c80c3
Anwesh43 <anweshthecool0@gmail.com>
2017-03-17 14:09:01 +0530

    Adding PathFollowingBall

---
+++ tripathbutton/src/main/java/com/anwesome/games/tripathbutton/PathFollowingBall.java
@@ -0,0 +1,49 @@
+package com.anwesome.games.tripathbutton;
+
+import android.graphics.*;
+
+/**
+ * Created by anweshmishra on 17/03/17.
+ */
+public class PathFollowingBall {
+    private TriPath triPath;
+    private final int SIDES = 3;
+    private float deg = -60,r=0,maxR = 0,dir = 0;
+    private int i = 0;
+    private PointF pivot = new PointF();
+    public PathFollowingBall(TriPath triPath) {
+        this.triPath = triPath;
+        initProperties();
+    }
+    public void initProperties() {
+        maxR = triPath.getA();
+        pivot = triPath.getVertexAt(i);
+    }
+    public void draw(Canvas canvas, Paint paint) {
+        float x = pivot.x+(float)(r*Math.cos(deg*Math.PI/180)),y = pivot.x+(float)(r*Math.cos(deg*Math.PI/180));
+        paint.setColor(Color.RED);
+        paint.setStyle(Paint.Style.FILL);
+        canvas.drawCircle(x,y,maxR/12,paint);
+    }
+    public void startMoving() {
+        dir = dir == 0?1:dir;
+    }
+    public void update() {
+        r+=maxR/5*dir;
+        if(r>=maxR) {
+            deg+=(360/SIDES);
+            r = 0;
+            dir = 0;
+            r = maxR;
+            i++;
+            i%=SIDES;
+            pivot = triPath.getVertexAt(i);
+        }
+    }
+    public boolean stopped() {
+        return dir == 0;
+    }
+    public int hashCode() {
+        return pivot.hashCode()+triPath.hashCode()+(int)(deg+r+maxR+i+dir);
+    }
+}
