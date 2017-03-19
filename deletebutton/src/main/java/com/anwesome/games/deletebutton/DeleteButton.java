531cb352734eff161fd75bfa655b1d985cfed109
Anwesh43 <anweshthecool0@gmail.com>
2017-03-20 03:29:23 +0530

    adding show method and deletebuttonview

--- deletebutton/src/main/java/com/anwesome/games/deletebutton/DeleteButton.java
+++ deletebutton/src/main/java/com/anwesome/games/deletebutton/DeleteButton.java
@@ -1,12 +1,18 @@
 package com.anwesome.games.deletebutton;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.MotionEvent;
+import android.view.View;
 
 /**
  * Created by anweshmishra on 20/03/17.
  */
 public class DeleteButton  {
     private Activity activity;
+    private DeleteButtonController deleteButtonController;
+    private DeleteButtonView deleteButtonView;
     private DeleteButton(Activity activity) {
         this.activity = activity;
     }
@@ -14,5 +20,31 @@ public class DeleteButton  {
         return new DeleteButton(activity);
     }
     public void show() {
+        if(deleteButtonView == null) {
+            deleteButtonView = new DeleteButtonView(activity);
+        }
+    }
+    private class DeleteButtonView extends View {
+        private boolean isAnimated = false;
+        public DeleteButtonView(Context context){
+            super(context);
+        }
+        public void onDraw(Canvas canvas) {
+            if(isAnimated) {
+                try {
+                    Thread.sleep(50);
+                    invalidate();
+                }
+                catch (Exception ex) {
+
+                }
+            }
+        }
+        public boolean onTouchEvent(MotionEvent event) {
+            if(event.getAction() == MotionEvent.ACTION_DOWN && !isAnimated) {
+
+            }
+            return true;
+        }
     }
 }
