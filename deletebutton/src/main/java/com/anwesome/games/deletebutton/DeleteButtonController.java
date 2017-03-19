086b55d88a926a852d08dc01de1afa3d7e1539ba
Anwesh43 <anweshthecool0@gmail.com>
2017-03-20 02:53:42 +0530

    deletebuttoncontroller

---
+++ deletebutton/src/main/java/com/anwesome/games/deletebutton/DeleteButtonController.java
@@ -0,0 +1,57 @@
+package com.anwesome.games.deletebutton;
+
+import android.graphics.*;
+
+/**
+ * Created by anweshmishra on 20/03/17.
+ */
+public class DeleteButtonController {
+    private boolean stopped = false;
+    private float x,y,size,l= 0,deg = 0,bounds;private int mode=0;
+    public DeleteButtonController(float x,float y,float size,float bounds) {
+        this.x = x;
+        this.y = y;
+        this.size = size;
+        this.bounds = bounds;
+    }
+    public void draw(Canvas canvas,Paint paint) {
+        canvas.save();
+        canvas.translate(x,y);
+        paint.setStyle(Paint.Style.FILL);
+        paint.setColor(Color.parseColor("#FF9800"));
+        canvas.drawCircle(0,0,size/2,paint);
+        paint.setColor(Color.parseColor("#BDBDBD"));
+        paint.setStrokeWidth(size/40);
+        canvas.drawLine(-size/10,0,0,-l,paint);
+        canvas.restore();
+    }
+    public void update() {
+        switch(mode) {
+            case 0:
+                l+=size/30;
+                if(l>=(2*size)/15) {
+                    l = (2*size)/15;
+                    mode = 1;
+                }
+                break;
+            case 1:
+                deg+=10;
+                if(deg>=90) {
+                    deg = 90;
+                    mode = 2;
+                }
+                break;
+            case 2:
+                x+=size/15;
+                if(x>=bounds && !stopped) {
+                    stopped = true;
+                }
+                break;
+            default:
+                break;
+        }
+    }
+    public int hashCode() {
+        return (int)(x+y+size+bounds);
+    }
+}
