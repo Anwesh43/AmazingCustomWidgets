2389df548de6357fdd1ecf08a0527b6955a1da7f
Anwesh43 <anweshthecool0@gmail.com>
2017-03-20 03:39:27 +0530

    drawing and updating deletebuttoncontroller

--- deletebutton/src/main/java/com/anwesome/games/deletebutton/DeleteButton.java
+++ deletebutton/src/main/java/com/anwesome/games/deletebutton/DeleteButton.java
@@ -3,8 +3,13 @@ package com.anwesome.games.deletebutton;
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Point;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+
+import com.anwesome.ui.dimensionsutil.DimensionsUtil;
 
 /**
  * Created by anweshmishra on 20/03/17.
@@ -22,15 +27,24 @@ public class DeleteButton  {
     public void show() {
         if(deleteButtonView == null) {
             deleteButtonView = new DeleteButtonView(activity);
+            Point size = DimensionsUtil.getDeviceDimension(activity);
+            deleteButtonController = new DeleteButtonController(size.x/8,size.x/8,size.x/4,size.x);
+            activity.addContentView(deleteButtonView,new ViewGroup.LayoutParams(size.x/4,size.x/4));
         }
     }
     private class DeleteButtonView extends View {
         private boolean isAnimated = false;
+        private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
         public DeleteButtonView(Context context){
             super(context);
         }
         public void onDraw(Canvas canvas) {
+            deleteButtonController.draw(canvas,paint);
             if(isAnimated) {
+                deleteButtonController.update();
+                if(deleteButtonController.isStopped()) {
+                    isAnimated = false;
+                }
                 try {
                     Thread.sleep(50);
                     invalidate();
@@ -42,7 +56,8 @@ public class DeleteButton  {
         }
         public boolean onTouchEvent(MotionEvent event) {
             if(event.getAction() == MotionEvent.ACTION_DOWN && !isAnimated) {
-
+                isAnimated = true;
+                postInvalidate();
             }
             return true;
         }
