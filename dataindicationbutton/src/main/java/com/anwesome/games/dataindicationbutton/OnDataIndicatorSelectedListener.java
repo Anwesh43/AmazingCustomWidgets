f8bef2b0ed6c9487b223d938690017fa6f71d340
Anwesh43 <anweshthecool0@gmail.com>
2017-03-21 16:05:51 +0530

    Adding OnDataIndicatorSelectedListener interface

---
+++ dataindicationbutton/src/main/java/com/anwesome/games/dataindicationbutton/OnDataIndicatorSelectedListener.java
@@ -0,0 +1,9 @@
+package com.anwesome.games.dataindicationbutton;
+
+/**
+ * Created by anweshmishra on 21/03/17.
+ */
+public interface OnDataIndicatorSelectedListener {
+    void onSelected();
+    void onUnSelected();
+}
