7fdbf22586e8aedca583171daf78d7d92d36f299
Anwesh43 <anweshthecool0@gmail.com>
2017-03-23 01:38:11 +0530

    Drawing and animating breakable button

--- breakablebutton/src/main/java/com/anwesome/games/breakablebutton/BreakableButton.java
+++ breakablebutton/src/main/java/com/anwesome/games/breakablebutton/BreakableButton.java
@@ -3,28 +3,60 @@ package com.anwesome.games.breakablebutton;
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Point;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+
+import com.anwesome.ui.dimensionsutil.DimensionsUtil;
 
 /**
  * Created by anweshmishra on 23/03/17.
  */
 public class BreakableButton  {
     private Activity activity;
-    public BreakableButton(Activity activity) {
+    private String text;
+    private BreakableButtonView breakableButtonView;
+    private BreakableButtonController breakableButtonController;
+    public BreakableButton(Activity activity,String text) {
         this.activity = activity;
+        this.text = text;
     }
-    public void show() {
-
+    public void show(int x,int y) {
+        if(breakableButtonView == null) {
+            breakableButtonView = new BreakableButtonView(activity);
+            Point size = DimensionsUtil.getDeviceDimension(activity);
+            int w = size.x,h = size.y;
+            breakableButtonController = new BreakableButtonController(text,w/2);
+            activity.addContentView(breakableButtonView,new ViewGroup.LayoutParams(w/2,w/2));
+        }
+        breakableButtonView.setX(x);
+        breakableButtonView.setY(y);
     }
     private class BreakableButtonView extends View {
+        private boolean isAnimated = false;
         public BreakableButtonView(Context context) {
             super(context);
         }
         public void onDraw(Canvas canvas) {
+            breakableButtonController.draw(canvas);
+            if(isAnimated) {
+                breakableButtonController.update();
+                try {
+                    Thread.sleep(50);
+                    invalidate();
+                }
+                catch (Exception ex) {
 
+                }
+            }
         }
         public boolean onTouchEvent(MotionEvent event) {
+            if(event.getAction()==MotionEvent.ACTION_DOWN && !isAnimated) {
+                breakableButtonController.startMoving();
+                isAnimated = true;
+                postInvalidate();
+            }
             return true;
         }
     }
