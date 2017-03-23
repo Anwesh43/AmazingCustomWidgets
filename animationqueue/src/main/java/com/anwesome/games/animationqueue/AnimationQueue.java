34f16301cc8379e3c648a8ca081b739f0604fdb5
Anwesh43 <anweshthecool0@gmail.com>
2017-03-24 01:09:13 +0530

    making object animation an interface

--- animationqueue/src/main/java/com/anwesome/games/animationqueue/AnimationQueue.java
+++ animationqueue/src/main/java/com/anwesome/games/animationqueue/AnimationQueue.java
@@ -3,7 +3,5 @@ package com.anwesome.games.animationqueue;
 /**
  * Created by anweshmishra on 24/03/17.
  */
-public interface ObjectAnimation {
-    void animate();
-    boolean isDone();
+public class AnimationQueue {
 }
