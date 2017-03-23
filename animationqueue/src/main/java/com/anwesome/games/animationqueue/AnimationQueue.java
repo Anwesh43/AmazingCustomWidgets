d8797b40a83529c5ea5de34ca869617b74121768
Anwesh43 <anweshthecool0@gmail.com>
2017-03-24 01:09:30 +0530

    Adding AnimationQueue

--- animationqueue/src/main/java/com/anwesome/games/animationqueue/AnimationQueue.java
+++ animationqueue/src/main/java/com/anwesome/games/animationqueue/AnimationQueue.java
@@ -1,7 +1,23 @@
 package com.anwesome.games.animationqueue;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by anweshmishra on 24/03/17.
  */
 public class AnimationQueue {
+    private List<ObjectAnimation> objectAnimations = new ArrayList<>();
+    public void addObjectAnimation(ObjectAnimation objectAnimation) {
+        this.objectAnimations.add(objectAnimation);
+    }
+    public void execute() {
+        if(objectAnimations.size()>0) {
+            ObjectAnimation objectAnimation = objectAnimations.get(0);
+            objectAnimation.animate();
+            if(objectAnimation.isDone()) {
+                objectAnimations.remove(0);
+            }
+        }
+    }
 }
