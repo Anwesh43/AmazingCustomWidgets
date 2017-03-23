527b0d783f9b49fc08b014e63123c6b57e818e0b
Anwesh43 <anweshthecool0@gmail.com>
2017-03-24 00:50:35 +0530

    Adding ObjectAnimation class

--- animationqueue/src/main/java/com/anwesome/games/animationqueue/ObjectAnimation.java
+++ animationqueue/src/main/java/com/anwesome/games/animationqueue/ObjectAnimation.java
@@ -4,4 +4,10 @@ package com.anwesome.games.animationqueue;
  * Created by anweshmishra on 24/03/17.
  */
 public class ObjectAnimation {
+   public void animate() {
+
+   }
+    public boolean isDown() {
+        return false;
+    }
 }
