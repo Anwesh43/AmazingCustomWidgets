10a5d5f6927864366a275a6e8629f6389b03f61e
Anwesh43 <anweshthecool0@gmail.com>
2017-03-24 01:31:37 +0530

    Adding AnimationQueue modifications

--- animationqueue/src/main/java/com/anwesome/games/animationqueue/AnimationQueue.java
+++ animationqueue/src/main/java/com/anwesome/games/animationqueue/AnimationQueue.java
@@ -7,17 +7,29 @@ import java.util.List;
  * Created by anweshmishra on 24/03/17.
  */
 public class AnimationQueue {
+    private float dir = 0;
+    private int index = -1;
     private List<ObjectAnimation> objectAnimations = new ArrayList<>();
     public void addObjectAnimation(ObjectAnimation objectAnimation) {
         this.objectAnimations.add(objectAnimation);
     }
+    public void setDir(float dir) {
+        this.dir = dir;
+        index+=dir;
+    }
     public void execute() {
-        if(objectAnimations.size()>0) {
-            ObjectAnimation objectAnimation = objectAnimations.get(0);
+        if(index>0 && index<objectAnimations.size()) {
+            ObjectAnimation objectAnimation = objectAnimations.get(index);
             objectAnimation.animate();
             if(objectAnimation.isDone()) {
-                objectAnimations.remove(0);
+                index+=dir;
             }
         }
+        else {
+            dir = 0;
+        }
+    }
+    public boolean stopped() {
+        return dir == 0;
     }
 }
