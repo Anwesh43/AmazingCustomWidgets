2081e8def43af2efc1326847c876336d0b4070a2
Anwesh43 <anweshthecool0@gmail.com>
2017-03-25 01:21:24 +0530

    Adding settingsbuttonview

--- settingsbutton/src/main/java/com/anwesome/demos/settingsbutton/SettingsButton.java
+++ settingsbutton/src/main/java/com/anwesome/demos/settingsbutton/SettingsButton.java
@@ -1,6 +1,10 @@
 package com.anwesome.demos.settingsbutton;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.MotionEvent;
+import android.view.View;
 
 /**
  * Created by anweshmishra on 25/03/17.
@@ -11,6 +15,17 @@ public class SettingsButton {
         this.activity = activity;
     }
     public void show() {
-        
+
+    }
+    private class SettingsButtonView extends View {
+        public SettingsButtonView(Context context) {
+            super(context);
+        }
+        public void onDraw(Canvas canvas) {
+
+        }
+        public boolean onTouchEvent(MotionEvent event) {
+            return true;
+        }
     }
 }
