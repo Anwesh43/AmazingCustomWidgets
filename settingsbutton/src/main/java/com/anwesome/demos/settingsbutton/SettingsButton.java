4fa880313516bb6730c5952c04593a7d913be439
Anwesh43 <anweshthecool0@gmail.com>
2017-03-25 01:20:05 +0530

    adding methods of settings button

---
+++ settingsbutton/src/main/java/com/anwesome/demos/settingsbutton/SettingsButton.java
@@ -0,0 +1,16 @@
+package com.anwesome.demos.settingsbutton;
+
+import android.app.Activity;
+
+/**
+ * Created by anweshmishra on 25/03/17.
+ */
+public class SettingsButton {
+    private Activity activity;
+    public SettingsButton(Activity activity) {
+        this.activity = activity;
+    }
+    public void show() {
+        
+    }
+}
