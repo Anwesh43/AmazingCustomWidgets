283ba1aee21e3e91e5545f5421dfd2f99cf16955
Anwesh43 <anweshthecool0@gmail.com>
2017-03-27 01:00:00 +0530

    adding the methods that will be exposed from playbackbutton

---
+++ playbackbutton/src/main/java/com/anwesome/demos/playbackbutton/PlaybackButton.java
@@ -0,0 +1,17 @@
+package com.anwesome.demos.playbackbutton;
+
+import android.app.Activity;
+
+/**
+ * Created by anweshmishra on 27/03/17.
+ */
+public class PlaybackButton {
+    private PlaybackButtonShape shape;
+    private Activity activity;
+    public PlaybackButton(Activity activity) {
+        this.activity = activity;
+    }
+    public void show() {
+
+    }
+}
