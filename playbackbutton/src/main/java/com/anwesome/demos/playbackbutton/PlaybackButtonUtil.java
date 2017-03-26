cb85e6676ac2c63d3d4a7b606ffe4a033ab06610
Anwesh43 <anweshthecool0@gmail.com>
2017-03-27 01:51:53 +0530

    adding util to draw playbackcontrol and replay

--- playbackbutton/src/main/java/com/anwesome/demos/playbackbutton/PlaybackButtonUtil.java
+++ playbackbutton/src/main/java/com/anwesome/demos/playbackbutton/PlaybackButtonUtil.java
@@ -1,7 +1,45 @@
 package com.anwesome.demos.playbackbutton;
 
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.RectF;
+
 /**
  * Created by anweshmishra on 27/03/17.
  */
 public class PlaybackButtonUtil {
+    public static void drawShape(Canvas canvas, Paint paint,float r,PlaybackButtonType type) {
+        switch(type) {
+            case PLAYBACK_CONTROL:
+                drawPlaybackControl(canvas,paint,r);
+                break;
+            case REPLAY:
+                drawReplay(canvas,paint,r);
+                break;
+            default:
+                break;
+        }
+    }
+    private static void drawPlaybackControl(Canvas canvas,Paint paint,float r) {
+        paint.setStyle(Paint.Style.FILL);
+        Path path = new Path();
+        path.moveTo(0,r);
+        path.lineTo(r,0);
+        path.lineTo(0,-r);
+        path.lineTo(0,r);
+        canvas.drawPath(path,paint);
+        paint.setStrokeWidth(r/10);
+        canvas.drawLine(r,-r,r,r,paint);
+    }
+    private static void drawReplay(Canvas canvas,Paint paint,float r) {
+        paint.setStyle(Paint.Style.STROKE);
+        canvas.drawArc(new RectF(-r,-r,r,r),-90,270,false,paint);
+        paint.setStyle(Paint.Style.FILL);
+        Path path = new Path();
+        path.moveTo(-r-r/10,0);
+        path.lineTo(-r,-r/10);
+        path.lineTo(-r+r/10,0);
+        canvas.drawPath(path,paint);
+    }
 }
