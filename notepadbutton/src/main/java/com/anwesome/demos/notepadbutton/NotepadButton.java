76c61180b81ec99af73b9edb8638be362337cd97
Anwesh43 <anweshthecool0@gmail.com>
2017-03-28 01:59:35 +0530

    adding notepadbutton and exposing methods

---
+++ notepadbutton/src/main/java/com/anwesome/demos/notepadbutton/NotepadButton.java
@@ -0,0 +1,17 @@
+package com.anwesome.demos.notepadbutton;
+
+import android.app.Activity;
+
+/**
+ * Created by anweshmishra on 28/03/17.
+ */
+public class NotepadButton {
+    private Activity activity;
+    private NotepadButton(Activity activity) {
+        this.activity = activity;
+    }
+    public void show() {
+
+    }
+    
+}
