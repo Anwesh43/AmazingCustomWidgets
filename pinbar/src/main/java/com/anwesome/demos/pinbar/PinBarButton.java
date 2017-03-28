93b3245cc942b73e2e3f9526f23ad2f01810dea2
Anwesh43 <anweshthecool0@gmail.com>
2017-03-29 01:47:31 +0530

    Adding View

--- pinbar/src/main/java/com/anwesome/demos/pinbar/PinBarButton.java
+++ pinbar/src/main/java/com/anwesome/demos/pinbar/PinBarButton.java
@@ -1,6 +1,9 @@
 package com.anwesome.demos.pinbar;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.*;
+import android.view.*;
 
 /**
  * Created by anweshmishra on 29/03/17.
@@ -12,5 +15,15 @@ public class PinBarButton  {
     }
     public void show() {
 
+    }
+    private class PinBarButtonView extends View {
+        public PinBarButtonView(Context context) {
+            super(context);
+        }
+        public void onDraw(Canvas canvas) {
+        }
+        public boolean onTouchEvent(MotionEvent event) {
+            return true;
+        }
     }
 }
