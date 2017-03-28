df7a0ce4ee1d19822c5d977660ba808e34a86d7f
Anwesh43 <anweshthecool0@gmail.com>
2017-03-29 02:43:32 +0530

    increasing gaps between pinbars and setting the heights properly according to the original height

--- pinbar/src/main/java/com/anwesome/demos/pinbar/PinBar.java
+++ pinbar/src/main/java/com/anwesome/demos/pinbar/PinBar.java
@@ -6,31 +6,32 @@ import android.graphics.*;
  * Created by anweshmishra on 29/03/17.
  */
 public class PinBar {
-    private float x,y,w,h1,h2,hSpeed,deg = 0,dir = 0,n = 3;
+    private float x,y,w,h1,h2,hSpeed,deg = 0,dir = 0,n = 3,origH;
     private PinBar(float x,float y,float w,float h,float n) {
         this.x = x;
         this.y = y;
         this.w = w;
         this.h1 = h/2;
         this.h2 = h;
-        this.hSpeed = h/10;
+        this.origH = h;
+        this.hSpeed = h/20;
         this.n = Math.max(n,this.n);
     }
     public static PinBar getInstance(float x,float y,float w,float h,float n) {
         return new PinBar(x,y,w,h,n);
     }
     public void update() {
-        deg+=dir*10;
+        deg+=dir*9;
         h1+=dir*hSpeed;
         h2-=dir*hSpeed;
         if(deg<=0 || deg>=90 ) {
             if(dir == 1) {
-                h1 = 10*hSpeed;
-                h2 = 5*hSpeed;
+                h1 = origH;
+                h2 = origH/2;
             }
             else {
-                h1 = 5*hSpeed;
-                h2 = 10*hSpeed;
+                h1 = origH/2;
+                h2 = origH;
             }
             dir = 0;
         }
@@ -53,13 +54,13 @@ public class PinBar {
             }
             drawPin(canvas, paint, gap, newH);
             canvas.restore();
+            xbar+=2*gap;
         }
         canvas.restore();
     }
     public void drawPin(Canvas canvas,Paint paint,float gap,float h) {
-        float origH = hSpeed*10;
-        canvas.drawRect(-gap/2,-origH/10,gap/2,0,paint);
-        canvas.drawRect(-gap/2,-origH/5,gap/2,h,paint);
+        canvas.drawRect(-gap/2,origH/10,gap/2,origH/5,paint);
+        canvas.drawRect(-gap/2,-h,gap/2,0,paint);
     }
     public int hashCode() {
         return (int)(x+y+w+deg+dir+h1+h2);
