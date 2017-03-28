c6f1b7526dd64dfba7ed6b24a98e55d29a4acda1
Anwesh43 <anweshthecool0@gmail.com>
2017-03-29 01:45:43 +0530

    Adding PinBarButton

---
+++ pinbar/src/main/java/com/anwesome/demos/pinbar/PinBarButton.java
@@ -0,0 +1,16 @@
+package com.anwesome.demos.pinbar;
+
+import android.app.Activity;
+
+/**
+ * Created by anweshmishra on 29/03/17.
+ */
+public class PinBarButton  {
+    private Activity activity;
+    public PinBarButton(Activity activity) {
+        this.activity = activity;
+    }
+    public void show() {
+
+    }
+}
