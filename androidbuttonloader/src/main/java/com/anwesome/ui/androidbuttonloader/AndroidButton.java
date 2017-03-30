1b4417f84a9faf799697797708126251e00ab6ba
Anwesh43 <anweshthecool0@gmail.com>
2017-03-30 09:50:34 +0530

    Adding AndroidButton to draw AndroidShape

--- androidbuttonloader/src/main/java/com/anwesome/ui/androidbuttonloader/AndroidButton.java
+++ androidbuttonloader/src/main/java/com/anwesome/ui/androidbuttonloader/AndroidButton.java
@@ -1,7 +1,25 @@
 package com.anwesome.ui.androidbuttonloader;
 
+import android.graphics.Canvas;
+import android.graphics.Paint;
+
 /**
  * Created by anweshmishra on 30/03/17.
  */
 public class AndroidButton {
+    private float x,y,size,deg = 0;
+    private AndroidButton(float x,float y,float size) {
+        this.x = x;
+        this.y = y;
+        this.size = size;
+    }
+    public void draw(Canvas canvas, Paint paint) {
+        AblDrawingUtil.drawAndroidShape(canvas,paint,size);
+    }
+    public void update() {
+        deg+=20;
+    }
+    public int hashCode() {
+        return (int)(x+y+deg);
+    }
 }
