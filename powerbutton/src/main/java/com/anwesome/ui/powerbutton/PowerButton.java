d6122576f9973ae2d646e9628be35d8351266118
Anwesh43 <anweshthecool0@gmail.com>
2017-03-31 01:04:52 +0530

    Adding Powerbutton class

---
+++ powerbutton/src/main/java/com/anwesome/ui/powerbutton/PowerButton.java
@@ -0,0 +1,16 @@
+package com.anwesome.ui.powerbutton;
+
+import android.app.Activity;
+
+/**
+ * Created by anweshmishra on 31/03/17.
+ */
+public class PowerButton {
+    private Activity activity;
+    private PowerButton(Activity activity) {
+        this.activity = activity;
+    }
+    public void show() {
+
+    }
+}
