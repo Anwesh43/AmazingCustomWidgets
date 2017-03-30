7eff8e59b2e3d47928ecf5b623c4690adafe45d5
Anwesh43 <anweshthecool0@gmail.com>
2017-03-31 01:11:18 +0530

    Initializing and Adding View,Intializing PowerButtonShape

--- powerbutton/src/main/java/com/anwesome/ui/powerbutton/PowerButton.java
+++ powerbutton/src/main/java/com/anwesome/ui/powerbutton/PowerButton.java
@@ -3,8 +3,13 @@ package com.anwesome.ui.powerbutton;
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Point;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+
+import com.anwesome.ui.dimensionsutil.DimensionsUtil;
 
 /**
  * Created by anweshmishra on 31/03/17.
@@ -12,12 +17,17 @@ import android.view.View;
 public class PowerButton {
     private Activity activity;
     private PowerButtonView powerButtonView;
+    private PowerButtonShape powerButtonShape;
     private PowerButton(Activity activity) {
         this.activity = activity;
     }
     public void show() {
         if(powerButtonView == null) {
             powerButtonView = new PowerButtonView(activity);
+            Point dimension = DimensionsUtil.getDeviceDimension(activity);
+            int w = dimension.x;
+            activity.addContentView(powerButtonView,new ViewGroup.LayoutParams(w/2,w/2));
+            powerButtonShape = PowerButtonShape.getInstance(w/4,w/4,w/4);
         }
     }
     private class PowerButtonView extends View {
