922dee7939c7632df831e4fa7cb30931c1114e7a
Anwesh43 <anweshthecool0@gmail.com>
2017-03-31 01:08:39 +0530

    Adding PowerbuttonView

--- powerbutton/src/main/java/com/anwesome/ui/powerbutton/PowerButton.java
+++ powerbutton/src/main/java/com/anwesome/ui/powerbutton/PowerButton.java
@@ -1,16 +1,34 @@
 package com.anwesome.ui.powerbutton;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.MotionEvent;
+import android.view.View;
 
 /**
  * Created by anweshmishra on 31/03/17.
  */
 public class PowerButton {
     private Activity activity;
+    private PowerButtonView powerButtonView;
     private PowerButton(Activity activity) {
         this.activity = activity;
     }
     public void show() {
+        if(powerButtonView == null) {
+            powerButtonView = new PowerButtonView(activity);
+        }
+    }
+    private class PowerButtonView extends View {
+        public PowerButtonView(Context context) {
+            super(context);
+        }
+        public void onDraw(Canvas canvas) {
 
+        }
+        public boolean onTouchEvent(MotionEvent event) {
+            return true;
+        }
     }
 }
