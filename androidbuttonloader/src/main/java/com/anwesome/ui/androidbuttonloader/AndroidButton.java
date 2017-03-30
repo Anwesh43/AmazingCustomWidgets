932f8489796fadd1c94cfd724b3d832d62c4a132
Anwesh43 <anweshthecool0@gmail.com>
2017-03-30 09:55:34 +0530

    dir must be 0 during initialization

--- androidbuttonloader/src/main/java/com/anwesome/ui/androidbuttonloader/AndroidButton.java
+++ androidbuttonloader/src/main/java/com/anwesome/ui/androidbuttonloader/AndroidButton.java
@@ -7,7 +7,7 @@ import android.graphics.Paint;
  * Created by anweshmishra on 30/03/17.
  */
 public class AndroidButton {
-    private float x,y,size,deg = 0,dir = 1;
+    private float x,y,size,deg = 0,dir = 0;
     private AndroidButton(float x,float y,float size) {
         this.x = x;
         this.y = y;
