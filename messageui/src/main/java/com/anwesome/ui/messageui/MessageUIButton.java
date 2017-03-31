b6cf0c78e57210e24e223332c0656695c966174f
Anwesh43 <anweshthecool0@gmail.com>
2017-04-01 03:07:04 +0530

    Initializing MessageButtonView object, adding to activity and positioning it

--- messageui/src/main/java/com/anwesome/ui/messageui/MessageUIButton.java
+++ messageui/src/main/java/com/anwesome/ui/messageui/MessageUIButton.java
@@ -4,19 +4,31 @@ import android.app.Activity;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Point;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+
+import com.anwesome.ui.dimensionsutil.DimensionsUtil;
 
 /**
  * Created by anweshmishra on 01/04/17.
  */
 public class MessageUIButton {
     private Activity activity;
+    private MessageUIButtonView mView;
     public MessageUIButton(Activity activity) {
         this.activity = activity;
     }
-    public void show() {
-
+    public void show(int x,int y) {
+        if(mView == null) {
+            mView = new MessageUIButtonView(activity);
+            Point dimension = DimensionsUtil.getDeviceDimension(activity);
+            int w = dimension.x;
+            activity.addContentView(mView,new ViewGroup.LayoutParams(w/3,w/3));
+        }
+        mView.setX(x);
+        mView.setY(y);
     }
     private class MessageUIButtonView extends View {
         private boolean isAnimated = false;
