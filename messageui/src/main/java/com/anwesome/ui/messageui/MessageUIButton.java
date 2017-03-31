c4c96c7a8f8ba74c3a7f094763b52742bf461c1b
Anwesh43 <anweshthecool0@gmail.com>
2017-04-01 02:57:02 +0530

    adding messageuibutton class

---
+++ messageui/src/main/java/com/anwesome/ui/messageui/MessageUIButton.java
@@ -0,0 +1,16 @@
+package com.anwesome.ui.messageui;
+
+import android.app.Activity;
+
+/**
+ * Created by anweshmishra on 01/04/17.
+ */
+public class MessageUIButton {
+    private Activity activity;
+    public MessageUIButton(Activity activity) {
+        this.activity = activity;
+    }
+    public void show() {
+        
+    }
+}
