3d1b8a362ba1e610188e55de0efc7590640ea7ef
Anwesh43 <anweshthecool0@gmail.com>
2017-04-02 01:43:19 +0530

    showing syncbshape

--- syncbutton/src/main/java/com/anwesome/ui/syncbutton/SyncButton.java
+++ syncbutton/src/main/java/com/anwesome/ui/syncbutton/SyncButton.java
@@ -4,8 +4,12 @@ import android.app.Activity;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Point;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+
+import com.anwesome.ui.dimensionsutil.DimensionsUtil;
 
 /**
  * Created by anweshmishra on 02/04/17.
@@ -13,11 +17,20 @@ import android.view.View;
 public class SyncButton {
     private Activity activity;
     private SyncShape syncShape;
+    private SyncButtonView syncButtonView;
     public SyncButton(Activity activity) {
         this.activity = activity;
     }
-    public void show() {
-
+    public void show(int x,int y) {
+        if(syncButtonView == null) {
+            syncButtonView = new SyncButtonView(activity);
+            Point dimension = DimensionsUtil.getDeviceDimension(activity);
+            int w = dimension.x;
+            activity.addContentView(syncButtonView,new ViewGroup.LayoutParams(w/3,w/3));
+            syncShape = new SyncShape(w/6,w/6,w/12);
+        }
+        syncButtonView.setX(x);
+        syncButtonView.setY(y);
     }
     private class SyncButtonView extends View {
         private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
@@ -30,6 +43,7 @@ public class SyncButton {
                 syncShape.draw(canvas,paint);
                 if(isAnimated) {
                     try {
+                        syncShape.update();
                         Thread.sleep(50);
                         invalidate();
                     }
