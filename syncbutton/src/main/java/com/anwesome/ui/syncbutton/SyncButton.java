08be4e97272a00b0beaaf9405bcde3655ecd37b9
Anwesh43 <anweshthecool0@gmail.com>
2017-04-02 01:21:35 +0530

    Adding SyncButton class

---
+++ syncbutton/src/main/java/com/anwesome/ui/syncbutton/SyncButton.java
@@ -0,0 +1,16 @@
+package com.anwesome.ui.syncbutton;
+
+import android.app.Activity;
+
+/**
+ * Created by anweshmishra on 02/04/17.
+ */
+public class SyncButton {
+    private Activity activity;
+    public SyncButton(Activity activity) {
+        this.activity = activity;
+    }
+    public void show() {
+        
+    }
+}
