74b5c75b2e817b74449cda372162f4b5cc898796
Anwesh43 <anweshthecool0@gmail.com>
2017-04-04 05:53:43 +0530

    drawing emergencybuttonshape and updating it

--- emergencybutton/src/main/java/com/anwesome/ui/emergencybutton/EmergencyButton.java
+++ emergencybutton/src/main/java/com/anwesome/ui/emergencybutton/EmergencyButton.java
@@ -3,8 +3,13 @@ package com.anwesome.ui.emergencybutton;
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Point;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+
+import com.anwesome.ui.dimensionsutil.DimensionsUtil;
 
 /**
  * Created by anweshmishra on 04/04/17.
@@ -19,16 +24,35 @@ public class EmergencyButton {
     public void show() {
         if(emergencyButtonShapeView == null) {
             emergencyButtonShapeView = new EmergencyButtonShapeView(activity);
+            Point size = DimensionsUtil.getDeviceDimension(activity);
+            int w = size.x;
+            activity.addContentView(emergencyButtonShapeView,new ViewGroup.LayoutParams(w/2,w/2));
+            emergencyButtonShape = new EmergencyButtonShape(w/4,w/4,w/4);
         }
     }
     private class EmergencyButtonShapeView extends View {
+        private boolean isAnimated = false;
+        private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
         public EmergencyButtonShapeView(Context context) {
             super(context);
         }
         public void onDraw(Canvas canvas) {
+            emergencyButtonShape.draw(canvas,paint);
+            if(isAnimated) {
+                try {
+                    emergencyButtonShape.update();
+                    Thread.sleep(50);
+                    invalidate();
+                }
+                catch (Exception ex) {
 
+                }
+            }
         }
         public boolean onTouchEvent(MotionEvent event) {
+            if(event.getAction() == MotionEvent.ACTION_DOWN && !isAnimated && emergencyButtonShape!=null) {
+                emergencyButtonShape.startMoving();
+            }
             return true;
         }
     }
