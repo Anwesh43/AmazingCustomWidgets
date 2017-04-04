c881971df3f923f1194194bea92ec2519a98f795
Anwesh43 <anweshthecool0@gmail.com>
2017-04-04 05:31:49 +0530

    Adding EmergencyButtonShape

---
+++ emergencybutton/src/main/java/com/anwesome/ui/emergencybutton/EmergencyButton.java
@@ -0,0 +1,7 @@
+package com.anwesome.ui.emergencybutton;
+
+/**
+ * Created by anweshmishra on 04/04/17.
+ */
+public class EmergencyButton {
+}
---
+++ emergencybutton/src/main/java/com/anwesome/ui/emergencybutton/EmergencyButtonShape.java
@@ -0,0 +1,55 @@
+package com.anwesome.ui.emergencybutton;
+
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+
+/**
+ * Created by anweshmishra on 04/04/17.
+ */
+public class EmergencyButtonShape {
+    private float x,y,size,deg = 0,dir = 0;
+    public EmergencyButtonShape(float x,float y,float size) {
+        this.x = x;
+        this.y = y;
+        this.size = size;
+    }
+    public void draw(Canvas canvas, Paint paint) {
+        paint.setColor(Color.BLACK);
+        paint.setStyle(Paint.Style.FILL);
+        canvas.save();
+        canvas.translate(x,y);
+        drawTriangle(canvas, paint);
+        paint.setColor(Color.WHITE);
+        drawEmergencyIcon(canvas,paint);
+        paint.setStyle(Paint.Style.STROKE);
+        drawTriangle(canvas,paint);
+        canvas.rotate(deg);
+        canvas.restore();
+    }
+    private void drawTriangle(Canvas canvas,Paint paint) {
+        Path path = new Path();
+        path.moveTo(-size/2,size/2);
+        path.lineTo(size/2,size/2);
+        path.lineTo(0,-size/2);
+        path.lineTo(-size/2,size/2);
+        canvas.drawPath(path,paint);
+    }
+    private void drawEmergencyIcon(Canvas canvas,Paint paint) {
+        canvas.drawLine(0,-size/4,0,size/4,paint);
+        canvas.drawCircle(0,size/4+size/8,size/32,paint);
+    }
+    public void update() {
+        deg+=24*dir;
+        if(deg % 360 == 0){
+            dir = 0;
+        }
+    }
+    public boolean stop() {
+        return dir == 0;
+    }
+    public void startMoving() {
+        dir = dir == 0?1:dir;
+    }
+}
