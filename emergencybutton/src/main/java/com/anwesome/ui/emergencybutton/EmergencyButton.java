50e3e3b8de1e51a7365ca9d93924f45dd9de6717
Anwesh43 <anweshthecool0@gmail.com>
2017-04-04 05:41:47 +0530

    Adding show method and EmergencyButtonShapeView

--- emergencybutton/src/main/java/com/anwesome/ui/emergencybutton/EmergencyButton.java
+++ emergencybutton/src/main/java/com/anwesome/ui/emergencybutton/EmergencyButton.java
@@ -1,7 +1,35 @@
 package com.anwesome.ui.emergencybutton;
 
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.MotionEvent;
+import android.view.View;
+
 /**
  * Created by anweshmishra on 04/04/17.
  */
 public class EmergencyButton {
+    private Activity activity;
+    private EmergencyButtonShape emergencyButtonShape;
+    private EmergencyButtonShapeView emergencyButtonShapeView;
+    public EmergencyButton(Activity activity) {
+        this.activity = activity;
+    }
+    public void show() {
+        if(emergencyButtonShapeView == null) {
+            emergencyButtonShapeView = new EmergencyButtonShapeView(activity);
+        }
+    }
+    private class EmergencyButtonShapeView extends View {
+        public EmergencyButtonShapeView(Context context) {
+            super(context);
+        }
+        public void onDraw(Canvas canvas) {
+
+        }
+        public boolean onTouchEvent(MotionEvent event) {
+            return true;
+        }
+    }
 }
