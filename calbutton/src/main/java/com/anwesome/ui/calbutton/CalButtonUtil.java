6816798996d26c4147690982b021649e41ece519
Anwesh43 <anweshthecool0@gmail.com>
2017-04-05 15:13:52 +0530

    Adding CalButtonUtil

---
+++ calbutton/src/main/java/com/anwesome/ui/calbutton/CalButtonUtil.java
@@ -0,0 +1,11 @@
+package com.anwesome.ui.calbutton;
+
+/**
+ * Created by anweshmishra on 05/04/17.
+ */
+public class CalButtonUtil {
+    public final static String[] months = {"JAN","FEB","MAR","APR","MAY","JUN","JUL","AUG","SEP","OCT","NOV","DEC"};
+    public final static String MONTH_BAR_COLOR = "#f44336";
+    public final static String DAY_BAR_COLOR = "#E0E0E0";
+    public final static String DAY_TEXT_COLOR = "#212121";
+}
