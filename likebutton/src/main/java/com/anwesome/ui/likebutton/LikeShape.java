494c658ec0bc5343d9b5eb13c86612a7dedf4d36
Anwesh43 <anweshthecool0@gmail.com>
2017-04-07 02:42:25 +0530

    updating and stopping animation oflikebutton

--- likebutton/src/main/java/com/anwesome/ui/likebutton/LikeShape.java
+++ likebutton/src/main/java/com/anwesome/ui/likebutton/LikeShape.java
@@ -6,7 +6,7 @@ import android.graphics.*;
  * Created by anweshmishra on 07/04/17.
  */
 public class LikeShape {
-    private float x,y,deg = 0,size;
+    private float x,y,deg = 0,size,dir = 0;
     public LikeShape(float x,float y,float size) {
         this.x = x;
         this.y = y;
@@ -30,5 +30,17 @@ public class LikeShape {
         path.lineTo(-size/2+size/5,size/2);
         canvas.drawPath(path,paint);
         canvas.restore();
+    }
+    public void update() {
+        deg+=dir*20;
+        if(deg>=180 || deg<=0) {
+            dir = 0;
+        }
+    }
+    public void startMoving() {
+        dir = deg == 180?1:-1;
+    }
+    public boolean stop() {
+        return dir == 0;
     }
 }
