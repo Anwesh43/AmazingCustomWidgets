008b450468d1ece1ff64cf62fa27063ac3201551
Anwesh43 <anweshthecool0@gmail.com>
2017-04-07 02:49:43 +0530

    drawing and updating likeshape in view

--- likebutton/src/main/java/com/anwesome/ui/likebutton/LikeButton.java
+++ likebutton/src/main/java/com/anwesome/ui/likebutton/LikeButton.java
@@ -2,6 +2,9 @@ package com.anwesome.ui.likebutton;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.view.MotionEvent;
 import android.view.View;
 
 /**
@@ -9,6 +12,7 @@ import android.view.View;
  */
 public class LikeButton  {
     private Activity activity;
+    private LikeShape likeShape;
     private LikeButtonView likeButtonView;
     public LikeButton(Activity activity) {
         this.activity = activity;
@@ -19,8 +23,35 @@ public class LikeButton  {
         }
     }
     private class LikeButtonView extends View {
+        private boolean isAnimated = false;
+        private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
         public LikeButtonView(Context context) {
             super(context);
+        }
+        public void onDraw(Canvas canvas) {
+            if(likeShape!=null) {
+                likeShape.draw(canvas,paint);
+                if (isAnimated) {
+                    likeShape.update();
+                    if(likeShape.stop()) {
+                        isAnimated = false;
+                    }
+                    try {
+                        Thread.sleep(50);
+                        invalidate();
+                    } catch (Exception ex) {
+
+                    }
+                }
+            }
+        }
+        public boolean onTouchEvent(MotionEvent event) {
+            if(!isAnimated && likeShape!=null && event.getAction() == MotionEvent.ACTION_DOWN) {
+                likeShape.startMoving();
+                isAnimated = true;
+                postInvalidate();
+            }
+            return  true;
         }
     }
 }
