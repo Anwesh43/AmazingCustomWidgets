9c65b627f97b260a69b8b444203938a86c28ec42
Anwesh43 <anweshthecool0@gmail.com>
2017-04-07 02:39:55 +0530

    Adding LikeShape class

---
+++ likebutton/src/main/java/com/anwesome/ui/likebutton/LikeShape.java
@@ -0,0 +1,34 @@
+package com.anwesome.ui.likebutton;
+
+import android.graphics.*;
+
+/**
+ * Created by anweshmishra on 07/04/17.
+ */
+public class LikeShape {
+    private float x,y,deg = 0,size;
+    public LikeShape(float x,float y,float size) {
+        this.x = x;
+        this.y = y;
+        this.size = size;
+    }
+    public void draw(Canvas canvas,Paint paint) {
+        canvas.save();
+        canvas.translate(x,y);
+        canvas.rotate(deg);
+        canvas.drawRect(new RectF(-size/2,-size/2,-size/2+size/10,size/2),paint);
+        paint.setStyle(Paint.Style.FILL);
+        paint.setColor(Color.GRAY);
+        Path path = new Path();
+        path.moveTo(-size/2+size/5,-size/2);
+        path.lineTo(0,-3*size/4);
+        path.lineTo(size/20,-3*size/4+size/20);
+        path.lineTo(size/20,-size/2);
+        path.lineTo(size/2-size/20,-size/2);
+        path.lineTo(size/2-size/20,-size/2+size/20);
+        path.lineTo(size/2-size/10,size/2);
+        path.lineTo(-size/2+size/5,size/2);
+        canvas.drawPath(path,paint);
+        canvas.restore();
+    }
+}
