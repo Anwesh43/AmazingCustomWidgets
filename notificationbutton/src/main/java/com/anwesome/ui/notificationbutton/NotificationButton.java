e39d2e5f6d4f0afefac746a7874fdcc7c35b8678
Anwesh43 <anweshthecool0@gmail.com>
2017-04-09 14:35:24 +0530

    using notificationbuttoncontroller in view

--- notificationbutton/src/main/java/com/anwesome/ui/notificationbutton/NotificationButton.java
+++ notificationbutton/src/main/java/com/anwesome/ui/notificationbutton/NotificationButton.java
@@ -3,6 +3,7 @@ package com.anwesome.ui.notificationbutton;
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -12,6 +13,7 @@ import android.view.View;
 public class NotificationButton {
     private Activity activity;
     private NotificationButtonView notificationButtonView;
+    private NotificationButtonController notificationButtonController;
     public NotificationButton(Activity activity) {
         this.activity = activity;
     }
@@ -21,15 +23,34 @@ public class NotificationButton {
         }
     }
     private class NotificationButtonView extends View {
+        private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        private boolean isAnimated = false;
         public NotificationButtonView(Context context) {
             super(context);
         }
         public void onDraw(Canvas canvas) {
+            if(notificationButtonController!=null) {
+                notificationButtonController.draw(canvas,paint);
+                if(isAnimated) {
+                    notificationButtonController.startMoving();
+                    if(notificationButtonController.stop()) {
+                        isAnimated = false;
+                    }
+                    try {
+                        Thread.sleep(50);
+                        invalidate();
+                    }
+                    catch (Exception  ex) {
 
+                    }
+                }
+            }
         }
         public boolean onTouchEvent(MotionEvent event) {
-            if(event.getAction() == MotionEvent.ACTION_DOWN) {
-
+            if(event.getAction() == MotionEvent.ACTION_DOWN && notificationButtonController!=null && !isAnimated) {
+                notificationButtonController.startMoving();
+                isAnimated = true;
+                postInvalidate();
             }
             return true;
         }
