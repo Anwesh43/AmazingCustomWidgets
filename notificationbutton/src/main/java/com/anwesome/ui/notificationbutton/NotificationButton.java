e2878c00f10e4ab423544cded7a36bbdd37e93d3
Anwesh43 <anweshthecool0@gmail.com>
2017-04-09 14:26:37 +0530

    Adding NoificationButtonView

--- notificationbutton/src/main/java/com/anwesome/ui/notificationbutton/NotificationButton.java
+++ notificationbutton/src/main/java/com/anwesome/ui/notificationbutton/NotificationButton.java
@@ -1,16 +1,37 @@
 package com.anwesome.ui.notificationbutton;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.MotionEvent;
+import android.view.View;
 
 /**
  * Created by anweshmishra on 09/04/17.
  */
 public class NotificationButton {
-    private Activity activity ;
+    private Activity activity;
+    private NotificationButtonView notificationButtonView;
     public NotificationButton(Activity activity) {
         this.activity = activity;
     }
     public void show() {
-        
+        if(notificationButtonView == null) {
+            notificationButtonView = new NotificationButtonView(activity);
+        }
+    }
+    private class NotificationButtonView extends View {
+        public NotificationButtonView(Context context) {
+            super(context);
+        }
+        public void onDraw(Canvas canvas) {
+
+        }
+        public boolean onTouchEvent(MotionEvent event) {
+            if(event.getAction() == MotionEvent.ACTION_DOWN) {
+
+            }
+            return true;
+        }
     }
 }
