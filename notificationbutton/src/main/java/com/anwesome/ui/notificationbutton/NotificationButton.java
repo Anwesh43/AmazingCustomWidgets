cbbc15a8652491847e51b190bc2bb3fcd2ec64e5
Anwesh43 <anweshthecool0@gmail.com>
2017-04-09 14:21:46 +0530

    Adding mehtods that will exposed outside the module

---
+++ notificationbutton/src/main/java/com/anwesome/ui/notificationbutton/NotificationButton.java
@@ -0,0 +1,16 @@
+package com.anwesome.ui.notificationbutton;
+
+import android.app.Activity;
+
+/**
+ * Created by anweshmishra on 09/04/17.
+ */
+public class NotificationButton {
+    private Activity activity ;
+    public NotificationButton(Activity activity) {
+        this.activity = activity;
+    }
+    public void show() {
+        
+    }
+}
