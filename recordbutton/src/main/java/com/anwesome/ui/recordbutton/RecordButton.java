9c6e1319a31b2b94f6ed48dd25cfbb5a28f47df3
Anwesh43 <anweshthecool0@gmail.com>
2017-04-10 00:34:41 +0530

    Adding recordbutton class exposing methods outside the module

---
+++ recordbutton/src/main/java/com/anwesome/ui/recordbutton/RecordButton.java
@@ -0,0 +1,16 @@
+package com.anwesome.ui.recordbutton;
+
+import android.app.Activity;
+
+/**
+ * Created by anweshmishra on 10/04/17.
+ */
+public class RecordButton {
+    private Activity activity;
+    public RecordButton(Activity activity) {
+        this.activity = activity;
+    }
+    public void show() {
+
+    }
+}
