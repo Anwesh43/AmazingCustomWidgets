85770e1f286df64f1fa64cec8863121f85e5a574
Anwesh43 <anweshthecool0@gmail.com>
2017-04-11 03:44:27 +0530

    Adding mutebutton class

---
+++ mutebutton/src/main/java/com/anwesome/ui/mutebutton/MuteButton.java
@@ -0,0 +1,16 @@
+package com.anwesome.ui.mutebutton;
+
+import android.app.Activity;
+
+/**
+ * Created by anweshmishra on 11/04/17.
+ */
+public class MuteButton {
+    private Activity activity;
+    public MuteButton(Activity activity) {
+        this.activity = activity;
+    }
+    public void show() {
+
+    }
+}
