d70bd905878fb01e9f0cb95f1400a481e6cadf97
Anwesh43 <anweshthecool0@gmail.com>
2017-04-11 03:47:51 +0530

    Adding mutebuttonview

--- mutebutton/src/main/java/com/anwesome/ui/mutebutton/MuteButton.java
+++ mutebutton/src/main/java/com/anwesome/ui/mutebutton/MuteButton.java
@@ -1,6 +1,10 @@
 package com.anwesome.ui.mutebutton;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.MotionEvent;
+import android.view.View;
 
 /**
  * Created by anweshmishra on 11/04/17.
@@ -12,5 +16,29 @@ public class MuteButton {
     }
     public void show() {
 
+    }
+    private class MuteButtonView extends View {
+        private boolean isAnimated = false;
+        public MuteButtonView(Context context) {
+            super(context);
+        }
+        public void onDraw(Canvas canvas) {
+            if(isAnimated) {
+                try {
+                    Thread.sleep(50);
+                    invalidate();
+                }
+                catch (Exception ex) {
+
+                }
+            }
+        }
+        public boolean onTouchEvent(MotionEvent event) {
+            if(event.getAction() == MotionEvent.ACTION_DOWN && !isAnimated) {
+                isAnimated = true;
+                postInvalidate();
+            }
+            return true;
+        }
     }
 }
