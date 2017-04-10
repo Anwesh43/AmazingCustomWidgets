c0082a02bc641404b7519257003b56a0d503fbb6
Anwesh43 <anweshthecool0@gmail.com>
2017-04-11 03:39:24 +0530

    Adding animationcontroller

---
+++ mutebutton/src/main/java/com/anwesome/ui/mutebutton/AnimationController.java
@@ -0,0 +1,26 @@
+package com.anwesome.ui.mutebutton;
+
+/**
+ * Created by anweshmishra on 11/04/17.
+ */
+public class AnimationController {
+    public float deg = 0,dir = 0;
+    public float getDeg() {
+        return deg;
+    }
+    public boolean stop() {
+        return dir == 0;
+    }
+    public void move() {
+        deg+=dir*20;
+        checkEdgeCondition();
+    }
+    private void checkEdgeCondition() {
+        if(deg%180 == 0) {
+            dir = 0;
+        }
+    }
+    public void startMoving() {
+        deg = deg == 0?1:-1;
+    }
+}
