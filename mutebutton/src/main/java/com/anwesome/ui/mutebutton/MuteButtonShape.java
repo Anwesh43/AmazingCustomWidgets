d2323b2164f611b1f6a96a071a013cb267e383ed
Anwesh43 <anweshthecool0@gmail.com>
2017-04-11 03:43:19 +0530

    drawing mutebuttonshape

--- mutebutton/src/main/java/com/anwesome/ui/mutebutton/MuteButtonShape.java
+++ mutebutton/src/main/java/com/anwesome/ui/mutebutton/MuteButtonShape.java
@@ -1,7 +1,38 @@
 package com.anwesome.ui.mutebutton;
 
+import android.graphics.*;
+
 /**
  * Created by anweshmishra on 11/04/17.
  */
 public class MuteButtonShape {
+    private float x,y,size;
+    private AnimationController animationController;
+    public MuteButtonShape(float x,float y,float size) {
+        this.x = x;
+        this.y = y;
+        this.size = size;
+        animationController = new AnimationController();
+    }
+    public void draw(Canvas canvas, Paint paint) {
+        canvas.save();
+        canvas.translate(x,y);
+        canvas.rotate(animationController.getDeg());
+        DrawingUtil.drawBlackCircle(canvas,paint,size);
+        DrawingUtil.drawWhiteArc(canvas,paint,size);
+        DrawingUtil.drawEllipse(canvas,paint,size);
+        canvas.restore();
+    }
+    public void update() {
+        animationController.move();
+    }
+    public void handleTap() {
+        animationController.startMoving();
+    }
+    public boolean stop() {
+        return animationController.stop();
+    }
+    public int hashCode() {
+        return (int)(x+y+size+animationController.getDeg());
+    }
 }
