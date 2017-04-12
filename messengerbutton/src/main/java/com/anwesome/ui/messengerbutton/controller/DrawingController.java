e17db48ba4a33c65b58764f4595e408a08bdcbff
Anwesh43 <anweshthecool0@gmail.com>
2017-04-13 00:54:28 +0530

    Adding DrawingController

---
+++ messengerbutton/src/main/java/com/anwesome/ui/messengerbutton/controller/DrawingController.java
@@ -0,0 +1,39 @@
+package com.anwesome.ui.messengerbutton.controller;
+
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.RectF;
+
+/**
+ * Created by anweshmishra on 13/04/17.
+ */
+public class DrawingController {
+    private float x,y,size;
+    public DrawingController(float x,float y,float size) {
+        this.x = x;
+        this.y = y;
+        this.size = size;
+    }
+    public void draw(Canvas canvas, Paint paint) {
+        canvas.save();
+        canvas.translate(x,y);
+        drawPath(canvas,paint);
+        canvas.restore();
+    }
+    private void drawPath(Canvas canvas,Paint paint) {
+        paint.setColor(Color.parseColor("#00E676"));
+        Path path = new Path();
+        path.addArc(new RectF(-size/2,-size/2,size/2,size/2),0,120);
+        lineToPath(path,size/2+size/6,135);
+        lineToPath(path,size/2,150);
+        path.addArc(new RectF(-size/2,-size/2,size/2,size/2),150,210);
+        canvas.drawPath(path,paint);
+    }
+    private void lineToPath(Path path,float r,float theta) {
+        float x = r*(float)(Math.cos(theta*Math.PI/180)),y = r*(float)(Math.sin(theta*Math.PI/180));
+        path.lineTo(x,y);
+    }
+}
+
