9fde51d5a96bb540afad7df472eb9eb121b47576
Anwesh43 <anweshthecool0@gmail.com>
2017-04-13 00:40:24 +0530

    Adding MessengerButton module

---
+++ messengerbutton/src/main/java/com/anwesome/ui/messengerbutton/MessengerButton.java
@@ -0,0 +1,12 @@
+package com.anwesome.ui.messengerbutton;
+
+import android.app.Activity;
+import android.content.Context;
+
+/**
+ * Created by anweshmishra on 13/04/17.
+ */
+public class MessengerButton {
+    public MessengerButton(Activity activity) {
+    }
+}
