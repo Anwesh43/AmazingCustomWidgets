4bac23413cec0e5db862db4f67bc25eef035d024
Anwesh43 <anweshthecool0@gmail.com>
2017-04-12 13:51:09 +0530

    move and draw fullscreenbuttonshape in fullscreenbuttonvieW

--- fullscreenbutton/src/main/java/com/anwesome/ui/fullscreenbutton/FullScreenButton.java
+++ fullscreenbutton/src/main/java/com/anwesome/ui/fullscreenbutton/FullScreenButton.java
@@ -3,6 +3,7 @@ package com.anwesome.ui.fullscreenbutton;
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -11,6 +12,7 @@ import android.view.View;
  */
 public class FullScreenButton {
     private Activity activity;
+    private FullScreenButtonShape fullScreenButtonShape;
     private FullScreenButtonView fullScreenButtonView;
     public FullScreenButton(Activity activity) {
         this.activity = activity;
@@ -21,13 +23,35 @@ public class FullScreenButton {
         }
     }
     private class FullScreenButtonView extends View {
+        private boolean isAnimated = false;
+        private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
         public FullScreenButtonView(Context context) {
             super(context);
         }
         public void onDraw(Canvas canvas) {
+            if(fullScreenButtonShape!=null) {
+                fullScreenButtonShape.draw(canvas,paint);
+                if(isAnimated) {
+                    fullScreenButtonShape.move();
+                    if(fullScreenButtonShape.stop()) {
+                        isAnimated = false;
+                    }
+                    try {
+                        Thread.sleep(50);
+                        invalidate();
+                    }
+                    catch (Exception ex) {
 
+                    }
+                }
+            }
         }
         public boolean onTouchEvent(MotionEvent event) {
+            if(event.getAction() == MotionEvent.ACTION_DOWN && !isAnimated && fullScreenButtonShape!=null) {
+                fullScreenButtonShape.handleTap();
+                isAnimated = true;
+                postInvalidate();
+            }
             return true;
         }
     }
