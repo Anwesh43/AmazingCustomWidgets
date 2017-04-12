d21efad1293d415858a10f4905476eb5405d5f0b
Anwesh43 <anweshthecool0@gmail.com>
2017-04-13 01:30:05 +0530

    Adding AnimationController

---
+++ messengerbutton/src/main/java/com/anwesome/ui/messengerbutton/controller/AnimationController.java
@@ -0,0 +1,51 @@
+package com.anwesome.ui.messengerbutton.controller;
+
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * Created by anweshmishra on 13/04/17.
+ */
+public class AnimationController {
+    private boolean isAnimated = false;
+    private View view;
+    private AnimationControllerListener controllerListener;
+    public void setControllerListener(AnimationControllerListener animationControllerListener) {
+        this.controllerListener = animationControllerListener;
+    }
+    public AnimationController(View view,AnimationControllerListener animationControllerListener) {
+        this.view = view;
+        this.controllerListener = animationControllerListener;
+    }
+    public void update() {
+        if(!isAnimated) {
+            if(controllerListener!=null) {
+                controllerListener.animate();
+                if(controllerListener.stop()) {
+                    isAnimated = false;
+                }
+            }
+            try {
+                Thread.sleep(50);
+                view.invalidate();
+            }
+            catch (Exception ex) {
+
+            }
+        }
+    }
+    public void handleTap(int action) {
+        if(action == MotionEvent.ACTION_DOWN) {
+            isAnimated = true;
+            if(controllerListener!=null) {
+                controllerListener.handleTap();
+            }
+            view.postInvalidate();
+        }
+    }
+    public interface AnimationControllerListener {
+        void animate();
+        boolean stop();
+        void handleTap();
+    }
+}
