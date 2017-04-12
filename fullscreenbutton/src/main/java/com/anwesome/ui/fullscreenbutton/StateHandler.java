0381ecf48a880e90de16a32bfa21cf8d18f86174
Anwesh43 <anweshthecool0@gmail.com>
2017-04-12 10:30:19 +0530

    Adding StateHandler for FullScreenButton

--- fullscreenbutton/src/main/java/com/anwesome/ui/fullscreenbutton/StateHandler.java
+++ fullscreenbutton/src/main/java/com/anwesome/ui/fullscreenbutton/StateHandler.java
@@ -4,4 +4,24 @@ package com.anwesome.ui.fullscreenbutton;
  * Created by anweshmishra on 12/04/17.
  */
 public class StateHandler {
+    private float deg = 0,dir = 0,scale = 0.5f;
+    public void move() {
+        deg+=dir*36;
+        scale+=0.1f*dir;
+        if(deg>=180 || deg<=0) {
+            dir = 0;
+        }
+    }
+    public float getDeg() {
+        return deg;
+    }
+    public float getDir() {
+        return dir;
+    }
+    public float getScale() {
+        return scale;
+    }
+    public boolean stop () {
+        return dir == 0;
+    }
 }
