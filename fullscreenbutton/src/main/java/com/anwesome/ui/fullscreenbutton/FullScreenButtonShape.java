3d264107c34ae8836de6395a6f528e13ff7be0be
Anwesh43 <anweshthecool0@gmail.com>
2017-04-12 10:33:49 +0530

    Using StateHandler in FullScreenButtonShape

--- fullscreenbutton/src/main/java/com/anwesome/ui/fullscreenbutton/FullScreenButtonShape.java
+++ fullscreenbutton/src/main/java/com/anwesome/ui/fullscreenbutton/FullScreenButtonShape.java
@@ -1,7 +1,29 @@
 package com.anwesome.ui.fullscreenbutton;
 
+import android.graphics.Canvas;
+import android.graphics.Paint;
+
 /**
  * Created by anweshmishra on 12/04/17.
  */
 public class FullScreenButtonShape {
+    private StateHandler stateHandler = new StateHandler();
+    private float x,y,size;
+    public FullScreenButtonShape(float x,float y,float size) {
+        this.x = x;
+        this.y = y;
+        this.size = size;
+    }
+    public void draw(Canvas canvas, Paint paint) {
+
+    }
+    public void handleTap() {
+        stateHandler.startMoving();
+    }
+    public void move() {
+        stateHandler.move();
+    }
+    public boolean stop() {
+        return stateHandler.stop();
+    }
 }
