bcb4b32951a6c7b00abc2b13d0c1287c7edcdc0c
Anwesh43 <anweshthecool0@gmail.com>
2017-04-13 01:48:47 +0530

    Adding MessengerButtonView

--- messengerbutton/src/main/java/com/anwesome/ui/messengerbutton/MessengerButton.java
+++ messengerbutton/src/main/java/com/anwesome/ui/messengerbutton/MessengerButton.java
@@ -2,16 +2,39 @@ package com.anwesome.ui.messengerbutton;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Canvas;
+import android.view.MotionEvent;
+import android.view.View;
+
+import com.anwesome.ui.messengerbutton.controller.AnimationController;
+
 
 /**
  * Created by anweshmishra on 13/04/17.
  */
 public class MessengerButton {
     private Activity activity;
+    private MessengerButtonShape messengerButtonShape;
+    private MessengerButtonView messengerButtonView;
+    private AnimationController animationController;
     public MessengerButton(Activity activity) {
         this.activity = activity;
     }
     public void show() {
+        if(messengerButtonView == null) {
+            messengerButtonView = new MessengerButtonView(activity);
+            
+        }
+    }
+    private class MessengerButtonView extends View {
+        public MessengerButtonView(Context context) {
+            super(context);
+        }
+        public void onDraw(Canvas canvas) {
 
+        }
+        public boolean onTouchEvent(MotionEvent event) {
+            return true;
+        }
     }
 }
