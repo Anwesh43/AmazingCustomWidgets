703621646f43c25df0467a3f1bd50b0cfe57c9ec
Anwesh43 <anweshthecool0@gmail.com>
2017-04-13 00:43:10 +0530

    Adding MessengerButton class

--- messengerbutton/src/main/java/com/anwesome/ui/messengerbutton/MessengerButton.java
+++ messengerbutton/src/main/java/com/anwesome/ui/messengerbutton/MessengerButton.java
@@ -7,6 +7,11 @@ import android.content.Context;
  * Created by anweshmishra on 13/04/17.
  */
 public class MessengerButton {
+    private Activity activity;
     public MessengerButton(Activity activity) {
+        this.activity = activity;
+    }
+    public void show() {
+
     }
 }
