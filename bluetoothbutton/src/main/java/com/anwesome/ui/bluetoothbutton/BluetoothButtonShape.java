4e92956a3f23f3c145acd86e8835854b62ea226d
Anwesh43 <anweshthecool0@gmail.com>
2017-04-14 07:43:19 +0530

    Using statecontroller and drawingcontroller in bluetoothbuttonshape

--- bluetoothbutton/src/main/java/com/anwesome/ui/bluetoothbutton/BluetoothButtonShape.java
+++ bluetoothbutton/src/main/java/com/anwesome/ui/bluetoothbutton/BluetoothButtonShape.java
@@ -3,23 +3,28 @@ package com.anwesome.ui.bluetoothbutton;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 
+import com.anwesome.ui.bluetoothbutton.controller.DrawingController;
+import com.anwesome.ui.bluetoothbutton.controller.StateController;
+
 /**
  * Created by anweshmishra on 14/04/17.
  */
 public class BluetoothButtonShape {
+    private DrawingController drawingController;
+    private StateController stateController = new StateController();
     public BluetoothButtonShape(float x,float y,float size) {
-
+        drawingController = new DrawingController(x,y,size,stateController);
     }
     public void draw(Canvas canvas, Paint paint) {
-
+        drawingController.draw(canvas,paint);
     }
     public void update() {
-
+        stateController.move();
     }
     public boolean stop() {
-        return false;
+        return stateController.stop();
     }
     public void startMoving() {
-
+        stateController.startMoving();
     }
 }
