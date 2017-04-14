d9a17c8676e5975c5463cc61c230f34a248c5b13
Anwesh43 <anweshthecool0@gmail.com>
2017-04-14 07:17:54 +0530

    Adding skeleton framework of BluetoothButtonShape

---
+++ bluetoothbutton/src/main/java/com/anwesome/ui/bluetoothbutton/BluetoothButtonShape.java
@@ -0,0 +1,25 @@
+package com.anwesome.ui.bluetoothbutton;
+
+import android.graphics.Canvas;
+import android.graphics.Paint;
+
+/**
+ * Created by anweshmishra on 14/04/17.
+ */
+public class BluetoothButtonShape {
+    public BluetoothButtonShape(float x,float y,float size) {
+
+    }
+    public void draw(Canvas canvas, Paint paint) {
+
+    }
+    public void update() {
+
+    }
+    public boolean stop() {
+        return false;
+    }
+    public void startMoving() {
+
+    }
+}
