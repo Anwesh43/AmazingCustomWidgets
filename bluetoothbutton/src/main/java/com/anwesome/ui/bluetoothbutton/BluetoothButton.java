3fda9f30a098a689bf801fbab71251271d17842b
Anwesh43 <anweshthecool0@gmail.com>
2017-04-14 07:44:41 +0530

    Adding bluetoothbutton class

---
+++ bluetoothbutton/src/main/java/com/anwesome/ui/bluetoothbutton/BluetoothButton.java
@@ -0,0 +1,16 @@
+package com.anwesome.ui.bluetoothbutton;
+
+import android.app.Activity;
+
+/**
+ * Created by anweshmishra on 14/04/17.
+ */
+public class BluetoothButton {
+    private Activity activity;
+    public BluetoothButton(Activity activity) {
+        this.activity = activity;
+    }
+    public void show() {
+        
+    }
+}
