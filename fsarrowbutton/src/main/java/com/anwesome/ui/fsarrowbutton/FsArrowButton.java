9d79ac535d6d731b5c49ff98007f18529cc54e3d
Anwesh43 <anweshthecool0@gmail.com>
2017-04-15 15:08:09 +0530

    Adding FsArrowButton

--- fsarrowbutton/src/main/java/com/anwesome/ui/fsarrowbutton/FsArrowButton.java
+++ fsarrowbutton/src/main/java/com/anwesome/ui/fsarrowbutton/FsArrowButton.java
@@ -1,7 +1,18 @@
 package com.anwesome.ui.fsarrowbutton;
 
+import android.app.Activity;
+
 /**
  * Created by anweshmishra on 15/04/17.
  */
 public class FsArrowButton {
+    private Activity activity;
+    private FsArrowButtonShape fsArrowButtonShape;
+    public FsArrowButton(Activity activity) {
+        this.activity = activity;
+
+    }
+    public void show() {
+
+    }
 }
