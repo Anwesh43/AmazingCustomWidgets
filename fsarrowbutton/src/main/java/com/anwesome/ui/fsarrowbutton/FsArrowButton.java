7feb85f098ca487b2349c8d3468fb32fad79c1b3
Anwesh43 <anweshthecool0@gmail.com>
2017-04-15 15:12:01 +0530

    Adding FsArrowButtonView to fsarrowbutton

--- fsarrowbutton/src/main/java/com/anwesome/ui/fsarrowbutton/FsArrowButton.java
+++ fsarrowbutton/src/main/java/com/anwesome/ui/fsarrowbutton/FsArrowButton.java
@@ -1,6 +1,12 @@
 package com.anwesome.ui.fsarrowbutton;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.MotionEvent;
+import android.view.View;
+
+import com.anwesome.ui.fsarrowbutton.controllers.AnimationController;
 
 /**
  * Created by anweshmishra on 15/04/17.
@@ -8,11 +14,26 @@ import android.app.Activity;
 public class FsArrowButton {
     private Activity activity;
     private FsArrowButtonShape fsArrowButtonShape;
+    private FsArrowButtonView view;
+    private AnimationController animationController;
     public FsArrowButton(Activity activity) {
         this.activity = activity;
-
     }
     public void show() {
+        if(view == null) {
+            view = new FsArrowButtonView(activity);
 
+        }
+    }
+    private class FsArrowButtonView extends View {
+        public FsArrowButtonView(Context context) {
+            super(context);
+        }
+        public void onDraw(Canvas canvas) {
+            
+        }
+        public boolean onTouchEvent(MotionEvent event) {
+            return true;
+        }
     }
 }
