2e45b5e564ea9479444a645d5521e58eddea36e2
Anwesh43 <anweshthecool0@gmail.com>
2017-04-15 15:20:16 +0530

    Using FSArrowButtonShape in fsarrowbutton

--- fsarrowbutton/src/main/java/com/anwesome/ui/fsarrowbutton/FsArrowButton.java
+++ fsarrowbutton/src/main/java/com/anwesome/ui/fsarrowbutton/FsArrowButton.java
@@ -3,9 +3,13 @@ package com.anwesome.ui.fsarrowbutton;
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Point;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 
+import com.anwesome.ui.dimensionsutil.DimensionsUtil;
 import com.anwesome.ui.fsarrowbutton.controllers.AnimationController;
 
 /**
@@ -22,17 +26,24 @@ public class FsArrowButton {
     public void show() {
         if(view == null) {
             view = new FsArrowButtonView(activity);
-
+            Point dimension = DimensionsUtil.getDeviceDimension(activity);
+            int w = dimension.x;
+            activity.addContentView(view,new ViewGroup.LayoutParams(w/2,w/2));
+            fsArrowButtonShape = new FsArrowButtonShape(w/4);
+            animationController = new AnimationController(view,fsArrowButtonShape);
         }
     }
     private class FsArrowButtonView extends View {
+        private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
         public FsArrowButtonView(Context context) {
             super(context);
         }
         public void onDraw(Canvas canvas) {
-            
+            fsArrowButtonShape.draw(canvas,paint);
+            animationController.animate();
         }
         public boolean onTouchEvent(MotionEvent event) {
+            animationController.handleTap(event);
             return true;
         }
     }
