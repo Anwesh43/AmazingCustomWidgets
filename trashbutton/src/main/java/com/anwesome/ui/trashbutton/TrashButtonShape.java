880e39dc7321a1166ed91a70e33bd6e4bc956b9b
Anwesh43 <anweshthecool0@gmail.com>
2017-04-16 04:14:30 +0530

    Adding DrawingUtil in TrashButtonShape

--- trashbutton/src/main/java/com/anwesome/ui/trashbutton/TrashButtonShape.java
+++ trashbutton/src/main/java/com/anwesome/ui/trashbutton/TrashButtonShape.java
@@ -4,6 +4,7 @@ import android.graphics.Canvas;
 import android.graphics.Paint;
 
 import com.anwesome.ui.trashbutton.controller.StateController;
+import com.anwesome.ui.trashbutton.util.DrawingUtil;
 
 /**
  * Created by anweshmishra on 16/04/17.
@@ -11,7 +12,7 @@ import com.anwesome.ui.trashbutton.controller.StateController;
 public class TrashButtonShape {
     private StateController stateController = new StateController();
     public void draw(Canvas canvas, Paint paint,float w) {
-
+        DrawingUtil.renderTrashButton(canvas,paint,w,stateController.getDeg());
     }
     public void move() {
         stateController.move();
