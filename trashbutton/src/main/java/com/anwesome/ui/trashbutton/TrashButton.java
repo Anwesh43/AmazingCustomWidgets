59108ce2ffb95edb72842efccbafff6944b8ee63
Anwesh43 <anweshthecool0@gmail.com>
2017-04-16 04:20:42 +0530

    Adding TrashButton

---
+++ trashbutton/src/main/java/com/anwesome/ui/trashbutton/TrashButton.java
@@ -0,0 +1,17 @@
+package com.anwesome.ui.trashbutton;
+
+import android.app.Activity;
+import android.content.Context;
+
+/**
+ * Created by anweshmishra on 16/04/17.
+ */
+public class TrashButton {
+    private Activity activity;
+    public TrashButton(Activity activity) {
+        this.activity = activity;
+    }
+    public void show() {
+
+    }
+}
