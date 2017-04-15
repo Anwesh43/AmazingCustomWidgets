e0b2ebfac2927d8cc30a133943dca384cbfbba2d
Anwesh43 <anweshthecool0@gmail.com>
2017-04-16 03:59:28 +0530

    Adding TrashButtonShape

---
+++ trashbutton/src/main/java/com/anwesome/ui/trashbutton/TrashButtonShape.java
@@ -0,0 +1,25 @@
+package com.anwesome.ui.trashbutton;
+
+import android.graphics.Canvas;
+import android.graphics.Paint;
+
+/**
+ * Created by anweshmishra on 16/04/17.
+ */
+public class TrashButtonShape {
+    public TrashButtonShape(float w) {
+
+    }
+    public void draw(Canvas canvas, Paint paint) {
+
+    }
+    public void move() {
+
+    }
+    public boolean stop() {
+        return false;
+    }
+    public void startMoving() {
+
+    }
+}
