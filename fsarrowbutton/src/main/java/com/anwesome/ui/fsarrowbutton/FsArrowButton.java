4d6a76cf5bfd3de104c2683f33c72f98dee8fd98
Anwesh43 <anweshthecool0@gmail.com>
2017-04-15 15:01:58 +0530

    using controllerin fsarrowbuttonshape

---
+++ fsarrowbutton/src/main/java/com/anwesome/ui/fsarrowbutton/FsArrowButton.java
@@ -0,0 +1,7 @@
+package com.anwesome.ui.fsarrowbutton;
+
+/**
+ * Created by anweshmishra on 15/04/17.
+ */
+public class FsArrowButton {
+}
--- fsarrowbutton/src/main/java/com/anwesome/ui/fsarrowbutton/FsArrowButtonShape.java
+++ fsarrowbutton/src/main/java/com/anwesome/ui/fsarrowbutton/FsArrowButtonShape.java
@@ -2,23 +2,28 @@ package com.anwesome.ui.fsarrowbutton;
 
 import android.graphics.*;
 
+import com.anwesome.ui.fsarrowbutton.controllers.DrawingController;
+import com.anwesome.ui.fsarrowbutton.controllers.StateController;
+
 /**
  * Created by anweshmishra on 15/04/17.
  */
 public class FsArrowButtonShape {
+    private StateController stateController = new StateController();
+    private DrawingController drawingController;
     public FsArrowButtonShape(float size) {
-
+        drawingController = new DrawingController(size,size,size,stateController);
     }
     public void draw(Canvas canvas, Paint paint) {
-
+        drawingController.draw(canvas,paint);
     }
     public void update() {
-
+        stateController.move();
     }
     public void startMoving() {
-
+        stateController.startMoving();
     }
     public boolean stop() {
-        return false;
+        return stateController.stop();
     }
 }
