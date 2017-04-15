85e91d30f36a2cd548c3393896730f0805b68615
Anwesh43 <anweshthecool0@gmail.com>
2017-04-15 13:07:49 +0530

    Adding FsArrowButtonShape

---
+++ fsarrowbutton/src/main/java/com/anwesome/ui/fsarrowbutton/FsArrowButtonShape.java
@@ -0,0 +1,24 @@
+package com.anwesome.ui.fsarrowbutton;
+
+import android.graphics.*;
+
+/**
+ * Created by anweshmishra on 15/04/17.
+ */
+public class FsArrowButtonShape {
+    public FsArrowButtonShape(float size) {
+
+    }
+    public void draw(Canvas canvas, Paint paint) {
+
+    }
+    public void update() {
+
+    }
+    public void startMoving() {
+
+    }
+    public boolean stop() {
+        return false;
+    }
+}
