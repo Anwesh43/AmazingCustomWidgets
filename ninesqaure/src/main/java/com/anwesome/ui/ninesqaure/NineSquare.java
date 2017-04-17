18358554bb80b9501c548de42bbbbc5bc0c57bd3
Anwesh43 <anweshthecool0@gmail.com>
2017-04-17 12:39:58 +0530

    Adding NineSquare class

--- ninesqaure/src/main/java/com/anwesome/ui/ninesqaure/NineSquare.java
+++ ninesqaure/src/main/java/com/anwesome/ui/ninesqaure/NineSquare.java
@@ -1,7 +1,16 @@
 package com.anwesome.ui.ninesqaure;
 
+import android.app.Activity;
+
 /**
  * Created by anweshmishra on 17/04/17.
  */
 public class NineSquare {
+    private Activity activity;
+    public NineSquare(Activity activity) {
+        this.activity = activity;
+    }
+    public void show() {
+        
+    }
 }
