df091ee5effee4c89afc2463cdf816a6eb3f127f
Anwesh43 <anweshthecool0@gmail.com>
2017-04-17 12:04:39 +0530

    Adding Drawing Util in NineSquare

--- ninesqaure/src/main/java/com/anwesome/ui/ninesqaure/DrawingUtil.java
+++ ninesqaure/src/main/java/com/anwesome/ui/ninesqaure/DrawingUtil.java
@@ -1,7 +1,21 @@
 package com.anwesome.ui.ninesqaure;
 
+import android.graphics.*;
 /**
  * Created by anweshmishra on 17/04/17.
  */
 public class DrawingUtil {
+    public static void drawNineSquare(Canvas canvas, Paint paint,float x,float y,float w) {
+        paint.setColor(Color.parseColor("#1565C0"));
+        canvas.save();
+        canvas.translate(x,y);
+        float sqX = -w/2+w/18+w/9,sqY = -w/2+w/18+w/9;
+        for(int i=0;i<9;i++){
+            canvas.save();
+            canvas.translate(sqX+(w/6)*(i%3),sqY+(w/6)*(i/3));
+            canvas.drawRect(new RectF(-w/12,-w/12,w/12,w/12),paint);
+            canvas.restore();
+        }
+        canvas.restore();
+    }
 }
