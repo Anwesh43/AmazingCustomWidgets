8d7e13d4295302e2e803b094c26a3dde889e1b5f
Anwesh43 <anweshthecool0@gmail.com>
2017-04-17 12:44:19 +0530

    Adding ninesquareview

--- ninesqaure/src/main/java/com/anwesome/ui/ninesqaure/NineSquare.java
+++ ninesqaure/src/main/java/com/anwesome/ui/ninesqaure/NineSquare.java
@@ -1,6 +1,10 @@
 package com.anwesome.ui.ninesqaure;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.MotionEvent;
+import android.view.View;
 
 /**
  * Created by anweshmishra on 17/04/17.
@@ -11,6 +15,17 @@ public class NineSquare {
         this.activity = activity;
     }
     public void show() {
-        
+
+    }
+    private class NineSquareView extends View {
+        public NineSquareView(Context context) {
+            super(context);
+        }
+        public void onDraw(Canvas canvas) {
+
+        }
+        public boolean onTouchEvent(MotionEvent event) {
+            return true;
+        }
     }
 }
