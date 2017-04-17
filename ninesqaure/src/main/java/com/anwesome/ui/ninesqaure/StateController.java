192bce58794a77f82a36ed29c92394fd3e94ee71
Anwesh43 <anweshthecool0@gmail.com>
2017-04-17 12:10:52 +0530

    Adding StateController

---
+++ ninesqaure/src/main/java/com/anwesome/ui/ninesqaure/StateController.java
@@ -0,0 +1,32 @@
+package com.anwesome.ui.ninesqaure;
+
+/**
+ * Created by anweshmishra on 17/04/17.
+ */
+public class StateController {
+    private float deg = 0,dir = 0,gap=0,maxGap = 0;
+    public StateController(float maxGap) {
+        this.gap = maxGap;
+        this.maxGap = maxGap;
+    }
+    public void move() {
+        deg+=dir*18;
+        gap-=dir*maxGap/5;
+        if((deg>=90 && gap<=0)) {
+            dir = 0;
+            deg = 90;
+            gap = 0;
+        }
+        if(gap>=maxGap && deg<=0) {
+            dir = 0;
+            deg = 90;
+            gap = 0;
+        }
+    }
+    public void startMoving() {
+        dir = deg == 0?1:-1;
+    }
+    public boolean stopped() {
+        return dir == 0;
+    }
+}
