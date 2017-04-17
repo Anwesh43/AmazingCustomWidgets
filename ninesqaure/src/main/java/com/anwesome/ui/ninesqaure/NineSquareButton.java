43cb27f1ef029c819e54c4ac79b255ad4f73cc8a
Anwesh43 <anweshthecool0@gmail.com>
2017-04-17 12:37:11 +0530

    Using StateController and Drawing Util in ninesquarebutton

--- ninesqaure/src/main/java/com/anwesome/ui/ninesqaure/NineSquareButton.java
+++ ninesqaure/src/main/java/com/anwesome/ui/ninesqaure/NineSquareButton.java
@@ -1,6 +1,5 @@
 package com.anwesome.ui.ninesqaure;
 
-import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 
@@ -9,21 +8,23 @@ import android.graphics.Paint;
  */
 public class NineSquareButton {
     private float x,y,w;
+    private StateController stateController;
     public NineSquareButton(float w) {
-        this.x = w/2;
-        this.y = w/2;
-        this.w = w/2;
+        this.x = w;
+        this.y = w;
+        this.w = w;
+        this.stateController = new StateController(w);
     }
     public void draw(Canvas canvas, Paint paint) {
-
+        DrawingUtil.drawNineSquare(canvas,paint,stateController,x,y,w);
     }
     public void move() {
-
+        stateController.move();
     }
     public void startMoving() {
-
+        stateController.startMoving();
     }
     public boolean stopped() {
-        return false;
+        return stateController.stopped();
     }
 }
