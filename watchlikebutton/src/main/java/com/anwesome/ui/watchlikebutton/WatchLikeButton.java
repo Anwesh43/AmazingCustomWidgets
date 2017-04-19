57ee50d9da641f21dad23e1df37b5f5bcd29ab11
Anwesh43 <anweshthecool0@gmail.com>
2017-04-19 11:47:57 +0530

    Creating and adding a view to activity

--- watchlikebutton/src/main/java/com/anwesome/ui/watchlikebutton/WatchLikeButton.java
+++ watchlikebutton/src/main/java/com/anwesome/ui/watchlikebutton/WatchLikeButton.java
@@ -1,16 +1,45 @@
 package com.anwesome.ui.watchlikebutton;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by anweshmishra on 19/04/17.
  */
 public class WatchLikeButton {
     private Activity activity;
+    private WatchLikeView watchLikeView;
+    private AnimationController animationController;
+    private WatchLikeShape watchLikeShape = new WatchLikeShape();
     public WatchLikeButton(Activity activity) {
         this.activity = activity;
     }
     public void show() {
-
+        if(watchLikeView == null) {
+            watchLikeView = new WatchLikeView(activity);
+            activity.addContentView(watchLikeView,new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+            animationController = new AnimationController(watchLikeView,watchLikeShape);
+        }
+    }
+    private class WatchLikeView extends View {
+        private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        public WatchLikeView(Context context) {
+            super(context);
+        }
+        public void onDraw(Canvas canvas) {
+            watchLikeShape.draw(canvas,paint,canvas.getWidth());
+            animationController.animate();
+        }
+        public boolean onTouchEvent(MotionEvent event) {
+            if(event.getAction() == MotionEvent.ACTION_DOWN) {
+                animationController.startAnimating();
+            }
+            return true;
+        }
     }
 }
