34f24fd010fbdac64b07621ca39afdf3cbdb8bba
Anwesh43 <anweshthecool0@gmail.com>
2017-04-19 11:14:48 +0530

    Adding watchlikeshape

---
+++ watchlikebutton/src/main/java/com/anwesome/ui/watchlikebutton/WatchLikeShape.java
@@ -0,0 +1,25 @@
+package com.anwesome.ui.watchlikebutton;
+
+import android.graphics.Canvas;
+import android.graphics.Paint;
+
+/**
+ * Created by anweshmishra on 19/04/17.
+ */
+public class WatchLikeShape {
+    public WatchLikeShape(float w) {
+
+    }
+    public void draw(Canvas canvas, Paint paint) {
+
+    }
+    public void move() {
+
+    }
+    public void startMoving() {
+
+    }
+    public boolean stop() {
+        return false;
+    }
+}
