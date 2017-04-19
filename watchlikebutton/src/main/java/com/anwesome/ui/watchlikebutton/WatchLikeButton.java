8f4e608b0bc55fa5a0be61819edace3d89b0f98d
Anwesh43 <anweshthecool0@gmail.com>
2017-04-19 11:09:20 +0530

    Adding WatchLikeButton class

--- watchlikebutton/src/main/java/com/anwesome/ui/watchlikebutton/WatchLikeButton.java
+++ watchlikebutton/src/main/java/com/anwesome/ui/watchlikebutton/WatchLikeButton.java
@@ -1,7 +1,16 @@
 package com.anwesome.ui.watchlikebutton;
 
+import android.app.Activity;
+
 /**
  * Created by anweshmishra on 19/04/17.
  */
 public class WatchLikeButton {
+    private Activity activity;
+    public WatchLikeButton(Activity activity) {
+        this.activity = activity;
+    }
+    public void show() {
+
+    }
 }
