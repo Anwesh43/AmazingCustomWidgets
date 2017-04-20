57fb4c3d2cb0988c211fd521f41c614a572d9e0c
Anwesh43 <anweshthecool0@gmail.com>
2017-04-20 06:37:20 +0530

    Adding ClockShape

---
+++ clockbutton/src/main/java/com/anwesome/ui/clockbutton/ClockShape.java
@@ -0,0 +1,22 @@
+package com.anwesome.ui.clockbutton;
+
+import android.graphics.Canvas;
+import android.graphics.Paint;
+
+/**
+ * Created by anweshmishra on 20/04/17.
+ */
+public class ClockShape {
+    public void draw(Canvas canvas, Paint paint,float size) {
+
+    }
+    public void move() {
+
+    }
+    public void startMoving() {
+
+    }
+    public boolean stopped() {
+        return false;
+    }
+}
