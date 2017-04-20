dbeeff277fef906182e16215f0f92192ee348998
Anwesh43 <anweshthecool0@gmail.com>
2017-04-20 06:45:41 +0530

    Adding ClockDrawingUtil

---
+++ clockbutton/src/main/java/com/anwesome/ui/clockbutton/utils/ClockDrawingUtil.java
@@ -0,0 +1,27 @@
+package com.anwesome.ui.clockbutton.utils;
+
+import android.graphics.Canvas;
+import android.graphics.Paint;
+
+/**
+ * Created by anweshmishra on 20/04/17.
+ */
+public class ClockDrawingUtil {
+    public static void drawClock(Canvas canvas, Paint paint,float w,float deg,int color) {
+        canvas.save();
+        canvas.translate(w,w);
+        paint.setStrokeWidth(w/30);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setColor(color);
+        canvas.drawCircle(0,0,w,paint);
+        canvas.save();
+        canvas.rotate(deg);
+        canvas.drawLine(0,0,0,-w/3,paint);
+        canvas.restore();
+        canvas.save();
+        canvas.rotate(deg/12);
+        canvas.drawLine(0,0,0,-w/5,paint);
+        canvas.restore();
+        canvas.restore();
+    }
+}
