3cdc2c53a36a80cc217f1d9ca8b516d87aadcc4c
Anwesh43 <anweshthecool0@gmail.com>
2017-04-20 06:33:06 +0530

    Adding ClockButton class

---
+++ clockbutton/src/main/java/com/anwesome/ui/clockbutton/ClockButton.java
@@ -0,0 +1,16 @@
+package com.anwesome.ui.clockbutton;
+
+import android.app.Activity;
+
+/**
+ * Created by anweshmishra on 20/04/17.
+ */
+public class ClockButton {
+    private Activity activity;
+    public ClockButton(Activity activity) {
+        this.activity = activity;
+    }
+    public void show() {
+
+    }
+}
