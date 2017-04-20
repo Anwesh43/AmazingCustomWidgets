57603da357e103127478f4f93d19f20a1c14735a
Anwesh43 <anweshthecool0@gmail.com>
2017-04-20 07:15:59 +0530

    Adding AnimationController

--- clockbutton/src/main/java/com/anwesome/ui/clockbutton/controllers/AnimationController.java
+++ clockbutton/src/main/java/com/anwesome/ui/clockbutton/controllers/AnimationController.java
@@ -1,7 +1,40 @@
 package com.anwesome.ui.clockbutton.controllers;
 
+import android.view.View;
+
+import com.anwesome.ui.clockbutton.ClockShape;
+
 /**
  * Created by anweshmishra on 20/04/17.
  */
 public class AnimationController {
+    private boolean isAnimated = false;
+    private View view;
+    private ClockShape clockShape;
+    public AnimationController(View view, ClockShape clockShape) {
+        this.view = view;
+        this.clockShape = clockShape;
+    }
+    public void animate() {
+        if(isAnimated) {
+            clockShape.move();
+            if(clockShape.stopped()) {
+                isAnimated = false;
+            }
+            try {
+                Thread.sleep(50);
+                view.invalidate();
+            }
+            catch (Exception ex) {
+
+            }
+        }
+    }
+    public void startAnimating() {
+        if(!isAnimated) {
+            clockShape.startMoving();
+            isAnimated = true;
+            view.postInvalidate();
+        }
+    }
 }
