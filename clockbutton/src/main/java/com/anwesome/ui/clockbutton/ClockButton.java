ba13ca4f92375da8dafe2be08d1e4b40e522ded8
Anwesh43 <anweshthecool0@gmail.com>
2017-04-20 07:29:32 +0530

    drawing clockshape and animationcontroller

--- clockbutton/src/main/java/com/anwesome/ui/clockbutton/ClockButton.java
+++ clockbutton/src/main/java/com/anwesome/ui/clockbutton/ClockButton.java
@@ -6,12 +6,16 @@ import android.graphics.*;
 import android.view.MotionEvent;
 import android.view.View;
 
+import com.anwesome.ui.clockbutton.controllers.AnimationController;
+
 /**
  * Created by anweshmishra on 20/04/17.
  */
 public class ClockButton {
     private Activity activity;
+    private AnimationController animationController;
     private ClockShape clockShape = new ClockShape();
+    private ClockButtonView clockButtonView;
     public void setColor(int color) {
         clockShape.setColor(color);
     }
@@ -19,7 +23,10 @@ public class ClockButton {
         this.activity = activity;
     }
     public void show() {
-
+        if(clockButtonView == null) {
+            clockButtonView = new ClockButtonView(activity);
+            animationController = new AnimationController(clockButtonView,clockShape);
+        }
     }
     private class ClockButtonView extends View {
         private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
@@ -27,9 +34,15 @@ public class ClockButton {
             super(context);
         }
         public void onDraw(Canvas canvas) {
-
+            if(clockShape != null && animationController != null) {
+                clockShape.draw(canvas,paint,canvas.getWidth()/2);
+                animationController.animate();
+            }
         }
         public boolean onTouchEvent(MotionEvent event) {
+            if(event.getAction() == MotionEvent.ACTION_DOWN) {
+                animationController.startAnimating();
+            }
             return true;
         }
     }
