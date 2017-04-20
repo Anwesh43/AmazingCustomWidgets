65068b27e2ec82a21e20994ccd4e235880de96c8
Anwesh43 <anweshthecool0@gmail.com>
2017-04-20 06:51:36 +0530

    Adding ClockMovementController

---
+++ clockbutton/src/main/java/com/anwesome/ui/clockbutton/controllers/ClockMovementController.java
@@ -0,0 +1,24 @@
+package com.anwesome.ui.clockbutton.controllers;
+
+/**
+ * Created by anweshmishra on 20/04/17.
+ */
+public class ClockMovementController {
+    private float deg = 0,dir = 0;
+    public float getDeg() {
+        return deg;
+    }
+    public void move() {
+        deg+=dir*10;
+        if(deg>=360) {
+            dir = 0;
+            deg = 0;
+        }
+    }
+    public void startMoving() {
+        dir = dir == 0?1:dir;
+    }
+    public boolean stopped() {
+        return dir == 0;
+    }
+}
