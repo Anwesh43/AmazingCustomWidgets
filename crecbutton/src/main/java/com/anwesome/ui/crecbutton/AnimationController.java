51e6ddad69c40a3b9671676a4147d572a446ae80
Anwesh43 <anweshthecool0@gmail.com>
2017-04-21 13:45:42 +0530

    Adding AnimationController

---
+++ crecbutton/src/main/java/com/anwesome/ui/crecbutton/AnimationController.java
@@ -0,0 +1,35 @@
+package com.anwesome.ui.crecbutton;
+
+import android.view.View;
+
+/**
+ * Created by anweshmishra on 21/04/17.
+ */
+public class AnimationController {
+    private boolean isAnimated = false;
+    private View view;
+    private CrecButtonShape crecButtonShape;
+    public AnimationController(View view,CrecButtonShape crecButtonShape) {
+        this.view = view;
+        this.crecButtonShape = crecButtonShape;
+    }
+    public void animate() {
+        if(isAnimated) {
+            crecButtonShape.update();
+            try {
+                Thread.sleep(50);
+                view.invalidate();
+            }
+            catch(Exception ex) {
+
+            }
+        }
+    }
+    public void startAnimating() {
+        if(!isAnimated) {
+            crecButtonShape.startUpdating();
+            isAnimated = true;
+            view.postInvalidate();
+        }
+    }
+}
