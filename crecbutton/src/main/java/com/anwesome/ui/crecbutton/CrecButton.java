3df444d879103993aed89cbaf030fc19db4e5156
Anwesh43 <anweshthecool0@gmail.com>
2017-04-21 13:56:09 +0530

    Adding CreacButtonView to activity

--- crecbutton/src/main/java/com/anwesome/ui/crecbutton/CrecButton.java
+++ crecbutton/src/main/java/com/anwesome/ui/crecbutton/CrecButton.java
@@ -1,16 +1,36 @@
 package com.anwesome.ui.crecbutton;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.MotionEvent;
+import android.view.View;
 
 /**
  * Created by anweshmishra on 21/04/17.
  */
 public class CrecButton {
     private Activity activity;
+    private CrecButtonShape crecButtonShape = new CrecButtonShape();
+    private CrecButtonView crecButtonView;
+    private AnimationController animationController;
     public CrecButton(Activity activity) {
         this.activity = activity;
     }
     public void show() {
+        if(crecButtonView == null) {
+            crecButtonView = new CrecButtonView(activity);
+        }
+    }
+    private class CrecButtonView extends View {
+        public CrecButtonView(Context context) {
+            super(context);
+        }
+        public void onDraw(Canvas canvas) {
 
+        }
+        public boolean onTouchEvent(MotionEvent event) {
+            return true;
+        }
     }
 }
