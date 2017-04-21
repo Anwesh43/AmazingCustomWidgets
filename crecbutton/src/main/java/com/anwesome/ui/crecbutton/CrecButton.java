a11550936e3b731cb503b88fba722e7effbfeb6b
Anwesh43 <anweshthecool0@gmail.com>
2017-04-21 12:55:38 +0530

    Adding CrecButton

---
+++ crecbutton/src/main/java/com/anwesome/ui/crecbutton/CrecButton.java
@@ -0,0 +1,16 @@
+package com.anwesome.ui.crecbutton;
+
+import android.app.Activity;
+
+/**
+ * Created by anweshmishra on 21/04/17.
+ */
+public class CrecButton {
+    private Activity activity;
+    public CrecButton(Activity activity) {
+        this.activity = activity;
+    }
+    public void show() {
+
+    }
+}
