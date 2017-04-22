c2d084c370cf0014e112b8c59738a0a3b735a13c
Anwesh43 <anweshthecool0@gmail.com>
2017-04-22 07:33:26 +0530

    Adding ui window module and UiWindow class

---
+++ uiwindow/src/main/java/com/anwesome/ui/uiwindow/UiWindow.java
@@ -0,0 +1,16 @@
+package com.anwesome.ui.uiwindow;
+
+import android.app.Activity;
+
+/**
+ * Created by anweshmishra on 22/04/17.
+ */
+public class UiWindow {
+    private Activity activity;
+    public UiWindow(Activity activity) {
+        this.activity = activity;
+    }
+    public void show() {
+        
+    }
+}
