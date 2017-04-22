04ab539468a230e95f2b5e089ad84a02610966f5
Anwesh43 <anweshthecool0@gmail.com>
2017-04-22 07:54:46 +0530

    Adding DrawingUtil

---
+++ uiwindow/src/main/java/com/anwesome/ui/uiwindow/DrawingUtil.java
@@ -0,0 +1,31 @@
+package com.anwesome.ui.uiwindow;
+
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.RectF;
+
+/**
+ * Created by anweshmishra on 22/04/17.
+ */
+public class DrawingUtil {
+    public static void drawUiWindow(Canvas canvas, Paint paint,float w,float deg) {
+        canvas.save();
+        canvas.translate(w,w);
+        canvas.rotate(deg);
+        paint.setColor(Color.parseColor("#FDD835"));
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(w/40);
+        canvas.drawRect(new RectF(-w/2,-w/2,w/2,w/2),paint);
+        paint.setStyle(Paint.Style.FILL);
+        Path path = new Path();
+        path.moveTo(-w/2,-w/2);
+        path.lineTo(-w/2,-w/2-w/10);
+        path.lineTo(-w/2+w/6,-w/2-w/10);
+        path.lineTo(-w/2+w/6+w/10,-w/2);
+        path.lineTo(-w/2,-w/2);
+        canvas.drawPath(path,paint);
+        canvas.restore();
+    }
+}
