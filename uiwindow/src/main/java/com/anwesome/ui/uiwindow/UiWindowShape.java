18744ccd66169550151ca6e19c4247bf96390e7b
Anwesh43 <anweshthecool0@gmail.com>
2017-04-22 07:35:50 +0530

    Adding UiWindowShape

---
+++ uiwindow/src/main/java/com/anwesome/ui/uiwindow/UiWindowShape.java
@@ -0,0 +1,22 @@
+package com.anwesome.ui.uiwindow;
+
+import android.graphics.Canvas;
+import android.graphics.Paint;
+
+/**
+ * Created by anweshmishra on 22/04/17.
+ */
+public class UiWindowShape {
+    public void draw(Canvas canvas, Paint paint,float w) {
+
+    }
+    public void update() {
+
+    }
+    public void startUpdating() {
+
+    }
+    public boolean stopped() {
+        return false;
+    }
+}
