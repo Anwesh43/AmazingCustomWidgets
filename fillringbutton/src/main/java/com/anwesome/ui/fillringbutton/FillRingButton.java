6360365352e06b7b25abfa3efe77efedac627dc2
Anwesh43 <anweshthecool0@gmail.com>
2017-04-23 10:49:54 +0530

    Adding FillRingButton

---
+++ fillringbutton/src/main/java/com/anwesome/ui/fillringbutton/FillRingButton.java
@@ -0,0 +1,16 @@
+package com.anwesome.ui.fillringbutton;
+
+import android.app.Activity;
+
+/**
+ * Created by anweshmishra on 23/04/17.
+ */
+public class FillRingButton {
+    private Activity activity;
+    public FillRingButton(Activity activity) {
+        this.activity = activity;
+    }
+    public void show() {
+        
+    }
+}
