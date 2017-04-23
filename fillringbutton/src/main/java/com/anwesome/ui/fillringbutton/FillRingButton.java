a6ad883a214e36ea0f72e104c123fae80eec0ffb
Anwesh43 <anweshthecool0@gmail.com>
2017-04-23 11:43:41 +0530

    Using AnimationController in fillringview and adding view in activity

--- fillringbutton/src/main/java/com/anwesome/ui/fillringbutton/FillRingButton.java
+++ fillringbutton/src/main/java/com/anwesome/ui/fillringbutton/FillRingButton.java
@@ -4,8 +4,12 @@ import android.app.Activity;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Point;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+
+import com.anwesome.ui.dimensionsutil.DimensionsUtil;
 
 /**
  * Created by anweshmishra on 23/04/17.
@@ -18,10 +22,16 @@ public class FillRingButton {
     public FillRingButton(Activity activity) {
         this.activity = activity;
     }
-    public void show() {
+    public void show(int x,int y) {
         if(fillRingView == null) {
             fillRingView = new FillRingView(activity);
+            Point dimension = DimensionsUtil.getDeviceDimension(activity);
+            int w = dimension.x/2;
+            animationController = new AnimationController(fillRingView,fillRingShape);
+            activity.addContentView(fillRingView,new ViewGroup.LayoutParams(w/2,w/2));
         }
+        fillRingView.setX(x);
+        fillRingView.setY(y);
     }
     private class FillRingView extends View {
         private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
@@ -30,8 +40,14 @@ public class FillRingButton {
         }
         public void onDraw(Canvas canvas) {
             fillRingShape.draw(canvas,paint,canvas.getWidth()/2);
+            if(animationController != null) {
+                animationController.animate();
+            }
         }
         public boolean onTouchEvent(MotionEvent event) {
+            if(event.getAction() == MotionEvent.ACTION_DOWN && animationController != null) {
+                animationController.startAnimating();
+            }
             return true;
         }
     }
