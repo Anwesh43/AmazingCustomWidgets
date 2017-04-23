387729e66fe752810f7762e0a1603c4a9cc54075
Anwesh43 <anweshthecool0@gmail.com>
2017-04-23 11:13:20 +0530

    Using MovementController in FillRingShape

--- fillringbutton/src/main/java/com/anwesome/ui/fillringbutton/FillRingShape.java
+++ fillringbutton/src/main/java/com/anwesome/ui/fillringbutton/FillRingShape.java
@@ -7,16 +7,25 @@ import android.graphics.Paint;
  * Created by anweshmishra on 23/04/17.
  */
 public class FillRingShape {
+    private int render = 0;
+    private MovementController movementController;
     public void draw(Canvas canvas, Paint paint,float w) {
-
+        if(render == 0) {
+            movementController = new MovementController(w);
+        }
+        render++;
     }
     public void update() {
-
+        if(movementController!=null) {
+            movementController.update();
+        }
     }
     public void startUpdating() {
-
+        if(movementController != null) {
+            movementController.startUpdating();
+        }
     }
     public boolean stopped() {
-        return false;
+        return movementController!=null && movementController.stopped();
     }
 }
