f4e6eac31c10b29c3bfd02cdee1a16b912baf61b
Anwesh43 <anweshthecool0@gmail.com>
2017-04-24 10:34:03 +0530

    Adding EyeButtonView in activity

--- eyebutton/src/main/java/com/anwesome/ui/eyebutton/EyeButton.java
+++ eyebutton/src/main/java/com/anwesome/ui/eyebutton/EyeButton.java
@@ -4,8 +4,12 @@ import android.app.Activity;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Point;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+
+import com.anwesome.ui.dimensionsutil.DimensionsUtil;
 
 /**
  * Created by anweshmishra on 24/04/17.
@@ -14,11 +18,20 @@ public class EyeButton {
     private Activity activity;
     private AnimationController animationController;
     private EyeButtonShape eyeButtonShape = new EyeButtonShape();
+    private EyeButtonView eyeButtonView;
     public EyeButton(Activity activity) {
         this.activity = activity;
     }
-    public void show() {
-        
+    public void show(int x,int y) {
+        if(eyeButtonView == null) {
+            eyeButtonView = new EyeButtonView(activity);
+            animationController = new AnimationController(eyeButtonView,eyeButtonShape);
+            Point dimension = DimensionsUtil.getDeviceDimension(activity);
+            int w = dimension.x/2;
+            activity.addContentView(eyeButtonView,new ViewGroup.LayoutParams(w,w));
+        }
+        eyeButtonView.setX(x);
+        eyeButtonView.setY(y);
     }
     private class EyeButtonView extends View {
         private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
