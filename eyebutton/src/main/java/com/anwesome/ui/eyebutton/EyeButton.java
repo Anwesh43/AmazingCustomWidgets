43a979a2b2cbe9c1f852ae3a696cff60cddf7cf5
Anwesh43 <anweshthecool0@gmail.com>
2017-04-24 09:54:31 +0530

    Adding EyeButton class

---
+++ eyebutton/src/main/java/com/anwesome/ui/eyebutton/EyeButton.java
@@ -0,0 +1,16 @@
+package com.anwesome.ui.eyebutton;
+
+import android.app.Activity;
+
+/**
+ * Created by anweshmishra on 24/04/17.
+ */
+public class EyeButton {
+    private Activity activity;
+    public EyeButton(Activity activity) {
+        this.activity = activity;
+    }
+    public void show() {
+        
+    }
+}
