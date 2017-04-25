5a79e87c7cc21bd5ec52403fc93785d0d4ee06a5
Anwesh43 <anweshthecool0@gmail.com>
2017-04-25 08:10:37 +0530

    Adding DirectionButton

---
+++ directionbutton/src/main/java/com/anwesome/ui/directionbutton/DrawingUtil.java
@@ -0,0 +1,27 @@
+package com.anwesome.ui.directionbutton;
+
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
+
+/**
+ * Created by anweshmishra on 25/04/17.
+ */
+public class DrawingUtil {
+    public static void draw(Canvas canvas, Paint paint,float size,float deg) {
+        canvas.save();
+        canvas.translate(size,size);
+        canvas.rotate(deg+60);
+        for(int i=0;i<2;i++) {
+            canvas.save();
+            canvas.scale(i*2-1,1);
+            Path path = new Path();
+            path.moveTo(0,0);
+            path.lineTo(size/2,-size/2);
+            path.lineTo(0,size/2);
+            canvas.drawPath(path,paint);
+            canvas.restore();
+        }
+        canvas.restore();
+    }
+}
