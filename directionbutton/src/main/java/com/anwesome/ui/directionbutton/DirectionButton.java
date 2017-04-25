54815712ffa0b4890f704b4210e131c277b21518
Anwesh43 <anweshthecool0@gmail.com>
2017-04-25 08:22:10 +0530

    Using AnimationController and DirectionButtonShape in DirectionButton

--- directionbutton/src/main/java/com/anwesome/ui/directionbutton/DirectionButton.java
+++ directionbutton/src/main/java/com/anwesome/ui/directionbutton/DirectionButton.java
@@ -1,16 +1,43 @@
 package com.anwesome.ui.directionbutton;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.view.MotionEvent;
+import android.view.View;
+
+import com.anwesome.ui.directionbutton.controller.AnimationController;
 
 /**
  * Created by anweshmishra on 25/04/17.
  */
 public class DirectionButton {
     private Activity activity;
+    private AnimationController animationController;
+    private DirectionButtonShape directionButtonShape = new DirectionButtonShape();
     public DirectionButton(Activity activity) {
         this.activity = activity;
     }
     public void show() {
 
+    }
+    private class DirectionButtonView extends View {
+        private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        public DirectionButtonView(Context context) {
+            super(context);
+        }
+        public void onDraw(Canvas canvas) {
+            directionButtonShape.draw(canvas,paint,canvas.getWidth()/2);
+            if(animationController != null) {
+                animationController.animate();
+            }
+        }
+        public boolean onTouchEvent(MotionEvent event) {
+            if(event.getAction() == MotionEvent.ACTION_DOWN && animationController != null) {
+                animationController.startAnimating();
+            }
+            return true;
+        }
     }
 }
