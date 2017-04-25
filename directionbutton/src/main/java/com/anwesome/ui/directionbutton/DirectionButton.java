e97a167195afb7f64de26dedf68e945da5dfa489
Anwesh43 <anweshthecool0@gmail.com>
2017-04-25 07:55:58 +0530

    Adding DirectionButton class

---
+++ directionbutton/src/main/java/com/anwesome/ui/directionbutton/DirectionButton.java
@@ -0,0 +1,16 @@
+package com.anwesome.ui.directionbutton;
+
+import android.app.Activity;
+
+/**
+ * Created by anweshmishra on 25/04/17.
+ */
+public class DirectionButton {
+    private Activity activity;
+    public DirectionButton(Activity activity) {
+        this.activity = activity;
+    }
+    public void show() {
+
+    }
+}
