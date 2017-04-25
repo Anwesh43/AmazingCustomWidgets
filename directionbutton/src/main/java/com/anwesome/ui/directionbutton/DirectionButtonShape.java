a37070b51bd4ab98a81e61cee5489b382c1af6ff
Anwesh43 <anweshthecool0@gmail.com>
2017-04-25 07:58:24 +0530

    Adding DirectionButtonShape class

---
+++ directionbutton/src/main/java/com/anwesome/ui/directionbutton/DirectionButtonShape.java
@@ -0,0 +1,22 @@
+package com.anwesome.ui.directionbutton;
+
+import android.graphics.Canvas;
+import android.graphics.Paint;
+
+/**
+ * Created by anweshmishra on 25/04/17.
+ */
+public class DirectionButtonShape {
+    public void draw(Canvas canvas, Paint paint,float w) {
+
+    }
+    public void update() {
+
+    }
+    public void startUpdating() {
+
+    }
+    public boolean stopped() {
+        return false;
+    }
+}
