6ee2b4954058e26c1cbd85547328a406bc5ce5df
Anwesh43 <anweshthecool0@gmail.com>
2017-04-25 08:15:42 +0530

    Using AnimationController and MovementController in DirectionButtonShape

--- directionbutton/src/main/java/com/anwesome/ui/directionbutton/DirectionButtonShape.java
+++ directionbutton/src/main/java/com/anwesome/ui/directionbutton/DirectionButtonShape.java
@@ -3,20 +3,23 @@ package com.anwesome.ui.directionbutton;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 
+import com.anwesome.ui.directionbutton.controller.MovementController;
+
 /**
  * Created by anweshmishra on 25/04/17.
  */
 public class DirectionButtonShape {
+    private MovementController movementController = new MovementController();
     public void draw(Canvas canvas, Paint paint,float w) {
-
+        DrawingUtil.draw(canvas,paint,w,movementController.getDeg());
     }
     public void update() {
-
+        movementController.move();
     }
     public void startUpdating() {
-
+        movementController.startMoving();
     }
     public boolean stopped() {
-        return false;
+        return movementController.stopped();
     }
 }
