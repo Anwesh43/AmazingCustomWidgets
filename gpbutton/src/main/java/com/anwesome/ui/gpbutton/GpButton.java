2319a32e6ba078888f37f152319afc8473df34ff
Anwesh43 <anweshthecool0@gmail.com>
2017-04-26 08:49:45 +0530

    Adding GpButton class

---
+++ gpbutton/src/main/java/com/anwesome/ui/gpbutton/GpButton.java
@@ -0,0 +1,16 @@
+package com.anwesome.ui.gpbutton;
+
+import android.app.Activity;
+
+/**
+ * Created by anweshmishra on 26/04/17.
+ */
+public class GpButton {
+    private Activity activity;
+    public GpButton(Activity activity) {
+        this.activity = activity;
+    }
+    public void show() {
+
+    }
+}
