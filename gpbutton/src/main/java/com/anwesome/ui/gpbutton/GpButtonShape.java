b87c3492359313f5b3bdc5516d83745ef59f849b
Anwesh43 <anweshthecool0@gmail.com>
2017-04-26 08:51:29 +0530

    Adding GpButtonShape

---
+++ gpbutton/src/main/java/com/anwesome/ui/gpbutton/GpButtonShape.java
@@ -0,0 +1,22 @@
+package com.anwesome.ui.gpbutton;
+
+import android.graphics.Canvas;
+import android.graphics.Paint;
+
+/**
+ * Created by anweshmishra on 26/04/17.
+ */
+public class GpButtonShape {
+    public void draw(Canvas canvas, Paint paint,float w) {
+
+    }
+    public void update() {
+
+    }
+    public void startUpdating() {
+
+    }
+    public boolean stopped() {
+        return false;
+    }
+}
