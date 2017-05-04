94415c16079be8dbff3e3965344bb2c3a948cd5c
Anwesh43 <anweshthecool0@gmail.com>
2017-05-04 08:24:47 +0530

    Adding DotbarSwitchShape

---
+++ dotbarswitch/src/main/java/com/anwesome/ui/dotbarswitch/DotbarSwitchShape.java
@@ -0,0 +1,22 @@
+package com.anwesome.ui.dotbarswitch;
+
+import android.graphics.Canvas;
+import android.graphics.Paint;
+
+/**
+ * Created by anweshmishra on 04/05/17.
+ */
+public class DotbarSwitchShape {
+    public void draw(Canvas canvas, Paint paint,float w) {
+        
+    }
+    public void update() {
+
+    }
+    public void startUpdating() {
+
+    }
+    public boolean stopUpdating() {
+        return false;
+    }
+}
