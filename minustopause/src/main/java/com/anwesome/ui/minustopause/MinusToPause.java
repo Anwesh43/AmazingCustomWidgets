a0b04d2498cf7cef412b74fde0a9748fd578ed35
Anwesh43 <anweshthecool0@gmail.com>
2017-05-13 23:11:36 +0530

    setting position of MinusToPauseView

--- minustopause/src/main/java/com/anwesome/ui/minustopause/MinusToPause.java
+++ minustopause/src/main/java/com/anwesome/ui/minustopause/MinusToPause.java
@@ -15,12 +15,14 @@ public class MinusToPause {
     public MinusToPause(Activity activity) {
         this.activity = activity;
     }
-    public void show() {
+    public void show(int x,int y) {
         if(minusToPauseView == null) {
             minusToPauseView = new MinusToPauseView(activity);
             Point size = DimensionsUtil.getDeviceDimension(activity);
             int w = size.x;
             activity.addContentView(minusToPauseView,new ViewGroup.LayoutParams(w/2,w/2));
         }
+        minusToPauseView.setX(x);
+        minusToPauseView.setY(y);
     }
 }
