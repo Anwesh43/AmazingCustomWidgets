09735e069a360c3e6cb04c6306cbb72d905a20fd
Anwesh43 <anweshthecool0@gmail.com>
2017-05-13 23:03:20 +0530

    implementing show method

--- minustopause/src/main/java/com/anwesome/ui/minustopause/MinusToPause.java
+++ minustopause/src/main/java/com/anwesome/ui/minustopause/MinusToPause.java
@@ -1,16 +1,26 @@
 package com.anwesome.ui.minustopause;
 
 import android.app.Activity;
+import android.graphics.Point;
+import android.view.ViewGroup;
+
+import com.anwesome.ui.dimensionsutil.DimensionsUtil;
 
 /**
  * Created by anweshmishra on 13/05/17.
  */
 public class MinusToPause {
     private Activity activity;
+    private MinusToPauseView minusToPauseView;
     public MinusToPause(Activity activity) {
         this.activity = activity;
     }
     public void show() {
-
+        if(minusToPauseView == null) {
+            minusToPauseView = new MinusToPauseView(activity);
+            Point size = DimensionsUtil.getDeviceDimension(activity);
+            int w = size.x;
+            activity.addContentView(minusToPauseView,new ViewGroup.LayoutParams(w/2,w/2));
+        }
     }
 }
