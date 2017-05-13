438deef5e35a7a798fa89b34ea799821b6a0483b
Anwesh43 <anweshthecool0@gmail.com>
2017-05-13 22:04:22 +0530

    adding MinusToPause class

---
+++ minustopause/src/main/java/com/anwesome/ui/minustopause/MinusToPause.java
@@ -0,0 +1,18 @@
+package com.anwesome.ui.minustopause;
+
+import android.animation.ValueAnimator;
+import android.app.Activity;
+import android.content.Context;
+
+/**
+ * Created by anweshmishra on 13/05/17.
+ */
+public class MinusToPause {
+    private Activity activity;
+    public MinusToPause(Activity activity) {
+        this.activity = activity;
+    }
+    public void show() {
+
+    }
+}
