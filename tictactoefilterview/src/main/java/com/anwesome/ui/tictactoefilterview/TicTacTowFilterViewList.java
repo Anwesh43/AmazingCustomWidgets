c6168f9616d7c22c68d6544291c04f7a7e17a63e
Anwesh43 <anweshthecool0@gmail.com>
2017-05-22 11:10:51 +0530

    Using ListLayout in List

--- tictactoefilterview/src/main/java/com/anwesome/ui/tictactoefilterview/TicTacTowFilterViewList.java
+++ tictactoefilterview/src/main/java/com/anwesome/ui/tictactoefilterview/TicTacTowFilterViewList.java
@@ -2,6 +2,7 @@ package com.anwesome.ui.tictactoefilterview;
 
 import android.app.Activity;
 import android.graphics.Bitmap;
+import android.view.ViewGroup;
 import android.widget.ScrollView;
 
 /**
@@ -11,13 +12,16 @@ public class TicTacTowFilterViewList {
     private Activity activity;
     private boolean isShown = false;
     private ScrollView scrollView;
+    private ListLayout listLayout;
     public TicTacTowFilterViewList(Activity activity) {
         this.activity = activity;
         scrollView = new ScrollView(activity);
+        listLayout = new ListLayout(activity);
+        scrollView.addView(listLayout,new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
     }
     public void addImage(Bitmap bitmap) {
         if(!isShown) {
-
+            listLayout.addImage(bitmap);
         }
     }
     public void show() {
