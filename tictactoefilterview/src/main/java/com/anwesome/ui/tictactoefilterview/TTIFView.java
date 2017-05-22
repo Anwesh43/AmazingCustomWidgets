f48fec5227297a7ac00960c3e3798a5937c0ddde
Anwesh43 <anweshthecool0@gmail.com>
2017-05-22 12:36:54 +0530

    making proper animation changes

--- tictactoefilterview/src/main/java/com/anwesome/ui/tictactoefilterview/TTIFView.java
+++ tictactoefilterview/src/main/java/com/anwesome/ui/tictactoefilterview/TTIFView.java
@@ -9,8 +9,11 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.RectF;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+
 
 /**
  * Created by anweshmishra on 22/05/17.
@@ -36,6 +39,7 @@ public class TTIFView extends View{
             ticTacFilter = new TicTacFilter();
             animationHandler = new AnimationHandler();
         }
+        time++;
         canvas.drawBitmap(bitmap,0,0,paint);
         if(shouldShowFilter) {
             ticTacFilter.draw(canvas);
@@ -47,45 +51,54 @@ public class TTIFView extends View{
     }
     public boolean onTouchEvent(MotionEvent event) {
         if(event.getAction() == MotionEvent.ACTION_DOWN){
-            shouldShowFilter = !shouldShowFilter;
+            if(!shouldShowFilter) {
+                shouldShowFilter = true;
+            }
             animationHandler.start();
         }
         return true;
     }
     private class TicTacFilter {
-        private float scale = 0;
+        private float r=0;
         public void draw(Canvas canvas) {
-            canvas.drawColor(Color.parseColor("#99000000"));
+            paint.setStyle(Paint.Style.FILL);
+            paint.setColor(Color.parseColor("#99000000"));
+            canvas.drawRect(new RectF(0,0,w,h),paint);
             paint.setStyle(Paint.Style.STROKE);
+            paint.setColor(Color.WHITE);
+            paint.setStrokeWidth(canvas.getWidth()/10);
             canvas.save();
             canvas.translate(w/2,h/2);
-            canvas.scale(scale,scale);
+            //canvas.scale(scale,scale);
             if(index%2 == 0) {
                 for(int i=0;i<2;i++) {
                     canvas.save();
                     canvas.translate(0, 0);
-                    canvas.rotate(45*(2*i-1));
-                    Path path = new Path();
-                    path.moveTo(0, -h / 2);
-                    path.lineTo(0, h / 2);
-                    canvas.drawPath(path, paint);
+                    canvas.rotate(45*(i*2-1));
+                    canvas.drawLine(0,-r,0,r,paint);
+                    Log.d("r",""+r);
+                    //canvas.drawPath(path, paint);
                     canvas.restore();
                 }
             }
             else {
-                canvas.drawCircle(0,0,w/2,paint);
+                canvas.drawCircle(0,0,r,paint);
             }
             canvas.restore();
         }
         public void update(float factor) {
-            scale = factor;
+            Log.d("factor",""+factor);
+            this.r = factor;
+            Log.d("r",""+this.r);
         }
     }
     private class AnimationHandler extends AnimatorListenerAdapter implements ValueAnimator.AnimatorUpdateListener{
         private boolean isAnimating = false;
         private float dir = 0;
-        private ValueAnimator addAnim = ValueAnimator.ofFloat(0,1),removeAnim = ValueAnimator.ofFloat(1,0);
+        private ValueAnimator addAnim,removeAnim;
         public AnimationHandler() {
+            addAnim = ValueAnimator.ofFloat(0,w/3);
+            removeAnim = ValueAnimator.ofFloat(w/3,0);
             addAnim.setDuration(500);
             removeAnim.setDuration(500);
             addAnim.addUpdateListener(this);
@@ -97,19 +110,26 @@ public class TTIFView extends View{
             update((float)valueAnimator.getAnimatedValue());
         }
         public void onAnimationEnd(Animator animator) {
-            if(!isAnimating) {
-                isAnimating = true;
+            if(isAnimating) {
+                isAnimating = false;
+                if(shouldShowFilter) {
+                    shouldShowFilter = false;
+                    postInvalidate();
+                }
             }
         }
         public void start() {
             if(!isAnimating) {
                 if(dir == 0) {
+                    //addAnim.addListener(this);
                     addAnim.start();
                 }
                 else {
+                    //removeAnim.addListener(this);
                     removeAnim.start();
                 }
                 dir = dir==0?1:0;
+                isAnimating = true;
             }
         }
     }
