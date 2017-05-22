644997a3002c6a3ba23046210153b9899d58e66a
Anwesh43 <anweshthecool0@gmail.com>
2017-05-22 10:43:12 +0530

    Adding TicTacFilter

--- tictactoefilterview/src/main/java/com/anwesome/ui/tictactoefilterview/TTIFView.java
+++ tictactoefilterview/src/main/java/com/anwesome/ui/tictactoefilterview/TTIFView.java
@@ -3,7 +3,9 @@ package com.anwesome.ui.tictactoefilterview;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -14,9 +16,11 @@ public class TTIFView extends View{
     private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
     private int time = 0,w,h;
     private Bitmap bitmap;
-    public TTIFView(Context context, Bitmap bitmap) {
+    private int index = 0;
+    public TTIFView(Context context, Bitmap bitmap,int index) {
         super(context);
         this.bitmap = bitmap;
+        this.index = index;
     }
     public void onDraw(Canvas canvas) {
         if(time == 0) {
@@ -31,5 +35,31 @@ public class TTIFView extends View{
 
         }
         return true;
+    }
+    private class TicTacFilter {
+        private float scale = 0;
+        public void draw(Canvas canvas) {
+            canvas.drawColor(Color.parseColor("#99000000"));
+            paint.setStyle(Paint.Style.STROKE);
+            canvas.save();
+            canvas.translate(w/2,h/2);
+            canvas.scale(scale,scale);
+            if(index%2 == 0) {
+                for(int i=0;i<2;i++) {
+                    canvas.save();
+                    canvas.translate(0, 0);
+                    canvas.rotate(45*(2*i-1));
+                    Path path = new Path();
+                    path.moveTo(0, -h / 2);
+                    path.lineTo(0, h / 2);
+                    canvas.drawPath(path, paint);
+                    canvas.restore();
+                }
+            }
+            else {
+                canvas.drawCircle(0,0,w/2,paint);
+            }
+            canvas.restore();
+        }
     }
 }
