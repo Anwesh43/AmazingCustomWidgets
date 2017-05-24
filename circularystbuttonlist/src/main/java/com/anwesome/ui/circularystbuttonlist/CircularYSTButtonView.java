e97545a22706068e509ee92329c822ec2aab4230
Anwesh43 <anweshthecool0@gmail.com>
2017-05-25 01:38:25 +0530

    adding clipboard

--- circularystbuttonlist/src/main/java/com/anwesome/ui/circularystbuttonlist/CircularYSTButtonView.java
+++ circularystbuttonlist/src/main/java/com/anwesome/ui/circularystbuttonlist/CircularYSTButtonView.java
@@ -2,7 +2,9 @@ package com.anwesome.ui.circularystbuttonlist;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.RectF;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -25,5 +27,34 @@ public class CircularYSTButtonView extends View {
     }
     public boolean onTouchEvent(MotionEvent event) {
         return true;
+    }
+    private class ClipBoard {
+        private float scale = 0,x,y,size;
+        public ClipBoard() {
+            x = w/2;
+            y = h/2;
+            size = Math.min(w,h)*0.8f;
+        }
+        public void draw(Canvas canvas) {
+            canvas.save();
+            canvas.translate(x,y);
+            paint.setColor(Color.WHITE);
+            paint.setStyle(Paint.Style.STROKE);
+            canvas.drawRoundRect(new RectF(-size/2,-size/2,size/2,size/2),size/20,size/20,paint);
+            int n = 5;
+            float circleX = w/10,gap = size/11,y_gap = size/(2*n+1);
+            for(int i=0;i<n;i++) {
+                canvas.drawCircle(circleX,y_gap,size/30,paint);
+                canvas.drawLine(circleX+size/5,y_gap,circleX+size*0.9f,y_gap,paint);
+                y_gap += gap;
+            }
+            paint.setStyle(Paint.Style.FILL);
+            paint.setColor(Color.argb(150,0,0,0));
+            canvas.drawRoundRect(new RectF(-size/2,-size/2,size/2,size/2),size/20,size/20,paint);
+            canvas.restore();
+        }
+        public void update(float factor) {
+            scale = factor;
+        }
     }
 }
