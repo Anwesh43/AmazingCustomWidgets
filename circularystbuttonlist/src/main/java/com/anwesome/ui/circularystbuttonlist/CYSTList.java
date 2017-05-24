3fc5dfa5c6615352dcdbbd58bb80380a2f725ec9
Anwesh43 <anweshthecool0@gmail.com>
2017-05-25 02:08:32 +0530

    adding listlayout class

--- circularystbuttonlist/src/main/java/com/anwesome/ui/circularystbuttonlist/CYSTList.java
+++ circularystbuttonlist/src/main/java/com/anwesome/ui/circularystbuttonlist/CYSTList.java
@@ -1,6 +1,13 @@
 package com.anwesome.ui.circularystbuttonlist;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Point;
+import android.hardware.display.DisplayManager;
+import android.view.Display;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ScrollView;
 
 /**
  * Created by anweshmishra on 25/05/17.
@@ -9,6 +16,7 @@ import android.app.Activity;
 public class CYSTList {
     private Activity activity;
     private boolean isShown = false;
+    private ScrollView scrollView;
     public CYSTList(Activity activity) {
         this.activity = activity;
     }
@@ -20,6 +28,44 @@ public class CYSTList {
     public void addButton() {
         if(!isShown) {
 
+        }
+    }
+    private class ListLayout extends ViewGroup {
+        private int w,h,viewSize;
+        public ListLayout(Context context) {
+            super(context);
+        }
+        public void addButton() {
+            addView(new CircularYSTButtonView(getContext()),new LayoutParams(w,viewSize));
+            requestLayout();
+        }
+        public void initDimension(Context context) {
+            DisplayManager displayManager = (DisplayManager)context.getSystemService(Context.DISPLAY_SERVICE);
+            Display display = displayManager.getDisplay(0);
+            if(display != null) {
+                Point size = new Point();
+                display.getRealSize(size);
+                w = size.x;
+                h = size.y;
+                viewSize = h/3;
+            }
+        }
+        public void onMeasure(int wspec,int hspec) {
+            int hmax = h/25;
+            for(int i=0;i<getChildCount();i++) {
+                View child = getChildAt(i);
+                measureChild(child,wspec,hspec);
+                hmax += child.getMeasuredHeight()+h/25;
+            }
+            setMeasuredDimension(w,Math.max(h,hmax+viewSize+h/25));
+        }
+        public void onLayout(boolean reloaded,int a,int b,int wa,int ha) {
+            int y = h/25;
+            for(int i=0;i<getChildCount();i++) {
+                View child = getChildAt(i);
+                child.layout(0,y,w,y+h/25);
+                y += child.getMeasuredHeight()+h/25;
+            }
         }
     }
 }
