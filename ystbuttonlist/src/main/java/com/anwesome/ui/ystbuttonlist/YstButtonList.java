78fe0d7e75783761c3ce0136b5fdd732d3393750
Anwesh43 <anweshthecool0@gmail.com>
2017-05-24 13:24:47 +0530

    adding ystbuttonlist

---
+++ ystbuttonlist/src/main/java/com/anwesome/ui/ystbuttonlist/YstButtonList.java
@@ -0,0 +1,66 @@
+package com.anwesome.ui.ystbuttonlist;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Point;
+import android.hardware.display.DisplayManager;
+import android.view.Display;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by anweshmishra on 24/05/17.
+ */
+public class YstButtonList {
+    private Activity activity;
+    private boolean isShown = false;
+    public YstButtonList(Activity activity) {
+        this.activity = activity;
+    }
+    public void addButton() {
+        if(!isShown) {
+            
+        }
+    }
+    public void show() {
+        if(!isShown) {
+            isShown = true;
+        }
+    }
+    private class ListLayout extends ViewGroup{
+        private int w,h;
+        public void initDimension(Context context) {
+            DisplayManager displayManager = (DisplayManager)context.getSystemService(Context.DISPLAY_SERVICE);
+            Display display = displayManager.getDisplay(0);
+            Point size = new Point();
+            display.getRealSize(size);
+            w = size.x;
+            h = size.y;
+        }
+        public ListLayout(Context context) {
+            super(context);
+            initDimension(context);
+        }
+        public void addButton() {
+            YSTButtonView ystButtonView = new YSTButtonView(getContext());
+            addView(ystButtonView,new LayoutParams(w/2,w/2));
+        }
+        public void onMeasure(int wspec,int hspec) {
+            int hMax = h/25;
+            for(int i=0;i<getChildCount();i++) {
+                View child = getChildAt(i);
+                measureChild(child,wspec,hsepc);
+                hMax += w/2 +h/25;
+            }
+            setMeasuredDimension(w,Math.max(hMax,h));
+        }
+        public void onLayout(boolean reloaded,int a,int b,int w,int h) {
+            int x = w/4,y = h/25;
+            for(int i=0;i<getChildCount();i++) {
+                View child = getChildAt(i);
+                child.layout(x,y,x+w/2,y+w/2);
+                y += w/2+h/25;
+            }
+        }
+    }
+}
