359f17ebe02b543b81fe5fa4c9a398a9cc58f5ae
Anwesh43 <anweshthecool0@gmail.com>
2017-05-28 16:28:59 +0530

    adding AnimationHandler

--- tvbutton/src/main/java/com/anwesome/ui/tvbutton/TVButtonView.java
+++ tvbutton/src/main/java/com/anwesome/ui/tvbutton/TVButtonView.java
@@ -1,5 +1,8 @@
 package com.anwesome.ui.tvbutton;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -92,5 +95,38 @@ public class TVButtonView extends View{
             canvas.drawLine(0,0,lx,0,paint);
             canvas.restore();
         }
+    }
+    private class AnimationHandler extends AnimatorListenerAdapter implements ValueAnimator.AnimatorUpdateListener{
+        private ValueAnimator startAnimator = ValueAnimator.ofFloat(0,1),endAnimator = ValueAnimator.ofFloat(1,0);
+        private int dir = 0;
+        private boolean isAnimated = false;
+        public AnimationHandler() {
+            startAnimator.setDuration(500);
+            endAnimator.setDuration(500);
+            startAnimator.addUpdateListener(this);
+            endAnimator.addUpdateListener(this);
+            startAnimator.addListener(this);
+            endAnimator.addListener(this);
+        }
+        public void onAnimationUpdate(ValueAnimator valueAnimator) {
+            update((float)valueAnimator.getAnimatedValue());
+        }
+        public void onAnimationEnd(Animator animator) {
+            if(isAnimated) {
+                dir = dir == 0?1:0;
+                isAnimated = false;
+            }
+        }
+        public void start() {
+            if(!isAnimated) {
+                if (dir == 0) {
+                     startAnimator.start();
+                } else {
+                    endAnimator.start();
+                }
+                isAnimated = true;
+            }
+        }
+
     }
 }
