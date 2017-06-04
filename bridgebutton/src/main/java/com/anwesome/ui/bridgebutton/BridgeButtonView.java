9453b8eb62ce4cd5cd7916be56fb3378764328a5
Anwesh43 <anweshthecool0@gmail.com>
2017-06-04 19:33:33 +0530

    Adding BridgePoint

--- bridgebutton/src/main/java/com/anwesome/ui/bridgebutton/BridgeButtonView.java
+++ bridgebutton/src/main/java/com/anwesome/ui/bridgebutton/BridgeButtonView.java
@@ -2,7 +2,9 @@ package com.anwesome.ui.bridgebutton;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
+import android.view.MotionEvent;
 import android.view.View;
 
 /**
@@ -12,6 +14,7 @@ import android.view.View;
 public class BridgeButtonView extends View {
     private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
     private int time = 0,w,h;
+    private int color = Color.parseColor("#3F51B5");
     public BridgeButtonView(Context context) {
         super(context);
     }
@@ -24,5 +27,34 @@ public class BridgeButtonView extends View {
     }
     public void update(float factor) {
         postInvalidate();
+    }
+    public boolean onTouchEvent(MotionEvent event) {
+        return true;
+    }
+    private class BridgePoint {
+        private float scale = 0,x,y,r;
+        public BridgePoint(int i) {
+            x = w/4 + i*w/2;
+            y = h/2;
+            r = w/12;
+        }
+        public void draw(Canvas canvas) {
+            paint.setColor(Color.GRAY);
+            paint.setStyle(Paint.Style.STROKE);
+            canvas.drawCircle(x,y,r,paint);
+            paint.setStyle(Paint.Style.FILL);
+            paint.setColor(color);
+            canvas.save();
+            canvas.translate(x,y);
+            canvas.scale(scale,scale);
+            canvas.drawCircle(0,0,r,paint);
+            canvas.restore();
+        }
+        public boolean handleTap(float x,float y) {
+            return x>=this.x -r && x<=this.x+r && y>=this.y -r && y<=this.y +r;
+        }
+        public void update(float factor) {
+            scale = factor;
+        }
     }
 }
