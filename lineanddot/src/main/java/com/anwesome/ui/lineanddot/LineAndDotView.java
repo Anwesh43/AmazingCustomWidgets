2084852cd80f2bb735e281a928f5413967fe24d5
Anwesh43 <anweshthecool0@gmail.com>
2017-06-06 02:58:21 +0530

    Adding LineAndDotView

---
+++ lineanddot/src/main/java/com/anwesome/ui/lineanddot/LineAndDotView.java
@@ -0,0 +1,32 @@
+package com.anwesome.ui.lineanddot;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * Created by anweshmishra on 06/06/17.
+ */
+
+public class LineAndDotView extends View{
+    private int time = 0,w,h;
+    private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    public LineAndDotView(Context context) {
+        super(context);
+    }
+    public void onDraw(Canvas canvas) {
+        if(time == 0) {
+            w = canvas.getWidth();
+            h = canvas.getHeight();
+        }
+        time++;
+    }
+    public boolean onTouchEvent(MotionEvent event) {
+        if(event.getAction() == MotionEvent.ACTION_DOWN) {
+
+        }
+        return true;
+    }
+}
