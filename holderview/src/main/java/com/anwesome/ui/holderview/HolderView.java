b6ca7112bf9f57ea18788e77c464052dfca876b5
Anwesh43 <anweshthecool0@gmail.com>
2017-06-07 13:19:30 +0530

    adding holdercomponents

--- holderview/src/main/java/com/anwesome/ui/holderview/HolderView.java
+++ holderview/src/main/java/com/anwesome/ui/holderview/HolderView.java
@@ -2,7 +2,9 @@ package com.anwesome.ui.holderview;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.RectF;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -41,6 +43,70 @@ public class HolderView extends View {
         }
         public void update(float factor) {
 
+        }
+    }
+    private class HolderComponent {
+        public void draw(Canvas canvas) {
+            paint.setColor(Color.GRAY);
+            paint.setStyle(Paint.Style.STROKE);
+            drawComponent(canvas);
+            paint.setColor(Color.BLUE);
+            drawComponent(canvas);
+        }
+        protected void drawComponent(Canvas canvas) {
+
+        }
+        public void update(float factor) {
+        }
+    }
+    private class HolderCircle extends HolderComponent {
+        private float deg = 0;
+        private float x;
+        public HolderCircle(float x) {
+            this.x = x;
+
+        }
+        public void drawComponent(Canvas canvas) {
+            float y = h/4,r = h/6;
+            canvas.save();
+            canvas.translate(x,y);
+            canvas.drawArc(new RectF(-r,-r,r,r),deg,360-deg,false,paint);
+            canvas.restore();
+        }
+        public void update(float factor) {
+            deg = 360*factor;
+        }
+    }
+    private class HolderLeg extends HolderComponent {
+        private float x,currY = 0,dir;
+        public HolderLeg(float x,float dir) {
+            this.x = x;
+            this.dir = dir;
+        }
+        public void drawComponent(Canvas canvas) {
+            float gapY = 0;
+            if(dir == -1) {
+                gapY = h/4;
+            }
+            canvas.save();
+            canvas.translate(x,h/4+h/6+gapY);
+            canvas.drawLine(0,currY,0,h/4*dir,paint);
+            canvas.restore();
+        }
+        public void update(float factor) {
+            currY = (h/4*dir)*factor;
+        }
+    }
+    private class HolderPlate extends HolderComponent {
+        private float currX = 0;
+        public void drawComponent(Canvas canvas) {
+            canvas.save();
+            canvas.translate(w/4,2*h/3);
+            canvas.drawLine(currX,0,w/2,0,paint);
+            canvas.restore();
+        }
+        public void update(float factor) {
+            currX = w/2*factor;
         }
     }
 }
