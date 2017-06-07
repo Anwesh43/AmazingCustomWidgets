dcf1b478b5fcad4bebde5a0837d8a91f778b55ef
Anwesh43 <anweshthecool0@gmail.com>
2017-06-08 00:51:20 +0530

    adding plusminusbutton

--- plusminuslineview/src/main/java/com/anwesome/ui/plusminuslineview/PlusMinusLineView.java
+++ plusminuslineview/src/main/java/com/anwesome/ui/plusminuslineview/PlusMinusLineView.java
@@ -2,6 +2,7 @@ package com.anwesome.ui.plusminuslineview;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.view.MotionEvent;
 import android.view.View;
@@ -28,6 +29,32 @@ public class PlusMinusLineView extends View {
     }
     public void update(float factor) {
         postInvalidate();
+    }
+    private class PlusMinusButton {
+        private float x,y,r,deg = 90;
+        public PlusMinusButton() {
+            x = w/2;
+            y = h/2;
+            r = w/10;
+        }
+        public void draw(Canvas canvas) {
+
+            paint.setColor(Color.BLUE);
+            canvas.save();
+            canvas.translate(x, y);
+            canvas.drawCircle(0,0,r,paint);
+            for(int i=0;i<2;i++) {
+                canvas.rotate(deg*i);
+                paint.setColor(Color.WHITE);
+                paint.setStrokeWidth(r/50);
+                canvas.drawLine(-r/2,0,r/2,0,paint);
+                canvas.restore();
+            }
+            canvas.restore();
+        }
+        public void update(float factor) {
+            deg = 90*(1-factor);
+        }
     }
 }
 
