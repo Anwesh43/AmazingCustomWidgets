9b3b152024df982e1f3f521b677b9ead45052dd9
Anwesh43 <anweshthecool0@gmail.com>
2017-06-09 06:40:41 +0530

    adding rectball

--- rectbutton/src/main/java/com/anwesome/ui/rectbutton/RectButtonView.java
+++ rectbutton/src/main/java/com/anwesome/ui/rectbutton/RectButtonView.java
@@ -2,6 +2,7 @@ package com.anwesome.ui.rectbutton;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.view.MotionEvent;
 import android.view.View;
@@ -21,6 +22,7 @@ public class RectButtonView extends View {
             w = canvas.getWidth();
             h = canvas.getHeight();
         }
+        paint.setColor(Color.BLUE);
         time++;
     }
     public boolean onTouchEvent(MotionEvent event) {
@@ -31,5 +33,39 @@ public class RectButtonView extends View {
     }
     public void update(float factor) {
         postInvalidate();
+    }
+    private class RectBall {
+        private float rot = 0,y = 0;
+        public void draw(Canvas canvas) {
+            float radius = w/15;
+            paint.setStyle(Paint.Style.FILL);
+            for(int i=0;i<4;i++) {
+                canvas.save();
+                canvas.translate(w / 2, h/2);
+                canvas.rotate((-90+i*90)+45);
+                canvas.save();
+                canvas.translate(0,y);
+                canvas.save();
+                canvas.rotate(rot);
+                paint.setColor(Color.WHITE);
+                paint.setStrokeCap(Paint.Cap.ROUND);
+                canvas.drawLine(0,-radius/2,0,radius/2,paint);
+                for(int j=0;j<2;j++) {
+                    canvas.save();
+                    canvas.translate(0,-radius/2);
+                    canvas.rotate((1-2*j)*45);
+                    canvas.drawLine(0,0,0,radius/8,paint);
+                    canvas.restore();
+                }
+                canvas.restore();
+                canvas.drawCircle(0,0,radius,paint);
+                canvas.restore();
+                canvas.restore();
+            }
+        }
+        public void update(float factor) {
+            rot = 180*factor;
+            y = (h/3-w/15)*factor;
+        }
     }
 }
