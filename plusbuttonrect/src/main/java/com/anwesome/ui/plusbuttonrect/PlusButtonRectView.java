efdf4f4d568b1e86ce8800f990adae8a6dfadcc1
Anwesh43 <anweshthecool0@gmail.com>
2017-06-11 03:03:58 +0530

    adding PlusButtonRectView

---
+++ plusbuttonrect/src/main/java/com/anwesome/ui/plusbuttonrect/PlusButtonRectView.java
@@ -0,0 +1,35 @@
+package com.anwesome.ui.plusbuttonrect;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * Created by anweshmishra on 11/06/17.
+ */
+
+public class PlusButtonRectView extends View {
+    private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    private int time = 0,w,h;
+    public PlusButtonRectView(Context context) {
+        super(context);
+    }
+    public void onDraw(Canvas canvas) {
+        if(time == 0) {
+            w = canvas.getWidth();
+            h = canvas.getHeight();
+        }
+        time++;
+    }
+    public boolean onTouchEvent(MotionEvent event) {
+        if(event.getAction() == MotionEvent.ACTION_DOWN) {
+
+        }
+        return true;
+    }
+    public void update(float factor) {
+        postInvalidate();
+    }
+}
