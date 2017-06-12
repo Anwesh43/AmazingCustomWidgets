93b0b4a03cd40d19f492ba5a5bfd348b1f619d55
Anwesh43 <anweshthecool0@gmail.com>
2017-06-13 01:15:05 +0530

    adding methods for CornerCenterView

--- cornercenterball/src/main/java/com/anwesome/ui/cornercenterball/CornerCenterBallView.java
+++ cornercenterball/src/main/java/com/anwesome/ui/cornercenterball/CornerCenterBallView.java
@@ -1,6 +1,9 @@
 package com.anwesome.ui.cornercenterball;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.view.MotionEvent;
 import android.view.View;
 
 /**
@@ -8,7 +11,25 @@ import android.view.View;
  */
 
 public class CornerCenterBallView extends View {
+    private int time = 0,w,h;
+    private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
     public CornerCenterBallView(Context context) {
         super(context);
+    }
+    public void onDraw(Canvas canvas) {
+        if(time == 0) {
+            w = canvas.getWidth();
+            h = canvas.getHeight();
+        }
+        time++;
+    }
+    public void update(float factor) {
+        postInvalidate();
+    }
+    public boolean onTouchEvent(MotionEvent event) {
+        if(event.getAction() == MotionEvent.ACTION_DOWN) {
+
+        }
+        return true;
     }
 }
