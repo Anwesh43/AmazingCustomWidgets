ab09bde199021c5bfc9e8ef44e4b48dd6c8e7117
Anwesh43 <anweshthecool0@gmail.com>
2017-06-13 01:10:11 +0530

    Adding CornerCenterBallView

---
+++ cornercenterball/src/main/java/com/anwesome/ui/cornercenterball/CornerCenterBallView.java
@@ -0,0 +1,14 @@
+package com.anwesome.ui.cornercenterball;
+
+import android.content.Context;
+import android.view.View;
+
+/**
+ * Created by anweshmishra on 13/06/17.
+ */
+
+public class CornerCenterBallView extends View {
+    public CornerCenterBallView(Context context) {
+        super(context);
+    }
+}
