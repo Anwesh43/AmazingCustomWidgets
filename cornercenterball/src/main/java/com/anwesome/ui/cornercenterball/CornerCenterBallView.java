0d20f3780f9b9104bbf568656c8d7165d1c43898
Anwesh43 <anweshthecool0@gmail.com>
2017-06-13 01:24:48 +0530

    Adding CornerCenterBall

--- cornercenterball/src/main/java/com/anwesome/ui/cornercenterball/CornerCenterBallView.java
+++ cornercenterball/src/main/java/com/anwesome/ui/cornercenterball/CornerCenterBallView.java
@@ -2,7 +2,9 @@ package com.anwesome.ui.cornercenterball;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.RectF;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -31,5 +33,41 @@ public class CornerCenterBallView extends View {
 
         }
         return true;
+    }
+    private class CornerCenterBall {
+        private int index = 0;
+        private float r = 0;
+        public void draw(Canvas canvas) {
+            paint.setStyle(Paint.Style.STROKE);
+            paint.setColor(Color.BLUE);
+            canvas.drawRect(new RectF(-w/3,-h/3,w/3,h/3),paint);
+            for(int i=0;i<4;i++) {
+                canvas.save();
+                canvas.rotate(i*90-45);
+                paint.setStyle(Paint.Style.STROKE);
+                paint.setColor(Color.BLUE);
+                canvas.drawCircle(0,-h/3,w/15,paint);
+                paint.setStyle(Paint.Style.FILL);
+                paint.setColor(Color.WHITE);
+                canvas.drawCircle(0,-h/3,w/15,paint);
+                canvas.restore();
+            }
+            paint.setStyle(Paint.Style.FILL);
+            paint.setColor(Color.BLUE);
+            canvas.save();
+            canvas.translate(w/2,h/2);
+            canvas.rotate(index*90-45);
+            canvas.drawCircle(0,r,w/15,paint);
+            canvas.restore();
+        }
+        public void update(float factor) {
+            r = -h/3*factor;
+        }
+        public void incrementIndex() {
+            index++;
+            if(index%4 == 0) {
+                index = 0;
+            }
+        }
     }
 }
