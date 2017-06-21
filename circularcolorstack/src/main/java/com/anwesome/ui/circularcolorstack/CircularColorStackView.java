51f5e3c9daed695aae0695977d9678a140bad6ed
Anwesh43 <anweshthecool0@gmail.com>
2017-06-21 12:24:39 +0530

    Adding ColorPie

--- circularcolorstack/src/main/java/com/anwesome/ui/circularcolorstack/CircularColorStackView.java
+++ circularcolorstack/src/main/java/com/anwesome/ui/circularcolorstack/CircularColorStackView.java
@@ -3,6 +3,7 @@ package com.anwesome.ui.circularcolorstack;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.RectF;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -12,9 +13,11 @@ import android.view.View;
 
 public class CircularColorStackView extends View {
     private int time = 0,w,h;
+    private int colors[];
     private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-    public CircularColorStackView(Context context) {
+    public CircularColorStackView(Context context,int colors[]) {
         super(context);
+        this.colors = colors;
     }
     public void onDraw(Canvas canvas) {
         if(time == 0) {
@@ -46,6 +49,29 @@ public class CircularColorStackView extends View {
                 index = 0;
                 dir = -1;
             }
+        }
+    }
+    private class ColorPie {
+        private float deg = 0,maxDeg = 0,rot;
+        private int color;
+        public ColorPie(int color,float maxDeg,float rot) {
+            this.maxDeg = maxDeg;
+            this.color = color;
+            this.rot = rot;
+        }
+        public void update(float factor) {
+            deg = maxDeg*factor;
+        }
+        public void draw(Canvas canvas) {
+            paint.setColor(color);
+            canvas.save();
+            canvas.translate(w/2,h/2);
+            canvas.rotate(rot);
+            canvas.drawArc(new RectF(-w/3,-w/3,w/3,w/3),0,deg,true,paint);
+            canvas.restore();
+        }
+        public int hashCode() {
+            return (int)(deg+rot)+color;
         }
     }
 }
