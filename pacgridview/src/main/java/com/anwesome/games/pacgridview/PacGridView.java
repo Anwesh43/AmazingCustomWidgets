f6a737cda1a66e692c1abc6bb627fd4b25ff0dee
Anwesh43 <anweshthecool0@gmail.com>
2017-06-24 01:12:36 +0530

    Adding pacgrid

--- pacgridview/src/main/java/com/anwesome/games/pacgridview/PacGridView.java
+++ pacgridview/src/main/java/com/anwesome/games/pacgridview/PacGridView.java
@@ -5,6 +5,7 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.RectF;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -13,7 +14,7 @@ import android.view.View;
  */
 
 public class PacGridView extends View {
-    private int time = 0,w,h;
+    private int time = 0,w,h,n=3;
     private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
     private Bitmap bitmap;
     public PacGridView(Context context,Bitmap bitmap) {
@@ -39,5 +40,46 @@ public class PacGridView extends View {
 
         }
         return true;
+    }
+    private class PacGrid {
+        private float x,y,size,deg=0,dir = 0,index;
+        public PacGrid(int i) {
+            this.index = i;
+            size = Math.min(w,h)/3;
+            x = ((i%n)*size)+size/2;
+            y = ((i/n)*size)+size/2;
+        }
+        public void draw(Canvas canvas) {
+            canvas.save();
+            canvas.translate(x,y);
+            int color = Color.parseColor("#FF5722");
+            int r = Color.red(color),g = Color.green(color),b = Color.blue(color);
+            paint.setColor(Color.argb(150,r,g,b));
+            canvas.drawArc(new RectF(-size/2,-size/2,size/2,size/2),deg,360-2*deg,true,paint);
+            canvas.restore();
+        }
+        private void startUpdating() {
+            dir = 1;
+        }
+        public void update() {
+            deg += 5*dir;
+            if(deg>30) {
+                dir = -1;
+            }
+            if(deg<0) {
+                deg = 0;
+                dir = 0;
+            }
+        }
+        public int hashCode() {
+            return (int)(x+y+index);
+        }
+        public boolean handleTap(float x,float y) {
+            boolean condition =  x>=this.x-size/2 && x<=this.x+size/2 && y>=this.y-size/2 && y<=this.y+size/2 && dir == 0;
+            if(condition) {
+                startUpdating();
+            }
+            return condition;
+        }
     }
 }
