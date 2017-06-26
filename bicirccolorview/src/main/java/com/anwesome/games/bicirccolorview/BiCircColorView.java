e7ead8791a607aa17da1254c8396ad2dd7479af6
Anwesh43 <anweshthecool0@gmail.com>
2017-06-26 17:34:18 +0530

    Adding BiCirccOLORvIEW

---
+++ bicirccolorview/src/main/java/com/anwesome/games/bicirccolorview/BiCircColorView.java
@@ -0,0 +1,32 @@
+package com.anwesome.games.bicirccolorview;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * Created by anweshmishra on 26/06/17.
+ */
+
+public class BiCircColorView extends View {
+    private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    private int time = 0,w,h;
+    public BiCircColorView(Context context) {
+        super(context);
+    }
+    public void onDraw(Canvas canvas) {
+        if(time == 0) {
+            w = canvas.getWidth();
+            h = canvas.getHeight();
+        }
+        time++;
+    }
+    public boolean onTouchEvent(MotionEvent event) {
+        if(event.getAction() == MotionEvent.ACTION_DOWN) {
+
+        }
+        return true;
+    }
+}
