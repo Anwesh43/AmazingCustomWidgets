c4cfdb1a9d9f0556a1bfd804a19213a680e71bf3
Anwesh43 <anweshthecool0@gmail.com>
2017-06-26 17:47:46 +0530

    Adding BiCirColor

--- bicirccolorview/src/main/java/com/anwesome/games/bicirccolorview/BiCircColorView.java
+++ bicirccolorview/src/main/java/com/anwesome/games/bicirccolorview/BiCircColorView.java
@@ -3,6 +3,7 @@ package com.anwesome.games.bicirccolorview;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.RectF;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -12,14 +13,19 @@ import android.view.View;
 
 public class BiCircColorView extends View {
     private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-    private int time = 0,w,h;
-    public BiCircColorView(Context context) {
+    private int time = 0,w,h,gapDeg = 0;
+    private int[] colors;
+    public BiCircColorView(Context context,int[] colors) {
         super(context);
+        this.colors = colors;
     }
     public void onDraw(Canvas canvas) {
         if(time == 0) {
             w = canvas.getWidth();
             h = canvas.getHeight();
+            if(colors.length > 0) {
+                gapDeg = 360/colors.length;
+            }
         }
         time++;
     }
@@ -28,5 +34,37 @@ public class BiCircColorView extends View {
 
         }
         return true;
+    }
+    private class BiCircColor {
+        private int index,sweepDeg = 0,startDeg = 0,dir=0;
+        public BiCircColor(int index) {
+            this.index = index;
+            startDeg = gapDeg*index;
+        }
+        public void draw(Canvas canvas) {
+            paint.setStyle(Paint.Style.STROKE);
+            paint.setColor(colors[index]);
+            paint.setStrokeWidth(w/60);
+            int r = Math.min(w,h)/2;
+            canvas.drawArc(new RectF(-r,-r,r,r),startDeg,sweepDeg,false,paint);
+            canvas.drawArc(new RectF(-r,-r,r,r),startDeg+gapDeg-sweepDeg,sweepDeg,false,paint);
+        }
+        public void update() {
+            this.sweepDeg += (this.dir)*(gapDeg/10);
+            if(this.sweepDeg> gapDeg/2) {
+                this.dir = 0;
+                this.sweepDeg = gapDeg/2;
+            }
+            if(this.sweepDeg < 0) {
+                this.dir = 0;
+                this.sweepDeg = 0;
+            }
+        }
+        public boolean stopped() {
+            return dir == 0;
+        }
+        public int hashCode() {
+            return index;
+        }
     }
 }
