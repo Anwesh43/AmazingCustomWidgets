c64ba744331033b9b72e92d6f63b1b70e9707ef1
Anwesh43 <anweshthecool0@gmail.com>
2017-06-28 17:29:14 +0530

    adding create method

--- circfourbitmap/src/main/java/com/anwesome/games/circfourbitmap/CircFourBitmapView.java
+++ circfourbitmap/src/main/java/com/anwesome/games/circfourbitmap/CircFourBitmapView.java
@@ -3,15 +3,20 @@ package com.anwesome.games.circfourbitmap;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.animation.ValueAnimator;
+import android.app.Activity;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.Point;
 import android.graphics.RectF;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+
+import com.anwesome.ui.dimensionsutil.DimensionsUtil;
 
 /**
  * Created by anweshmishra on 28/06/17.
@@ -112,5 +117,10 @@ public class CircFourBitmapView extends View{
                 isAnimated = true;
             }
         }
+    }
+    public static void create(Activity activity,Bitmap bitmap) {
+        CircFourBitmapView circFourBitmapView = new CircFourBitmapView(activity,bitmap);
+        Point size = DimensionsUtil.getDeviceDimension(activity);
+        activity.addContentView(circFourBitmapView,new ViewGroup.LayoutParams(size.x,size.x));
     }
 }
