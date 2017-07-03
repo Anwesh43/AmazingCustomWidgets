ff31bd0d700790239b3ee2b33b37704fa781d2ad
Anwesh43 <anweshthecool0@gmail.com>
2017-07-03 12:55:15 +0530

    adding verticalclipimage

--- verticalclipimageview/src/main/java/com/anwesome/games/verticalclipimageview/VerticalClipImageView.java
+++ verticalclipimageview/src/main/java/com/anwesome/games/verticalclipimageview/VerticalClipImageView.java
@@ -5,6 +5,7 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.graphics.RectF;
 import android.view.MotionEvent;
 import android.view.View;
@@ -30,14 +31,15 @@ public class VerticalClipImageView extends View {
             wSize = size/n;
         }
         canvas.save();
-        canvas.translate(w/2-size/2,h/2);
+        canvas.translate(w/2-size/2,h/2-size/2);
         paint.setColor(Color.BLUE);
         paint.setStyle(Paint.Style.STROKE);
         paint.setStrokeWidth(5);
         for(int i=0;i<n;i++) {
             float x = i*wSize;
-            canvas.drawRect(new RectF(x,-size/2,x+wSize,size/2),paint);
+            canvas.drawRect(new RectF(x,0,x+wSize,size),paint);
         }
+        paint.setStyle(Paint.Style.FILL);
         canvas.restore();
         time++;
     }
@@ -49,5 +51,54 @@ public class VerticalClipImageView extends View {
     }
     public void setN(int n) {
         this.n = Math.max(n,this.n);
+    }
+    private class VerticalClipImage {
+        private int index;
+        private float x,y,scale = 0,dir = 0;
+        public VerticalClipImage(int index) {
+            this.index = index;
+            this.x = wSize*index;
+            this.y = 0;
+        }
+        public void draw(Canvas canvas) {
+            canvas.save();
+            Path path = new Path();
+            path.addRect(new RectF(0,0,wSize,size*scale), Path.Direction.CCW);
+            canvas.clipPath(path);
+            canvas.drawBitmap(bitmap,0,0,paint);
+            canvas.restore();
+        }
+        private void startUpdating() {
+            if(scale >= 1) {
+                dir = -1;
+            }
+            else {
+                dir = 1;
+            }
+        }
+        public void update() {
+            scale+=dir*0.2f;
+            if(scale > 1) {
+                scale = 1;
+                dir = 0;
+            }
+            if(scale < 0) {
+                scale = 0;
+                dir = 0;
+            }
+        }
+        public int hashCode() {
+            return index;
+        }
+        public boolean handleTap(float x,float y) {
+            boolean condition = x>=this.x && x<=this.x+wSize && y>=this.y && y<=this.y+size;
+            if(condition) {
+                startUpdating();
+            }
+            return condition;
+        }
+        public boolean stopped() {
+            return dir == 0;
+        }
     }
 }
