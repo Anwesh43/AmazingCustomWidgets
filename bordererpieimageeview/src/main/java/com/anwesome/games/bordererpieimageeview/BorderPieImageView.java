639596d9ee07f3955b757af2f3e68eac5fa2a642
Anwesh43 <anweshthecool0@gmail.com>
2017-07-04 11:31:58 +0530

    creating PieBitmap

--- bordererpieimageeview/src/main/java/com/anwesome/games/bordererpieimageeview/BorderPieImageView.java
+++ bordererpieimageeview/src/main/java/com/anwesome/games/bordererpieimageeview/BorderPieImageView.java
@@ -3,6 +3,10 @@ package com.anwesome.games.bordererpieimageeview;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.RectF;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -13,6 +17,8 @@ import android.view.View;
 public class BorderPieImageView extends View {
     private int time = 0,w,h,size;
     private Bitmap bitmap;
+    private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    private int bitmapPivots[][] = {{1,1},{-1,1},{-1,-1},{1,-1}};
     public BorderPieImageView(Context context,Bitmap bitmap) {
         super(context);
         this.bitmap = bitmap;
@@ -34,5 +40,61 @@ public class BorderPieImageView extends View {
 
         }
         return true;
+    }
+    private class PieBitmap {
+        private int index;
+        private float x,y,r,dir=0,scale = 0;
+        public PieBitmap(int index) {
+            this.index = index;
+            r = size/5;
+            x = (float)((size+r)*Math.cos(((index*90)+45)*Math.PI/180));
+            y = (float)((size+r)*Math.sin(((index*90)+45)*Math.PI/180));
+        }
+        public void draw(Canvas canvas) {
+            canvas.save();
+            canvas.translate(x,y);
+            paint.setColor(Color.BLUE);
+            paint.setStyle(Paint.Style.STROKE);
+            paint.setStrokeWidth(r/30);
+            canvas.drawCircle(0,0,r,paint);
+            paint.setStyle(Paint.Style.FILL);
+            canvas.drawArc(new RectF(-r,-r,r,r),0,360*scale,true,paint);
+            canvas.restore();
+            canvas.save();
+            canvas.translate(size/2*bitmapPivots[index][0],size/2*bitmapPivots[index][1]);
+            Path path = new Path();
+            path.addRect(new RectF((-size/4)*scale,(-size/4)*scale,(size/4)*scale,(size/4)*scale), Path.Direction.CCW);
+            canvas.drawBitmap(bitmap,-size/2,-size/2,paint);
+            canvas.restore();
+        }
+        public int hashCode() {
+            return index;
+        }
+        public void update() {
+            scale+=0.2f*dir;
+            if(scale > 1) {
+                dir = 0;
+                scale = 1;
+            }
+            if(scale < 0) {
+                dir = 0;
+                scale = 0;
+            }
+        }
+        private void startUpdating() {
+            if(scale >= 1) {
+                dir = 1;
+            }
+            if(scale <= 0) {
+                dir = 0;
+            }
+        }
+        public boolean handleTap(float x,float y) {
+            boolean condition =  x>=this.x-r && x<=this.x+r && y>=this.y-r && y<=this.y+r;
+            if(condition) {
+                startUpdating();
+            }
+            return condition;
+        }
     }
 }
