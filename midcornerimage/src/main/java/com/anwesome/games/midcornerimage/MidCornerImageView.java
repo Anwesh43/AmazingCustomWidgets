c532e220040c33ad01126fb5de538feb73c86a56
Anwesh43 <anweshthecool0@gmail.com>
2017-07-06 02:29:44 +0530

    adding MidCornerImage class

--- midcornerimage/src/main/java/com/anwesome/games/midcornerimage/MidCornerImageView.java
+++ midcornerimage/src/main/java/com/anwesome/games/midcornerimage/MidCornerImageView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.RectF;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -28,14 +29,33 @@ public class MidCornerImageView extends View {
         }
         canvas.save();
         canvas.translate(w/2,h/2);
-        canvas.drawBitmap(bitmap,-size/2,-size/2,paint)
+        canvas.drawBitmap(bitmap,-size/2,-size/2,paint);
         canvas.restore();
         time++;
     }
     public boolean onTouchEvent(MotionEvent event) {
         if(event.getAction() == MotionEvent.ACTION_DOWN) {
-            
+
         }
         return true;
+    }
+    private class MidCornerImage {
+        private float scale = 0;
+        public void draw(Canvas canvas) {
+            for(int i=0;i<4;i++) {
+                canvas.save();
+                canvas.rotate(i*90);
+                float x = size/2,y = size/2;
+                canvas.save();
+                canvas.translate(x,y);
+                canvas.drawLine((-size/2)*scale,(-size/2)*scale,0,0,paint);
+                canvas.drawArc(new RectF(-size/10,-size/10,size/10,size/10),0,360*scale,true,paint);
+                canvas.restore();
+                canvas.restore();
+            }
+        }
+        public void update(float factor) {
+            scale = factor;
+        }
     }
 }
