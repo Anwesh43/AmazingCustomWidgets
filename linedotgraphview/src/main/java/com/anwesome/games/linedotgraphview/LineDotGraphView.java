527ee3cd10fe738a7dd5b509cf128e3d8b7f91fd
Anwesh43 <anweshthecool0@gmail.com>
2017-07-07 09:59:49 +0530

    adding linedot

--- linedotgraphview/src/main/java/com/anwesome/games/linedotgraphview/LineDotGraphView.java
+++ linedotgraphview/src/main/java/com/anwesome/games/linedotgraphview/LineDotGraphView.java
@@ -10,7 +10,7 @@ import android.view.View;
  */
 
 public class LineDotGraphView extends View {
-    private int data[],time = 0,w,h;
+    private int data[],time = 0,w,h,size,px,py;
     public LineDotGraphView(Context context,int[] data) {
         super(context);
         this.data = data;
@@ -19,6 +19,9 @@ public class LineDotGraphView extends View {
         if(time == 0) {
             w = canvas.getWidth();
             h = canvas.getHeight();
+            size = w/(2*data.length+1);
+            px = w/8;
+            py = h/8;
         }
         time++;
     }
@@ -27,5 +30,42 @@ public class LineDotGraphView extends View {
 
         }
         return true;
+    }
+    private class LineDot {
+        private float x,h,dir = 0,scale = 0;
+        public LineDot(float x,float h) {
+            this.x = x;
+            this.h = h;
+        }
+        public void draw(Canvas canvas) {
+
+        }
+        public void update() {
+            scale += 0.2f*dir;
+            if(scale > 1) {
+                dir = 0;
+                scale = 0;
+            }
+            if(scale < 0) {
+                dir = 0;
+                scale = 0;
+            }
+        }
+        private void startUpdating() {
+            dir = scale == 0?1:0;
+        }
+        public int hashCode() {
+            return (int)(x+h+scale+dir);
+        }
+        public boolean handleTap(float x,float y) {
+            boolean condition =  x>=px+this.x-size && x<=px+this.x+size && y>=py+this.h-size && y<=py+this.h+size;
+            if(condition) {
+                startUpdating();
+            }
+            return condition;
+        }
+        public boolean stopUpdating() {
+            return dir == 0;
+        }
     }
 }
