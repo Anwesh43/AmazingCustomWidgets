9fe9c7e9aba727afaf6a1394d4b41946d6eaf92a
Anwesh43 <anweshthecool0@gmail.com>
2017-07-09 02:58:41 +0530

    adding PolygonalLineDot

--- polygonallinedotview/src/main/java/com/anwesome/games/polygonallinedotview/PolygonalLineDotView.java
+++ polygonallinedotview/src/main/java/com/anwesome/games/polygonallinedotview/PolygonalLineDotView.java
@@ -2,6 +2,8 @@ package com.anwesome.games.polygonallinedotview;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.RectF;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -10,7 +12,8 @@ import android.view.View;
  */
 
 public class PolygonalLineDotView extends View{
-    private int n = 3,time = 0,w,h,size;
+    private int n = 3,time = 0,w,h,size,deg;
+    private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
     public PolygonalLineDotView(Context context,int n) {
         super(context);
         this.n = Math.max(n,this.n);
@@ -20,7 +23,12 @@ public class PolygonalLineDotView extends View{
             w = canvas.getWidth();
             h = canvas.getHeight();
             size = Math.min(w,h)/2;
+            deg = 360/n;
         }
+        canvas.save();
+        canvas.translate(w/2,h/2);
+
+        canvas.restore();
         time++;
     }
     public boolean onTouchEvent(MotionEvent event) {
@@ -28,5 +36,42 @@ public class PolygonalLineDotView extends View{
 
         }
         return true;
+    }
+    private class PolygonalLineDot {
+        private int index,dir=0;
+        private float x,y,scale = 0;
+        public PolygonalLineDot(int index) {
+            this.index = index;
+            this.x = (float)((size/2)*Math.cos(index*deg*Math.PI/180));
+            this.y = (float)((size/2)*Math.sin(index*deg*Math.PI/180));
+        }
+        public void draw(Canvas canvas) {
+            canvas.save();
+            canvas.translate(x,y);
+            canvas.drawLine(0,0,x*(1-scale),y*(1-scale),paint);
+            canvas.drawArc(new RectF(-size/8,-size/8,size/8,size/8),0,360*scale,true,paint);
+            canvas.restore();
+        }
+        public void update() {
+            scale+=0.2f*dir;
+            if(scale > 1) {
+                scale = 1;
+                dir = 0;
+            }
+            if(scale < 0) {
+                scale = 0;
+                dir = 0;
+            }
+        }
+        public int hashCode() {
+            return index;
+        }
+        public boolean handleTap(float x,float y) {
+            boolean condition =  x>=this.x-size/8 && x<=this.x+size/8 && y>=this.y-size/8 && y<=this.y+size/8 && dir == 0;
+            if(condition) {
+                dir = scale == 0?1:-1;
+            }
+            return condition;
+        }
     }
 }
