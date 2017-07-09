7456f2a828237027e6d1c174cddf2628790fc32c
Anwesh43 <anweshthecool0@gmail.com>
2017-07-10 05:19:28 +0530

    adding OrbitRing class

--- orbitringview/src/main/java/com/anwesome/games/orbitringview/OrbitRingView.java
+++ orbitringview/src/main/java/com/anwesome/games/orbitringview/OrbitRingView.java
@@ -2,7 +2,9 @@ package com.anwesome.games.orbitringview;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -71,6 +73,68 @@ public class OrbitRingView extends View {
                 animated = true;
                 postInvalidate();
             }
+        }
+    }
+    private class OrbitRing {
+        private int index = 0;
+        private float x,y,r,scale = 0,dir = 0;
+        public OrbitRing(int index,float size) {
+            this.index = index;
+            this.r = size*(index+1);
+            this.x = 0;
+            this.y = -this.r;
+        }
+        public void draw(Canvas canvas) {
+            float deg = 360*scale;
+            canvas.save();
+            canvas.translate(canvas.getWidth()/2,canvas.getHeight()/2);
+            canvas.save();
+            canvas.rotate(deg);
+            paint.setStyle(Paint.Style.STROKE);
+            paint.setColor(Color.BLUE);
+            canvas.drawCircle(this.x,this.y,r/8,paint);
+            canvas.restore();
+            paint.setStyle(Paint.Style.STROKE);
+            paint.setStrokeWidth(r/60);
+            paint.setColor(Color.GRAY);
+            canvas.drawCircle(0,0,r,paint);
+            paint.setColor(Color.BLUE);
+            Path path = new Path();
+            for(float i=0;i<deg;i+=10) {
+                float x= (float)(r*Math.cos(i*Math.PI/180)),y = (float)(r*Math.sin(i*Math.PI/180));
+                if(i == 0) {
+                    path.moveTo(x,y);
+                }
+                else {
+                    path.lineTo(x,y);
+                }
+            }
+            canvas.drawPath(path,paint);
+            canvas.restore();
+        }
+        public void update() {
+            scale += 0.2f*dir;
+            if(scale > 1) {
+                dir = 0;
+                scale = 1;
+            }
+            if(scale < 0) {
+                dir = 0;
+                scale = 0;
+            }
+        }
+        public boolean stopped() {
+            return dir == 0;
+        }
+        public int hashCode() {
+            return index;
+        }
+        public boolean handleTap(float x,float y) {
+            boolean condition =  x>=this.x-r/8 && x>=this.x+r/8 && y>=this.y-r/8 && y<=this.y+r/8 && dir == 0;
+            if(condition) {
+                dir = scale >=0?1:-1;
+            }
+            return condition;
         }
     }
 }
