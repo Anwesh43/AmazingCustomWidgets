636603935530453968d167722728e54a99d9aa2b
Anwesh43 <anweshthecool0@gmail.com>
2017-07-13 11:12:54 +0530

    adding CircularPlate

--- circularplatecollapser/src/main/java/com/anwesome/games/circularplatecollapser/CircularPlateCollapser.java
+++ circularplatecollapser/src/main/java/com/anwesome/games/circularplatecollapser/CircularPlateCollapser.java
@@ -2,6 +2,9 @@ package com.anwesome.games.circularplatecollapser;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -28,5 +31,24 @@ public class CircularPlateCollapser {
             }
             return true;
         }
+        private class CircularPlate {
+            private float deg = 0;
+            private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+            public void draw(Canvas canvas) {
+                canvas.save();
+                canvas.translate(w/2,w/10+w/4);
+                paint.setColor(Color.BLUE);
+                paint.setStrokeWidth(w/60);
+                paint.setStyle(Paint.Style.STROKE);
+                canvas.drawArc(new RectF(-w/4,-w/4,w/4,w/4),0,360,true,paint);
+                paint.setStyle(Paint.Style.FILL);
+                canvas.drawArc(new RectF(-w/4,-w/4,w/4,w/4),0,deg,true,paint);
+                canvas.restore();
+            }
+            public void update(float scale) {
+                deg = 360*scale;
+            }
+        }
     }
+
 }
