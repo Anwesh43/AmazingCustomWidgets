1f8015a72d4c80fea7c31b31b87ade7db80065ca
Anwesh43 <anweshthecool0@gmail.com>
2017-07-14 17:24:31 +0530

    adding create method for CrossLineView

--- crosslineview/src/main/java/com/anwesome/games/crosslineview/CrossLineView.java
+++ crosslineview/src/main/java/com/anwesome/games/crosslineview/CrossLineView.java
@@ -1,12 +1,17 @@
 package com.anwesome.games.crosslineview;
 
+import android.app.Activity;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Point;
 import android.graphics.RectF;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+
+import com.anwesome.ui.dimensionsutil.DimensionsUtil;
 
 /**
  * Created by anweshmishra on 14/07/17.
@@ -103,5 +108,10 @@ public class CrossLineView extends View {
                 postInvalidate();
             }
         }
+    }
+    public static void create(Activity activity) {
+        CrossLineView crossLineView = new CrossLineView(activity);
+        Point size = DimensionsUtil.getDeviceDimension(activity);
+        activity.addContentView(crossLineView,new ViewGroup.LayoutParams(size.x,size.x));
     }
 }
