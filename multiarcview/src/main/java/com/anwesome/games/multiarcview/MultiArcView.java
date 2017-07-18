9e411dc353cfbd38fed9c73e698592fd1fb03574
Anwesh43 <anweshthecool0@gmail.com>
2017-07-18 16:07:22 +0530

    adding multiarc class

--- multiarcview/src/main/java/com/anwesome/games/multiarcview/MultiArcView.java
+++ multiarcview/src/main/java/com/anwesome/games/multiarcview/MultiArcView.java
@@ -2,7 +2,9 @@ package com.anwesome.games.multiarcview;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.RectF;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -12,7 +14,10 @@ import android.view.View;
 
 public class MultiArcView extends View {
     private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-    private int time = 0,w,h;
+    private int time = 0,w,h,n=3;
+    public void setN(int n) {
+        this.n = Math.max(this.n,n);
+    }
     public MultiArcView(Context context) {
         super(context);
     }
@@ -28,5 +33,22 @@ public class MultiArcView extends View {
 
         }
         return true;
+    }
+    private class MultiArc {
+        public void draw(Canvas canvas, float scale) {
+            for(int i=0;i<n;i++) {
+                paint.setColor(Color.GRAY);
+                drawArc(canvas,paint,1,i);
+                paint.setColor(Color.CYAN);
+                drawArc(canvas,paint,scale,i);
+            }
+        }
+        private void drawArc(Canvas canvas,Paint paint,float scale,int i) {
+            canvas.save();
+            canvas.translate(w/2,h/2);
+            float size = ((w/2*i)*1.0f)/n;
+            canvas.drawArc(new RectF(-size/2,-size/2,size/2,size/2),270,90*scale,false,paint);
+            canvas.restore();
+        }
     }
 }
