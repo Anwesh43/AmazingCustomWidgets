67a42923aecce57dda5cd0520ba15b48e590e1fd
Anwesh43 <anweshthecool0@gmail.com>
2017-07-18 15:44:06 +0530

    initializing width and height

--- multiarcview/src/main/java/com/anwesome/games/multiarcview/MultiArcView.java
+++ multiarcview/src/main/java/com/anwesome/games/multiarcview/MultiArcView.java
@@ -12,11 +12,16 @@ import android.view.View;
 
 public class MultiArcView extends View {
     private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    private int time = 0,w,h;
     public MultiArcView(Context context) {
         super(context);
     }
     public void onDraw(Canvas canvas) {
-
+        if(time == 0) {
+            w = canvas.getWidth();
+            h = canvas.getHeight();
+        }
+        time++;
     }
     public boolean onTouchEvent(MotionEvent event) {
         if(event.getAction() == MotionEvent.ACTION_DOWN) {
