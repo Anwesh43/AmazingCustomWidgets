cf94c339056dcc5b63ef8bbd3a0dc6ab43749288
Anwesh43 <anweshthecool0@gmail.com>
2017-07-21 16:32:04 +0530

    adding ColorSlideContainer

--- colorslidecontainer/src/main/java/com/anwesome/games/colorslidecontainer/ColorSlideContainer.java
+++ colorslidecontainer/src/main/java/com/anwesome/games/colorslidecontainer/ColorSlideContainer.java
@@ -2,7 +2,9 @@ package com.anwesome.games.colorslidecontainer;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.RectF;
 import android.view.View;
 
 /**
@@ -15,6 +17,11 @@ public class ColorSlideContainer extends View{
     private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
     public ColorSlideContainer(Context context,int colors[]) {
         super(context);
+        this.colors = colors;
+        initColorSlides();
+    }
+    public void initColorSlides() {
+
     }
     public void onDraw(Canvas canvas) {
         if(time == 0) {
@@ -22,5 +29,18 @@ public class ColorSlideContainer extends View{
             h = canvas.getHeight();
         }
         time++;
+    }
+    private class ColorSlide {
+        private int color;
+        public ColorSlide(int color) {
+            this.color = color;
+        }
+        public void draw(Canvas canvas) {
+            paint.setColor(color);
+            canvas.drawRect(new RectF(0,0,w,h),paint);
+        }
+        public int hashCode() {
+            return color;
+        }
     }
 }
